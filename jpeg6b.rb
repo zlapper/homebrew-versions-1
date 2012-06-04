5cdb95195b375779f13bfe1a56b441c64cf3aa42
Jack Nagel <jacknagel@gmail.com>
2012-06-04 17:27:03 -0500

    jpeg 6b
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ jpeg6b.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Jpeg6b < Formula
+  homepage 'http://www.ijg.org'
+  url 'http://www.ijg.org/files/jpegsrc.v6b.tar.gz'
+  sha1 '7079f0d6c42fad0cfba382cf6ad322add1ace8f9'
+  version '6b'
+
+  depends_on 'libtool' => :build
+
+  def install
+    bin.mkpath
+    lib.mkpath
+    include.mkpath
+    man1.mkpath
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-shared"
+
+    system "make", "install",
+                   "install-lib",
+                   "install-headers",
+                   "mandir=#{man1}",
+                   "LIBTOOL=glibtool"
+  end
+end
