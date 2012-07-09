1575cd1fd21ad4066d0be73c1f4de12ce74e4cd5
Ramiro Rikkert <rrikkert@gmail.com>
2012-07-09 09:14:50 +0300

    play12 1.2.5
    Closes #23.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- play12.rb
+++ play12.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Play12 < Formula
   homepage 'http://www.playframework.org/'
-  url 'http://download.playframework.org/releases/play-1.2.4.zip'
-  md5 'ec8789f8cc02927ece536d102f5e649e'
+  url 'http://download.playframework.org/releases/play-1.2.5.zip'
+  md5 '31d204bb105f67c5e418fad073e818a4'
 
   def install
     rm_rf 'python' # we don't need the bundled Python for windows
