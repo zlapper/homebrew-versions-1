e7ace76346224e21d2fa390204122412a139b325
Jack Nagel <jacknagel@gmail.com>
2012-08-19 16:59:19 -0500

    gnutls3 3.1.0
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- gnutls3.rb
+++ gnutls3.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gnutls3 < Formula
   homepage 'http://www.gnu.org/software/gnutls/gnutls.html'
-  url 'http://ftpmirror.gnu.org/gnutls/gnutls-3.0.21.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/gnutls/gnutls-3.0.21.tar.xz'
-  sha256 '6901b0203a613869cf475f18d44acd47b36adf714c67d1f9ad29c26ab7bec410'
+  url 'http://ftpmirror.gnu.org/gnutls/gnutls-3.1.0.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/gnutls/gnutls-3.1.0.tar.xz'
+  sha256 '4fdb58572fb91fc0afbdfcd7845d4467d4b13ef2f9141bdaa955b959a319f8cc'
 
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
@@ -12,13 +12,23 @@ class Gnutls3 < Formula
   depends_on 'nettle'
   depends_on 'libidn'
   depends_on 'gmp'
-  depends_on 'libtasn1' => :optional
+  depends_on 'libtasn1'
+
+  conflicts_with 'gnutls',
+    :because => <<-EOS.undent
+      GnuTLS 2.x and 3.x install identically named libraries,
+      headers, executables, and other files.
+      EOS
+
+  # Fix an "undeclared identifier" error with clang
+  def patches
+    "http://git.savannah.gnu.org/cgit/gnutls.git/patch/?id=3b91334247"
+  end
 
   def install
     ENV.append 'LDFLAGS', '-ltasn1'
 
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
+    system "./configure", "--disable-dependency-tracking",
                           "--disable-guile",
                           "--disable-static",
                           "--disable-hardware-acceleration",
