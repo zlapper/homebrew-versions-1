860993d8701266fdb462382d7c94e5fcab8c98ff
Jack Nagel <jacknagel@gmail.com>
2012-09-04 17:32:39 -0500

    imagemagick-ruby: fix X11 usage
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- imagemagick-ruby186.rb
+++ imagemagick-ruby186.rb
@@ -11,24 +11,18 @@ def ghostscript_srsly?
   ARGV.include? '--with-ghostscript'
 end
 
-def x11?
-  # I used this file because old Xcode seems to lack it, and its that old
-  # Xcode that loads of people seem to have installed still
-  File.file? '/usr/X11/include/ft2build.h'
-end
-
 class ImagemagickRuby186 < Formula
   homepage 'http://www.imagemagick.org'
   url 'http://image_magick.veidrodis.com/image_magick/ImageMagick-6.5.9-8.tar.bz2'
   md5 '89892e250e81fad51b4b2a1f816987e6'
 
   depends_on 'jpeg'
-  depends_on 'libwmf' => :optional if x11?
+  depends_on 'libwmf' => :optional
   depends_on 'libtiff' => :optional
   depends_on 'little-cms' => :optional
   depends_on 'jasper' => :optional
-  depends_on 'ghostscript' => :recommended if ghostscript_srsly? and x11?
-  depends_on 'libpng' unless x11?
+  depends_on 'ghostscript' => :recommended if ghostscript_srsly?
+  depends_on :libpng
 
   def skip_clean? path
     path.extname == '.la'
@@ -59,7 +53,6 @@ class ImagemagickRuby186 < Formula
   end
 
   def install
-    ENV.libpng
     ENV.deparallelize
 
     fix_configure
@@ -74,10 +67,6 @@ class ImagemagickRuby186 < Formula
     (share+"ImageMagick/NEWS.txt").unlink
     (share+"ImageMagick/LICENSE").unlink
     (share+"ImageMagick/ChangeLog").unlink
-  end
-
-  def caveats
-    "You don't have X11 from the Xcode DMG installed. Consequently Imagemagick is less fully featured." unless x11?
   end
 end
 
