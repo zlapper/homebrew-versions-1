7e81afe52d58041a38b1767e06b0861e702c1f41
Adam Vandenberg <flangy@gmail.com>
2013-01-20 10:44:51 -0800

    Appledoc 2.0.6 for Snow Leopard

---
+++ appledoc20.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+# The 2.1 series requires Lion or newer.
+# 2.0.6 is provided for Snow Leopard compatibility.
+class Appledoc20 < Formula
+  homepage 'http://appledoc.gentlebytes.com/'
+  url 'https://github.com/tomaz/appledoc/archive/v2.0.6.tar.gz'
+  sha1 '3d76172339cbfea24ef53f55e2452cc78930413e'
+
+  depends_on :xcode # For working xcodebuild.
+
+  def install
+    system "xcodebuild", "-project", "appledoc.xcodeproj",
+                         "-target", "appledoc",
+                         "-configuration", "Release",
+                         "clean", "install",
+                         "SYMROOT=build",
+                         "DSTROOT=build",
+                         "INSTALL_PATH=/bin",
+                         "OTHER_CFLAGS='-DCOMPILE_TIME_DEFAULT_TEMPLATE_PATH=@\"#{prefix}/Templates\"'"
+    bin.install "build/bin/appledoc"
+    prefix.install "Templates/"
+  end
+
+  def test
+    system "#{bin}/appledoc", "--version"
+  end
+end
