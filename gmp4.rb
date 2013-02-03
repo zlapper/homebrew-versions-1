c19007b7150c9910d4945f2b0815994111a57b7c
Jack Nagel <jacknagel@gmail.com>
2013-02-02 22:07:53 -0600

    gmp4: fix audit warnings

--- gmp4.rb
+++ gmp4.rb
@@ -5,12 +5,8 @@ class Gmp4 < Formula
   url 'ftp://ftp.gmplib.org/pub/gmp-4.3.2/gmp-4.3.2.tar.bz2'
   sha1 'c011e8feaf1bb89158bd55eaabd7ef8fdd101a2c'
 
-  def options
-    [
-      ["--32-bit", "Force 32-bit."],
-      ["--skip-check", "Do not run 'make check' to verify libraries."]
-    ]
-  end
+  option '32-bit'
+  option 'skip-check', 'Do not run `make check` to verify libraries'
 
   def install
     # Reports of problems using gcc 4.0 on Leopard
@@ -21,7 +17,7 @@ class Gmp4 < Formula
     args = ["--prefix=#{prefix}", "--enable-cxx"]
 
     # Build 32-bit where appropriate, and help configure find 64-bit CPUs
-    if MacOS.prefer_64_bit? and not ARGV.include? "--32-bit"
+    if MacOS.prefer_64_bit? and not build.build_32_bit?
       ENV.m64
       args << "--build=x86_64-apple-darwin"
     else
@@ -36,6 +32,6 @@ class Gmp4 < Formula
 
     # Different compilers and options can cause tests to fail even
     # if everything compiles, so yes, we want to do this step.
-    system "make check" unless ARGV.include? "--skip-check"
+    system "make check" unless build.include? "skip-check"
   end
 end
