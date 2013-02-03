a8636d4b6f91da43fc1deccbf48091d6270ba781
Jack Nagel <jacknagel@gmail.com>
2013-02-02 22:06:44 -0600

    gsl114: fix audit warnings

--- gsl114.rb
+++ gsl114.rb
@@ -6,12 +6,10 @@ class Gsl114 < Formula
   mirror 'http://ftp.gnu.org/gnu/gsl/gsl-1.14.tar.gz'
   sha1 'e1a600e4fe359692e6f0e28b7e12a96681efbe52'
 
-  def options
-    [["--universal", "Build a universal binary."]]
-  end
+  option :universal
 
   def install
-    ENV.universal_binary if ARGV.build_universal?
+    ENV.universal_binary if build.universal?
 
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make" # "make" and "make install" _must_ be done separately
