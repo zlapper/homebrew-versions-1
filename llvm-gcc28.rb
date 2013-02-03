71ef06640ac6f480b43602936ff5f80aa44cb806
Jack Nagel <jacknagel@gmail.com>
2013-02-02 21:58:43 -0600

    llvm-gcc28: remove skip_clean :all

--- llvm-gcc28.rb
+++ llvm-gcc28.rb
@@ -6,7 +6,6 @@ class LlvmGcc28 < Formula
   version '2.8'
   sha1 'f387a5f774c36cd96ab378f919c5861351110941'
 
-  skip_clean :all # This is a binary install
   keg_only :provided_by_osx
 
   def install
