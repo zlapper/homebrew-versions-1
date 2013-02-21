e951cb41f6ad81ea8086adf5378f755c0729563b
Jack Nagel <jacknagel@gmail.com>
2013-02-21 16:58:10 -0600

    Bite the bullet and rename node4 to node04

--- node04.rb
+++ node04.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Node4 < Formula
+class Node04 < Formula
   homepage 'http://nodejs.org/'
   url 'http://nodejs.org/dist/node-v0.4.12.tar.gz'
   sha1 '1c6e34b90ad6b989658ee85e0d0cb16797b16460'
