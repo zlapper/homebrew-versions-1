a58fb65910d660b24921dd738b3c65fbf8f0bd4f
Jack Nagel <jacknagel@gmail.com>
2013-02-21 16:43:16 -0600

    Add ruby 1.9.3
    Presumably we will want this here once 2.0.0 hits core.

---
+++ ruby193.rb
@@ -0,0 +1,68 @@
+require 'formula'
+
+class Ruby193 < Formula
+  homepage 'http://www.ruby-lang.org/en/'
+  url 'http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p385.tar.bz2'
+  sha256 'f991ee50414dc795696bad0fc5c7b0b94d93b9b38fed943326d20ce4e9dda42b'
+
+  head 'http://svn.ruby-lang.org/repos/ruby/trunk/'
+
+  env :std
+
+  option :universal
+  option 'with-suffix', 'Suffix commands with "193"'
+  option 'with-doc', 'Install documentation'
+  option 'with-tcltk', 'Install with Tcl/Tk support'
+
+  if build.universal?
+    depends_on 'autoconf' => :build
+  elsif build.head?
+    depends_on :autoconf
+  end
+
+  depends_on 'pkg-config' => :build
+  depends_on 'readline'
+  depends_on 'gdbm'
+  depends_on 'libyaml'
+  depends_on :x11 if build.include? 'with-tcltk'
+
+  fails_with :llvm do
+    build 2326
+  end
+
+  def install
+    system "autoconf" if build.head?
+
+    args = ["--prefix=#{prefix}",
+            "--enable-shared"]
+
+    args << "--program-suffix=193" if build.include? "with-suffix"
+    args << "--with-arch=x86_64,i386" if build.universal?
+    args << "--disable-tcltk-framework" <<  "--with-out-ext=tcl" <<  "--with-out-ext=tk" unless build.include? "with-tcltk"
+    args << "--disable-install-doc" unless build.include? "with-doc"
+
+    # Put gem, site and vendor folders in the HOMEBREW_PREFIX
+    ruby_lib = HOMEBREW_PREFIX/"lib/ruby"
+    (ruby_lib/'site_ruby').mkpath
+    (ruby_lib/'vendor_ruby').mkpath
+    (ruby_lib/'gems').mkpath
+
+    (lib/'ruby').install_symlink ruby_lib/'site_ruby',
+                                 ruby_lib/'vendor_ruby',
+                                 ruby_lib/'gems'
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+    system "make install-doc" if build.include? "with-doc"
+
+  end
+
+  def caveats; <<-EOS.undent
+    NOTE: By default, gem installed binaries will be placed into:
+      #{opt_prefix}/bin
+
+    You may want to add this to your PATH.
+    EOS
+  end
+end
