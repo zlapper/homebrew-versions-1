15fc52069e6d062e991658a2a8bf89416d845c03
Jack Nagel <jacknagel@gmail.com>
2013-03-19 12:35:21 -0500

    erlang-r15: fix checksum

--- erlang-r15.rb
+++ erlang-r15.rb
@@ -24,7 +24,7 @@ class ErlangR15 < Formula
   homepage 'http://www.erlang.org'
   # Download tarball from GitHub; it is served faster than the official tarball.
   url 'https://github.com/erlang/otp/archive/OTP_R15B03-1.tar.gz'
-  sha1 '5ba866722de79956b06966c232490d32bb7ba0a6'
+  sha1 '7843070f5d325f95ef13022fc416b22b6b14120d'
 
   # remove the autoreconf if possible
   depends_on :automake
