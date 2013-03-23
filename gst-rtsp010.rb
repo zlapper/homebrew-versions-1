571c0448eb2bdcad655115fb37aebd992f657577
Jack Nagel <jacknagel@gmail.com>
2013-03-23 17:21:48 -0500

    gst-rtsp010: use configure directly

--- gst-rtsp010.rb
+++ gst-rtsp010.rb
@@ -5,19 +5,13 @@ class GstRtsp010 < Formula
   url 'http://gstreamer.freedesktop.org/src/gst-rtsp/gst-rtsp-0.10.8.tar.bz2'
   sha256 '9915887cf8515bda87462c69738646afb715b597613edc7340477ccab63a6617'
 
-  depends_on :automake
-  depends_on :libtool
-
   depends_on 'pkg-config' => :build
   depends_on 'gettext'
   depends_on 'homebrew/versions/gst-plugins-base010'
 
   def install
-    # Compatibility with Automake 1.13 and newer.
-    inreplace 'configure.ac', 'AM_CONFIG_HEADER', 'AC_CONFIG_HEADERS'
-    system "./autogen.sh", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure",  "--disable-debug", "--disable-dependency-tracking",
                            "--prefix=#{prefix}",
-                           "--disable-schemas-install",
                            "--disable-gtk-doc"
     system "make"
     system "make install"
