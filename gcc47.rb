56876dfa9920ce5bc5a3e695b372f4074c837fac
Xiyue Deng <manphiz@gmail.com>
2013-04-12 01:04:18 -0700

    GCC47: 4.7.3.
    * GCC 4.7 now can be bootstrapped by Clang. Drop fails_with part.
    * Remove obsolete ENV.m64 part. GCC 4.7 detects host triplets.
    * Apply patches from macports to fix debugging.
    * Don't unset __GLIBCXX_DEBUG* as original bug didn't actually do that.
    * Make arch and osmajor standalone functions, and construct host triplet
      for build.
    * Integrate '--enable-languages' into args.
    
    Closes #99.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- gcc47.rb
+++ gcc47.rb
@@ -5,10 +5,18 @@ require 'formula'
 # is in progress. It may or may not work on other platforms."
 
 class Gcc47 < Formula
+  def arch
+    `uname -m`.chomp
+  end
+
+  def osmajor
+    `uname -r`.chomp
+  end
+
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.7.2/gcc-4.7.2.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.7.2/gcc-4.7.2.tar.bz2'
-  sha1 'a464ba0f26eef24c29bcd1e7489421117fb9ee35'
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.7.3/gcc-4.7.3.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.7.3/gcc-4.7.3.tar.bz2'
+  sha1 '69e02737bd6e1a7c6047d801600d39c32b9427ca'
 
   head 'svn://gcc.gnu.org/svn/gcc/branches/gcc-4_7-branch'
 
@@ -27,66 +35,17 @@ class Gcc47 < Formula
   depends_on 'mpfr'
   depends_on 'ecj' if build.include? 'enable-java' or build.include? 'enable-all-languages'
 
-  fails_with :clang do
-    build 421
-    cause <<-EOS.undent
-      We have had many different clang failure reports:
-        https://github.com/Homebrew/homebrew-dupes/issues/20
-        https://github.com/Homebrew/homebrew-dupes/issues/49
-        https://github.com/Homebrew/homebrew-dupes/pull/66
-        https://github.com/Homebrew/homebrew-dupes/issues/68
-      Unfortunately, nobody seems to be interested in investigating and fixing them.
-      If you have any knowledge to share or can provide a fix, please open an issue.
-      Thanks!
-      EOS
-  end
+  # Import patches from macports:
+  # https://trac.macports.org/browser/trunk/dports/lang/gcc47/files/gcc-PR-53453.patch
+  def patches; DATA; end
 
   def install
-    # Force 64-bit on systems that use it. Build failures reported for some
-    # systems when this is not done.
-    ENV.m64 if MacOS.prefer_64_bit?
-
     # GCC will suffer build errors if forced to use a particular linker.
     ENV.delete 'LD'
-
-    # This is required on systems running a version newer than 10.6, and
-    # it's probably a good idea regardless.
-    #
-    # https://trac.macports.org/ticket/27237
-    ENV.append 'CXXFLAGS', '-U_GLIBCXX_DEBUG -U_GLIBCXX_DEBUG_PEDANTIC'
 
     gmp = Formula.factory 'gmp'
     mpfr = Formula.factory 'mpfr'
     libmpc = Formula.factory 'libmpc'
-
-    # Sandbox the GCC lib, libexec and include directories so they don't wander
-    # around telling small children there is no Santa Claus. This results in a
-    # partially keg-only brew following suggestions outlined in the "How to
-    # install multiple versions of GCC" section of the GCC FAQ:
-    #     http://gcc.gnu.org/faq.html#multiple
-    gcc_prefix = prefix + 'gcc'
-
-    args = [
-      # Sandbox everything...
-      "--prefix=#{gcc_prefix}",
-      # ...except the stuff in share...
-      "--datarootdir=#{share}",
-      # ...and the binaries...
-      "--bindir=#{bin}",
-      # ...which are tagged with a suffix to distinguish them.
-      "--program-suffix=-#{version.to_s.slice(/\d\.\d/)}",
-      "--with-gmp=#{gmp.opt_prefix}",
-      "--with-mpfr=#{mpfr.opt_prefix}",
-      "--with-mpc=#{libmpc.opt_prefix}",
-      "--with-system-zlib",
-      "--enable-stage1-checking",
-      "--enable-plugin",
-      "--enable-lto",
-      # a no-op unless --HEAD is built because in head warnings will raise errs.
-      "--disable-werror"
-    ]
-
-    args << '--disable-nls' unless build.include? 'enable-nls'
 
     if build.include? 'enable-all-languages'
       # Everything but Ada, which requires a pre-existing GCC Ada compiler
@@ -104,6 +63,37 @@ class Gcc47 < Formula
       languages << 'objc' if build.include? 'enable-objc'
       languages << 'obj-c++' if build.include? 'enable-objcxx'
     end
+
+    # Sandbox the GCC lib, libexec and include directories so they don't wander
+    # around telling small children there is no Santa Claus. This results in a
+    # partially keg-only brew following suggestions outlined in the "How to
+    # install multiple versions of GCC" section of the GCC FAQ:
+    #     http://gcc.gnu.org/faq.html#multiple
+    gcc_prefix = prefix + 'gcc'
+
+    args = [
+      "--build=#{arch}-apple-darwin#{osmajor}",
+      # Sandbox everything...
+      "--prefix=#{gcc_prefix}",
+      # ...except the stuff in share...
+      "--datarootdir=#{share}",
+      # ...and the binaries...
+      "--bindir=#{bin}",
+      # ...which are tagged with a suffix to distinguish them.
+      "--enable-languages=#{languages.join(',')}",
+      "--program-suffix=-#{version.to_s.slice(/\d\.\d/)}",
+      "--with-gmp=#{gmp.opt_prefix}",
+      "--with-mpfr=#{mpfr.opt_prefix}",
+      "--with-mpc=#{libmpc.opt_prefix}",
+      "--with-system-zlib",
+      "--enable-stage1-checking",
+      "--enable-plugin",
+      "--enable-lto",
+      # a no-op unless --HEAD is built because in head warnings will raise errs.
+      "--disable-werror"
+    ]
+
+    args << '--disable-nls' unless build.include? 'enable-nls'
 
     if build.include? 'enable-java' or build.include? 'enable-all-languages'
       ecj = Formula.factory 'ecj'
@@ -124,7 +114,7 @@ class Gcc47 < Formula
         args << "--with-sysroot=#{MacOS.sdk_path}"
       end
 
-      system '../configure', "--enable-languages=#{languages.join(',')}", *args
+      system '../configure', *args
 
       if build.include? 'enable-profiled-build'
         # Takes longer to build, may bug out. Provided for those who want to
@@ -144,3 +134,95 @@ class Gcc47 < Formula
     end
   end
 end
+
+__END__
+http://gcc.gnu.org/ml/gcc-patches/2012-06/msg01181.html
+http://gcc.gnu.org/bugzilla/show_bug.cgi?id=53453
+
+diff -uNr gcc/config/darwin.h gcc/config/darwin.h
+--- gcc-4.7.0/gcc/config/darwin.h	2012-02-16 03:21:46.000000000 -0500
++++ gcc-4.7.0/gcc/config/darwin.h	2012-06-08 08:54:25.000000000 -0400
+@@ -356,7 +356,9 @@
+      %{!Zbundle:%{pg:%{static:-lgcrt0.o}				    \
+                      %{!static:%{object:-lgcrt0.o}			    \
+                                %{!object:%{preload:-lgcrt0.o}		    \
+-                                 %{!preload:-lgcrt1.o %(darwin_crt2)}}}}    \
++                                 %{!preload:-lgcrt1.o                       \
++                                 %:version-compare(>= 10.8 mmacosx-version-min= -no_new_main) \
++                                 %(darwin_crt2)}}}}    \
+                 %{!pg:%{static:-lcrt0.o}				    \
+                       %{!static:%{object:-lcrt0.o}			    \
+                                 %{!object:%{preload:-lcrt0.o}		    \
+@@ -379,7 +381,7 @@
+ #define DARWIN_CRT1_SPEC						\
+   "%:version-compare(!> 10.5 mmacosx-version-min= -lcrt1.o)		\
+    %:version-compare(>< 10.5 10.6 mmacosx-version-min= -lcrt1.10.5.o)	\
+-   %:version-compare(>= 10.6 mmacosx-version-min= -lcrt1.10.6.o)	\
++   %:version-compare(>< 10.6 10.8 mmacosx-version-min= -lcrt1.10.6.o)	\
+    %{fgnu-tm: -lcrttms.o}"
+ 
+ /* Default Darwin ASM_SPEC, very simple.  */
+@@ -414,6 +416,8 @@
+ 
+ #define TARGET_WANT_DEBUG_PUB_SECTIONS true
+ 
++#define TARGET_FORCE_AT_COMP_DIR true
++
+ /* When generating stabs debugging, use N_BINCL entries.  */
+ 
+ #define DBX_USE_BINCL
+diff -uNr gcc/doc/tm.texi gcc/doc/tm.texi
+--- gcc-4.7.0/gcc/doc/tm.texi	2012-01-26 16:48:27.000000000 -0500
++++ gcc-4.7.0/gcc/doc/tm.texi	2012-06-08 08:54:25.000000000 -0400
+@@ -9487,6 +9487,10 @@
+ True if the @code{.debug_pubtypes} and @code{.debug_pubnames} sections should be emitted.  These sections are not used on most platforms, and in particular GDB does not use them.
+ @end deftypevr
+ 
++@deftypevr {Target Hook} bool TARGET_FORCE_AT_COMP_DIR
++True if the @code{DW_AT_comp_dir} attribute should be emitted for each  compilation unit.  This attribute is required for the darwin linker  to emit debug information.
++@end deftypevr
++
+ @deftypevr {Target Hook} bool TARGET_DELAY_SCHED2
+ True if sched2 is not to be run at its normal place.  This usually means it will be run as part of machine-specific reorg.
+ @end deftypevr
+diff -uNr gcc/doc/tm.texi.in gcc/doc/tm.texi.in
+--- gcc-4.7.0/gcc/doc/tm.texi.in	2012-01-26 16:48:27.000000000 -0500
++++ gcc-4.7.0/gcc/doc/tm.texi.in	2012-06-08 08:54:25.000000000 -0400
+@@ -9386,6 +9386,8 @@
+ 
+ @hook TARGET_WANT_DEBUG_PUB_SECTIONS
+ 
++@hook TARGET_FORCE_AT_COMP_DIR
++
+ @hook TARGET_DELAY_SCHED2
+ 
+ @hook TARGET_DELAY_VARTRACK
+diff -uNr gcc/dwarf2out.c gcc/dwarf2out.c
+--- gcc-4.7.0/gcc/dwarf2out.c	2012-06-04 09:24:24.000000000 -0400
++++ gcc-4.7.0/gcc/dwarf2out.c	2012-06-08 08:54:25.000000000 -0400
+@@ -22501,7 +22501,7 @@
+   /* Add the name for the main input file now.  We delayed this from
+      dwarf2out_init to avoid complications with PCH.  */
+   add_name_attribute (comp_unit_die (), remap_debug_filename (filename));
+-  if (!IS_ABSOLUTE_PATH (filename))
++  if (!IS_ABSOLUTE_PATH (filename) || targetm.force_at_comp_dir)
+     add_comp_dir_attribute (comp_unit_die ());
+   else if (get_AT (comp_unit_die (), DW_AT_comp_dir) == NULL)
+     {
+diff -uNr gcc/target.def gcc/target.def
+--- gcc-4.7.0/gcc/target.def	2012-01-26 16:48:27.000000000 -0500
++++ gcc-4.7.0/gcc/target.def	2012-06-08 08:54:25.000000000 -0400
+@@ -2748,6 +2748,13 @@
+  bool, false)
+ 
+ DEFHOOKPOD
++(force_at_comp_dir,
++ "True if the @code{DW_AT_comp_dir} attribute should be emitted for each \
++ compilation unit.  This attribute is required for the darwin linker \
++ to emit debug information.",
++ bool, false)
++
++DEFHOOKPOD
+ (delay_sched2, "True if sched2 is not to be run at its normal place.  \
+ This usually means it will be run as part of machine-specific reorg.",
+ bool, false)
