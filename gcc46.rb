34aa4a9cb2ee7bb478107d1159718652be19cc11
Xiyue Deng <manphiz@gmail.com>
2013-05-04 13:54:13 -0700

    New formula: GCC 4.6
    * Version 4.6.4.
    * Enable graphite loop optimization.
    * Miscellaneous improvements.

---
+++ gcc46.rb
@@ -0,0 +1,139 @@
+require 'formula'
+
+class Gcc46 < Formula
+  def arch
+    if Hardware::CPU.type == :intel
+      if MacOS.prefer_64_bit?
+        'x86_64'
+      else
+        'i686'
+      end
+    elsif Hardware::CPU.type == :ppc
+      if MacOS.prefer_64_bit?
+        'ppc64'
+      else
+        'ppc'
+      end
+    end
+  end
+
+  def osmajor
+    `uname -r`.chomp
+  end
+
+  homepage 'http://gcc.gnu.org'
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.6.4/gcc-4.6.4.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.6.4/gcc-4.6.4.tar.bz2'
+  sha1 '63933a8a5cf725626585dbba993c8b0f6db1335d'
+
+  option 'enable-cxx', 'Build the g++ compiler'
+  option 'enable-fortran', 'Build the gfortran compiler'
+  option 'enable-java', 'Buld the gcj compiler'
+  option 'enable-objc', 'Enable Objective-C language support'
+  option 'enable-objcxx', 'Enable Objective-C++ language support'
+  option 'enable-all-languages', 'Enable all compilers and languages, except Ada'
+  option 'enable-nls', 'Build with native language support (localization)'
+  option 'enable-profiled-build', 'Make use of profile guided optimization when bootstrapping GCC'
+  option 'enable-multilib', 'Build with multilib support'
+
+  depends_on 'gmp4'
+  depends_on 'libmpc08'
+  depends_on 'mpfr2'
+  depends_on 'ppl011'
+  depends_on 'cloog-ppl015'
+  depends_on 'ecj' if build.include? 'enable-java' or build.include? 'enable-all-languages'
+
+  def install
+    # GCC will suffer build errors if forced to use a particular linker.
+    ENV.delete 'LD'
+
+    if build.include? 'enable-all-languages'
+      # Everything but Ada, which requires a pre-existing GCC Ada compiler
+      # (gnat) to bootstrap. GCC 4.6.0 add go as a language option, but it is
+      # currently only compilable on Linux.
+      languages = %w[c c++ fortran java objc obj-c++]
+    else
+      # The C compiler is always built, but additional defaults can be added
+      # here.
+      languages = %w[c]
+
+      languages << 'c++' if build.include? 'enable-cxx'
+      languages << 'fortran' if build.include? 'enable-fortran'
+      languages << 'java' if build.include? 'enable-java'
+      languages << 'objc' if build.include? 'enable-objc'
+      languages << 'obj-c++' if build.include? 'enable-objcxx'
+    end
+
+    # Sandbox the GCC lib, libexec and include directories so they don't wander
+    # around telling small children there is no Santa Claus. This results in a
+    # partially keg-only brew following suggestions outlined in the "How to
+    # install multiple versions of GCC" section of the GCC FAQ:
+    #     http://gcc.gnu.org/faq.html#multiple
+    gcc_prefix = prefix + 'gcc'
+
+    args = [
+      "--build=#{arch}-apple-darwin#{osmajor}",
+      # Sandbox everything...
+      "--prefix=#{gcc_prefix}",
+      # ...except the stuff in share...
+      "--datarootdir=#{share}",
+      # ...and the binaries...
+      "--bindir=#{bin}",
+      # ...which are tagged with a suffix to distinguish them.
+      "--enable-languages=#{languages.join(',')}",
+      "--program-suffix=-#{version.to_s.slice(/\d\.\d/)}",
+      "--with-gmp=#{Formula.factory('gmp4').opt_prefix}",
+      "--with-mpfr=#{Formula.factory('mpfr2').opt_prefix}",
+      "--with-mpc=#{Formula.factory('libmpc08').opt_prefix}",
+      "--with-ppl=#{Formula.factory('ppl011').opt_prefix}",
+      "--with-cloog=#{Formula.factory('cloog-ppl015').opt_prefix}",
+      "--with-system-zlib",
+      "--enable-libstdcxx-time=yes",
+      "--enable-stage1-checking",
+      "--enable-checking=release",
+      "--enable-plugin",
+      "--enable-lto",
+      # a no-op unless --HEAD is built because in head warnings will raise errs.
+      "--disable-werror"
+    ]
+
+    args << '--disable-nls' unless build.include? 'enable-nls'
+
+    if build.include? 'enable-java' or build.include? 'enable-all-languages'
+      args << "--with-ecj-jar=#{Formula.factory('ecj').opt_prefix}/share/java/ecj.jar"
+    end
+
+    if build.include? 'enable-multilib'
+      args << '--enable-multilib'
+    else
+      args << '--disable-multilib'
+    end
+
+    mkdir 'build' do
+      unless MacOS::CLT.installed?
+        # For Xcode-only systems, we need to tell the sysroot path.
+        # 'native-system-header's will be appended
+        args << "--with-native-system-header-dir=/usr/include"
+        args << "--with-sysroot=#{MacOS.sdk_path}"
+      end
+
+      system '../configure', *args
+
+      if build.include? 'enable-profiled-build'
+        # Takes longer to build, may bug out. Provided for those who want to
+        # optimise all the way to 11.
+        system 'make profiledbootstrap'
+      else
+        system 'make bootstrap'
+      end
+
+      # At this point `make check` could be invoked to run the testsuite. The
+      # deja-gnu and autogen formulae must be installed in order to do this.
+
+      system 'make install'
+
+      # Remove conflicting manpages in man7
+      man7.rmtree
+    end
+  end
+end
