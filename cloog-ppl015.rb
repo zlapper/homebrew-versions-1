22a32c886cee88c4c5af77b9caffb5dd2c69d2bb
Xiyue Deng <manphiz@gmail.com>
2013-05-05 03:21:17 -0700

    Add cloog-ppl.
    * Tracking cloog-ppl 0.15.11 in GCC infrastructure.

---
+++ cloog-ppl015.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class CloogPpl015 < Formula
+  homepage 'http://repo.or.cz/w/cloog-ppl.git'
+  url 'ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-ppl-0.15.11.tar.gz'
+  sha1 '42fa476a79a1d52da41608a946dcb47c70f7e3b9'
+
+  depends_on 'gmp4'
+  depends_on 'ppl011'
+
+  def install
+    gmp4 = Formula.factory 'gmp4'
+    ppl011 = Formula.factory 'ppl011'
+
+    args = [
+      "--prefix=#{prefix}",
+      "--with-gmp=#{gmp4.opt_prefix}",
+      "--with-ppl=#{ppl011.opt_prefix}"
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+end
