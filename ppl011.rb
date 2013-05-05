1d240ec4fa2db94db6439d8cb7fe4bd0e5445ede
Xiyue Deng <manphiz@gmail.com>
2013-05-05 03:09:45 -0700

    ppl 0.11
    Closes #118.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ ppl011.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Ppl011 < Formula
+  homepage 'http://bugseng.com/products/ppl/'
+  url 'ftp://gcc.gnu.org/pub/gcc/infrastructure/ppl-0.11.tar.gz'
+  sha1 'b3b12de9bcd156ed9254f76259835f40e162afc8'
+
+  keg_only 'Conflicts with ppl in main repository.'
+
+  depends_on 'gmp4'
+
+  def install
+    gmp4 = Formula.factory 'gmp4'
+
+    args = [
+      "--prefix=#{prefix}",
+      "--disable-dependency-tracking",
+      "--enable-optimization=sspeed",
+      "--with-gmp=#{gmp4.opt_prefix}"
+    ]
+
+    system "./configure", *args
+    system "make install"
+  end
+end
