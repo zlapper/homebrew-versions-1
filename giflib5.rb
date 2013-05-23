7172a593a0d0b64adbece569f9995a25d74b2ff3
Xiyue Deng <manphiz@gmail.com>
2013-05-23 03:36:00 -0700

    More giflib5 tweaks.
    * Make it keg_only to avoid conflicts with giflib in main repository.
    * Drop unrecognized "--disable-debug" configure option.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

--- giflib5.rb
+++ giflib5.rb
@@ -5,8 +5,10 @@ class Giflib5 < Formula
   url 'http://downloads.sourceforge.net/project/giflib/giflib-5.x/giflib-5.0.4.tar.bz2'
   sha1 'af3fdf84e2b9ac5c18e7102835a92e2066c7c9f1'
 
+  keg_only "Conflicts with giflib in main repository."
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
   end
 end
