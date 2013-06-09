37e621e238141451dd30dc17b7f999cab435105b
Adam Vandenberg <flangy@gmail.com>
2013-06-08 22:21:42 -0700

    libotr 4.0.0
    Version 4 isn't compatible with things in core that use 3.2.x.

---
+++ libotr4.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Libotr4 < Formula
+  homepage 'http://www.cypherpunks.ca/otr/'
+  url 'http://www.cypherpunks.ca/otr/libotr-4.0.0.tar.gz'
+  sha1 '8865e9011b8674290837afcf7caf90c492ae09cc'
+
+  depends_on 'libgcrypt'
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make install"
+  end
+end
