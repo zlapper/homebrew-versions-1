56939ee67cf039659327de96f0338a3a6be0dbaa
Christopher Jones <christopherdavidjones@gmail.com>
2013-06-11 18:31:19 -0700

    node 0.8.24
    Closes #144.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- node08.rb
+++ node08.rb
@@ -42,8 +42,8 @@ end
 
 class Node08 < Formula
   homepage 'http://nodejs.org/'
-  url 'http://nodejs.org/dist/v0.8.23/node-v0.8.23.tar.gz'
-  sha1 '13772b9c1060aae5441f11cd3fdbe4e4791612b9'
+  url 'http://nodejs.org/dist/v0.8.24/node-v0.8.24.tar.gz'
+  sha1 '4035c547a98a9e9b9e7a036abe84f19c78220b8f'
 
   option 'enable-debug', 'Build with debugger hooks'
   option 'without-npm', 'npm will not be installed'
