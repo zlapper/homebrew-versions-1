3f92a5e120f146ba677360f6374495e1f0ebfff0
Mario Kleiner <mario.kleiner@tuebingen.mpg.de>
2013-07-11 03:34:57 +0200

    Add formula for gst-ffmpeg010
    The GStreamer ffmpeg plugins v0.10.13 from the good old 0.10 series.
    Resurrected from the last formula in main Homebrew.
    
    Closes #285.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ gst-ffmpeg010.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class GstFfmpeg010 < Formula
+  homepage 'http://gstreamer.freedesktop.org/'
+  url 'http://gstreamer.freedesktop.org/src/gst-ffmpeg/gst-ffmpeg-0.10.13.tar.bz2'
+  sha1 '8de5c848638c16c6c6c14ce3b22eecd61ddeed44'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'gettext'
+  depends_on 'homebrew/versions/gst-plugins-base010'
+
+  def install
+    system './configure', "--prefix=#{prefix}",
+                          "--with-ffmpeg-extra-configure=--cc=#{ENV.cc}",
+                          '--disable-dependency-tracking'
+    system "make install"
+  end
+end
