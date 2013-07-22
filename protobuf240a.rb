940a3ea9de07482e209b0facc9dfe10beea235ee
Jeff Kolesky <jeff@kolesky.com>
2013-07-22 13:20:04 -0700

    Adds version 2.4.0a of Protocol Buffers
    This was needed to build the trunk of Hadoop 2.0.0
    
    Closes #170.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

---
+++ protobuf240a.rb
@@ -0,0 +1,37 @@
+require 'formula'
+
+class Protobuf240a < Formula
+  homepage 'http://code.google.com/p/protobuf/'
+  url 'http://protobuf.googlecode.com/files/protobuf-2.4.0a.tar.bz2'
+  sha1 '5816b0dd686115c3d90c3beccf17fd89432d3f07'
+
+  keg_only 'Conflicts with protobuf in main repository.'
+
+  option :universal
+
+  fails_with :llvm do
+    build 2334
+  end
+
+  def install
+    # Don't build in debug mode. See:
+    # https://github.com/mxcl/homebrew/issues/9279
+    # http://code.google.com/p/protobuf/source/browse/trunk/configure.ac#61
+    ENV.prepend 'CXXFLAGS', '-DNDEBUG'
+    ENV.universal_binary if build.universal?
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-zlib"
+    system "make"
+    system "make install"
+
+    # Install editor support and examples
+    doc.install %w( editors examples )
+  end
+
+  def caveats; <<-EOS.undent
+    Editor support and examples have been installed to:
+      #{doc}
+    EOS
+  end
+end
