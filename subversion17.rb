5b40c923bbb29902f98eae5add8e5a951adbf496
Xiyue Deng <manphiz@gmail.com>
2013-07-30 21:26:46 -0700

    subversion17: restore dynamic link, and add archive as mirror for backup.

--- subversion17.rb
+++ subversion17.rb
@@ -2,7 +2,8 @@ require 'formula'
 
 class Subversion17 < Formula
   homepage 'http://subversion.apache.org/'
-  url 'http://apache.fayea.com/apache-mirror/subversion/subversion-1.7.11.tar.bz2'
+  url 'http://www.apache.org/dyn/closer.cgi?path=subversion/subversion-1.7.11.tar.bz2'
+  mirror 'http://archive.apache.org/dist/subversion/subversion-1.7.11.tar.bz2'
   sha1 'd82e187803043b74c072cd5a861ac02e4a027684'
 
   option :universal
