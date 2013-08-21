3549b3874d452fa5c41683371f3073d34bca8c02
Jeff Wear <jeff@inkling.com>
2013-08-20 19:51:02 -0700

    appledoc 2.1
    * Add Appledoc 2.1 to continue to support projects with undocumented
      enumerated and bitmask types.
    
    * This is identical to the last version of homebrew/appledoc that
      referenced 2.1 (mxcl/homebrew@33aa810) except for the comments at top
      and the replacement of `def test` with a block-style test (noting that
      that modification was made to the last version of Appledoc in
      homebrew-versions (ec73b7bbf3)).
    
    Closes #190.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

---
+++ appledoc21.rb
@@ -0,0 +1,38 @@
+require 'formula'
+
+# The 2.2 series introduces support for documenting enumerated and
+# bitmask types, but will emit warnings on encountering undocumented
+# instances of those types.  2.1 (build 840) is provided to allow
+# projects to successfully build with undocumented enumerated and
+# bitmask types.
+class Appledoc21 < Formula
+  homepage 'http://appledoc.gentlebytes.com/'
+  url "https://github.com/tomaz/appledoc/archive/v2.1.tar.gz"
+  sha1 'c30675e340d2ae1334e3d9254701de6f40d6658c'
+
+  keg_only %{
+This formula is keg-only to avoid conflicts with the core Appledoc formula.
+The executable installed by this formula may be invoked explicitly,
+or (if it is the only version installed) linked after it is installed.
+  }
+
+  depends_on :xcode
+  depends_on :macos => :lion
+
+  def install
+    system "xcodebuild", "-project", "appledoc.xcodeproj",
+                         "-target", "appledoc",
+                         "-configuration", "Release",
+                         "clean", "install",
+                         "SYMROOT=build",
+                         "DSTROOT=build",
+                         "INSTALL_PATH=/bin",
+                         "OTHER_CFLAGS='-DCOMPILE_TIME_DEFAULT_TEMPLATE_PATH=@\"#{prefix}/Templates\"'"
+    bin.install "build/bin/appledoc"
+    prefix.install "Templates/"
+  end
+
+  test do
+    system "#{bin}/appledoc", "--version"
+  end
+end
