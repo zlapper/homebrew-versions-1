395ec754672a7434517709e92f16dad750c92471
Bozo Dragojevic <bozzo.dragojevic@gmail.com>
2013-09-09 13:44:28 +0200

    log4cplus10: 1.0.4.3 - fix toolchain issue
    log4cplus 1.0.4 doesnt build for me cleanly, but 1.0.4.3 does
    log4cplus is at 1.1.1 already so mark logcplus10 as keg_only
    
    Closes #202.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

---
+++ log4cplus10.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Log4cplus10 < Formula
+  url 'http://sourceforge.net/projects/log4cplus/files/log4cplus-stable/1.0.4/log4cplus-1.0.4.3.tar.bz2'
+  homepage 'http://log4cplus.sourceforge.net/'
+  sha1 '917d244f7f3d58a5fff35e3eef7fff9c74e9409b'
+
+  keg_only "so it can live alongside current version of log4cplus"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
