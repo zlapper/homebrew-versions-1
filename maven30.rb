8255a53de8db17985e0685cb33d887675821137e
Ealden Esto E. Escañan <ealden@escanan.com>
2013-09-18 13:48:32 +0800

    Maven 3.0.5
    This is the same formula in Homebrew before it was replaced by the newer
    3.1.0 release.
    
    There seems to be an outstanding issue in Maven with regards to their
    migration from Sonatype Aether to Eclipse Aether, which causes projects
    to not build with 3.1.0.
    
    https://cwiki.apache.org/confluence/display/MAVEN/AetherClassNotFound
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>
    
    Closes #209.

---
+++ maven30.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class Maven30 < Formula
+  homepage 'http://maven.apache.org/'
+  url 'http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz'
+  sha1 'aecc0d3d67732939c0056d4a0d8510483ee1167e'
+
+  def install
+    # Remove windows files
+    rm_f Dir["bin/*.bat"]
+
+    # Fix the permissions on the global settings file.
+    chmod 0644, Dir["conf/settings.xml"]
+
+    prefix.install_metafiles
+    libexec.install Dir['*']
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def cavets; <<-EOS.undent
+    WARNING: This older version will conflict with Maven if installed at the
+    same time.
+    EOS
+  end
+end
