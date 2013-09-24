c789ccd8dda0ba01e63c085f7828781126998380
Mike McQuaid <mike@mikemcquaid.com>
2013-09-25 00:20:58 +0100

    llvm-gcc28: fix homepage.

--- llvm-gcc28.rb
+++ llvm-gcc28.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class LlvmGcc28 < Formula
-  homepage 'http://llvm.org/docs/CommandGuide/html/llvmgcc.html'
+  homepage 'http://llvm.org/releases/2.8/docs/CommandGuide/html/llvmgcc.html'
   url 'http://llvm.org/releases/2.8/llvm-gcc-4.2-2.8-x86_64-apple-darwin10.tar.gz'
   version '2.8'
   sha1 'f387a5f774c36cd96ab378f919c5861351110941'
