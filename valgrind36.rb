c8b21f46e6ce19ea64bc87bd07765350eb3792fa
Adam Vandenberg <flangy@gmail.com>
2013-09-23 20:17:16 -0700

    valgrind: add leopard version

---
+++ valgrind36.rb
@@ -0,0 +1,42 @@
+require 'formula'
+
+class Valgrind36 < Formula
+  homepage 'http://www.valgrind.org/'
+  url "http://valgrind.org/downloads/valgrind-3.6.1.tar.bz2"
+  sha1 "6116ddca2708f56e0a2851bdfbe88e01906fa300"
+
+  # Valgrind needs vcpreload_core-*-darwin.so to have execute permissions.
+  # See #2150 for more information.
+  skip_clean 'lib/valgrind'
+
+  def patches
+    # 1: For Xcode-only systems, we have to patch hard-coded paths, use xcrun &
+    #    add missing CFLAGS. See: https://bugs.kde.org/show_bug.cgi?id=295084
+    # 2: Fix for 10.7.4 w/XCode-4.5, duplicate symbols. Reported upstream in
+    #    https://bugs.kde.org/show_bug.cgi?id=307415
+    p = []
+    p << 'https://gist.github.com/raw/3784836/f046191e72445a2fc8491cb6aeeabe84517687d9/patch1.diff' unless MacOS::CLT.installed?
+    p << 'https://gist.github.com/raw/3784930/dc8473c0ac5274f6b7d2eb23ce53d16bd0e2993a/patch2.diff' if MacOS.version == :lion
+    return p
+  end
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+    if MacOS.prefer_64_bit?
+      args << "--enable-only64bit" << "--build=amd64-darwin"
+    else
+      args << "--enable-only32bit"
+    end
+
+    system "./configure", *args
+    system 'make'
+    system "make install"
+  end
+
+  def test
+    system "#{bin}/valgrind", "ls", "-l"
+  end
+end
