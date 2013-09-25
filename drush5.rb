dae9d2743cf4fafb342ddc02486f68ab96903543
Jason Huebsch <jasonh@uoregon.edu>
2013-09-25 13:57:11 -0700

    drush5 5.9 (new)
    * Add version 5 of drush.
    
    Closes #217.
    
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

---
+++ drush5.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Drush5 < Formula
+  homepage 'http://drupal.org/project/drush'
+  url 'http://ftp.drupal.org/files/projects/drush-7.x-5.9.tar.gz'
+  sha1 '12533dbc7a18f1fef79a1853a8fdb88171f4fed8'
+
+  def install
+    libexec.install Dir['*']
+    bin.install_symlink libexec+'drush'
+  end
+end
