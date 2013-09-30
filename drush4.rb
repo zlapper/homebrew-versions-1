57d0916c53e9780eedfb2d09b7790448ff482c8b
Jason Huebsch <jhuebsch@gmail.com>
2013-09-29 22:44:55 -0700

    Made drush version 4 and 5 keg_only.
    Signed-off-by: Xiyue Deng <manphiz@gmail.com>

--- drush4.rb
+++ drush4.rb
@@ -5,6 +5,8 @@ class Drush4 < Formula
   url 'http://ftp.drupal.org/files/projects/drush-7.x-4.6.tar.gz'
   sha1 '51d7a7743342cb3dabb201b4c5433ab3da06fb40'
 
+  keg_only "Conflicts with drush in main repository."
+
   def install
     libexec.install Dir['*']
     bin.install_symlink libexec+'drush'
--- drush5.rb
+++ drush5.rb
@@ -5,6 +5,8 @@ class Drush5 < Formula
   url 'http://ftp.drupal.org/files/projects/drush-7.x-5.9.tar.gz'
   sha1 '12533dbc7a18f1fef79a1853a8fdb88171f4fed8'
 
+  keg_only "Conflicts with drush in main repository."
+
   def install
     libexec.install Dir['*']
     bin.install_symlink libexec+'drush'
