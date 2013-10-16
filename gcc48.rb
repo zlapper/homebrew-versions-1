c1fb34449e3df1209a6d52b68d3783172c330c74
Xiyue Deng <manphiz@gmail.com>
2013-10-16 11:45:38 -0700

    gcc48 4.8.2

--- gcc48.rb
+++ gcc48.rb
@@ -22,8 +22,8 @@ class Gcc48 < Formula
   end
 
   homepage 'http://gcc.gnu.org'
-  url 'ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.8.1/gcc-4.8.1.tar.bz2'
-  sha1 '4e655032cda30e1928fcc3f00962f4238b502169'
+  url 'ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.8.2/gcc-4.8.2.tar.bz2'
+  sha1 '810fb70bd721e1d9f446b6503afe0a9088b62986'
 
   head 'svn://gcc.gnu.org/svn/gcc/branches/gcc-4_8-branch'
 
