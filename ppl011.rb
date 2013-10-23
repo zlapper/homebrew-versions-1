119cc95263f460a041f3cc31fec0d8c0f6bcb699
Xiyue Deng <manphiz@gmail.com>
2013-10-23 14:25:48 -0700

    ppl011: track correct tarball.

--- ppl011.rb
+++ ppl011.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Ppl011 < Formula
   homepage 'http://bugseng.com/products/ppl/'
   # Track gcc infrastructure releases.
-  url 'http://bugseng.com/products/ppl/download/ftp/releases/0.11/ppl-0.11.tar.bz2'
+  url 'http://bugseng.com/products/ppl/download/ftp/releases/0.11/ppl-0.11.tar.gz'
   mirror 'ftp://gcc.gnu.org/pub/gcc/infrastructure/ppl-0.11.tar.gz'
   sha1 'b3b12de9bcd156ed9254f76259835f40e162afc8'
 
