a6716851dc70e35da585832cdbb4c2be2efee93b
Mike McQuaid <mike@mikemcquaid.com>
2013-11-14 22:59:33 +0000

    jenkins-lts: enable TLS mail connections.

--- jenkins-lts.rb
+++ jenkins-lts.rb
@@ -23,6 +23,7 @@ class JenkinsLts < Formula
         <key>ProgramArguments</key>
         <array>
           <string>/usr/bin/java</string>
+          <string>-Dmail.smtp.starttls.enable=true</string>
           <string>-jar</string>
           <string>#{opt_prefix}/libexec/jenkins.war</string>
           <string>--httpListenAddress=127.0.0.1</string>
