1163b71072dbe70c7c98a84f71a598d4b3715548
Jack Nagel <jacknagel@gmail.com>
2013-11-27 19:47:17 -0600

    glfw: use upstream patch to fix universal build
    Fixes #256.

--- glfw3.rb
+++ glfw3.rb
@@ -14,6 +14,7 @@ class Glfw3 < Formula
 
   def patches
     # make library name consistent
+    # Fix conflicting typedefs
     DATA
   end
 
@@ -54,3 +55,21 @@ index
  
  #--------------------------------------------------------------------
  # Add subdirectories
+diff --git a/deps/GL/glext.h b/deps/GL/glext.h
+index 6cd79a2..acb4698 100644
+--- a/deps/GL/glext.h
++++ b/deps/GL/glext.h
+@@ -4130,8 +4130,13 @@
+ 
+ #ifndef GL_ARB_vertex_buffer_object
+ #define GL_ARB_vertex_buffer_object 1
++#if defined(__APPLE__)
++typedef long GLsizeiptrARB;
++typedef long GLintptrARB;
++#else
+ typedef ptrdiff_t GLsizeiptrARB;
+ typedef ptrdiff_t GLintptrARB;
++#endif
+ #define GL_BUFFER_SIZE_ARB                0x8764
+ #define GL_BUFFER_USAGE_ARB               0x8765
+ #define GL_ARRAY_BUFFER_ARB               0x8892
