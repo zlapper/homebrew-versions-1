ee27cb77c6ba033c5f7e76a6419792a17633cd62
Vaibhav Bajpai <contact@vaibhavbajpai.com>
2013-12-29 00:23:31 +0100

    json-c010 0.10
    Closes #320.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ json-c010.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class JsonC010 < Formula
+  homepage 'https://github.com/json-c/json-c/wiki'
+  url 'https://github.com/downloads/json-c/json-c/json-c-0.10.tar.gz'
+  sha1 'f90f643c8455da21d57b3e8866868a944a93c596'
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+
+    # The Makefile forgets to install this header.
+    (include/'json').install 'json_object_iterator.h'
+  end
+end
