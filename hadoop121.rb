76bc45d5a3b643c507aebdbed0824059fe71831d
Kashif Rasul <kashif.rasul@gmail.com>
2014-01-28 11:44:02 +0100

    hadoop 1.2.1
    Closes #342.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ hadoop121.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class Hadoop121 < Formula
+  homepage 'http://hadoop.apache.org/'
+  url 'http://www.apache.org/dyn/closer.cgi?path=hadoop/core/hadoop-1.2.1/hadoop-1.2.1.tar.gz'
+  sha1 'b07b88ca658dc9d338aa84f5c68c809eb7c70964'
+
+  keg_only "Conflicts with hadoop in core."
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin conf lib webapps contrib]
+    libexec.install Dir['*.jar']
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+    # But don't make rcc visible, it conflicts with Qt
+    (bin/'rcc').unlink
+
+    inreplace "#{libexec}/conf/hadoop-env.sh",
+      "# export JAVA_HOME=/usr/lib/j2sdk1.5-sun",
+      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
+  end
+
+  def caveats; <<-EOS.undent
+    In Hadoop's config file:
+      #{libexec}/conf/hadoop-env.sh
+    $JAVA_HOME has been set to be the output of:
+      /usr/libexec/java_home
+    EOS
+  end
+end
