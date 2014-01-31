1098766f507b7affaba9917226994df8bf77e6c4
Adam Vandenberg <flangy@gmail.com>
2014-01-30 20:07:49 -0800

    phantomjs 1.8.2: fix requirement
    Closes #346.

--- phantomjs182.rb
+++ phantomjs182.rb
@@ -5,7 +5,7 @@ class Phantomjs182 < Formula
   url 'http://phantomjs.googlecode.com/files/phantomjs-1.8.2-macosx.zip'
   sha1 '904a89cd5df585e69cba20c6502e5c6d32b3be86'
 
-  depends_on :mac_os => :snow_leopard
+  depends_on :macos => :snow_leopard
 
   def install
     bin.install 'bin/phantomjs'
