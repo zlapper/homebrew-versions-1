d349658a1dc6f22d8a3eb06d2fce9e60c0b0b0ad
Adam Vandenberg <flangy@gmail.com>
2014-01-30 20:08:15 -0800

    phantomjs 1.7.0: use requirement

--- phantomjs17.rb
+++ phantomjs17.rb
@@ -1,20 +1,11 @@
 require 'formula'
 
-class SnowLeopardOrNewer < Requirement
-  fatal true
-  satisfy MacOS.version >= :snow_leopard
-
-  def message
-    "PhantomJS requires Mac OS X 10.6 (Snow Leopard) or newer."
-  end
-end
-
 class Phantomjs17 < Formula
   homepage 'http://www.phantomjs.org/'
   url 'http://phantomjs.googlecode.com/files/phantomjs-1.7.0-macosx.zip'
   sha1 'de9ed8092d7fd5095447ada2cf96efb6c949b359'
 
-  depends_on SnowLeopardOrNewer
+  depends_on :macos => :snow_leopard
 
   def install
     bin.install 'bin/phantomjs'
