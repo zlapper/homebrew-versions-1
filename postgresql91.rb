89a0cc62897ae1ac16e6f97da18919452d78a6b8
James Blanding <james@basicsgroup.com>
2014-02-26 12:03:39 -0500

    postgresql 9.1.12
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- postgresql91.rb
+++ postgresql91.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Postgresql91 < Formula
   homepage 'http://www.postgresql.org/'
-  url 'http://ftp.postgresql.org/pub/source/v9.1.11/postgresql-9.1.11.tar.bz2'
-  sha256 'ccbc35aae1490ee5878b97a6aea48dad7465cdad296b380542e4303b68cc6f74'
+  url 'http://ftp.postgresql.org/pub/source/v9.1.12/postgresql-9.1.12.tar.bz2'
+  sha256 '0b267ebab5feb39ad6ef945b9588787886e7f7e5284467921d18cc7b76bcb383'
 
   depends_on 'readline'
   depends_on 'libxml2' if MacOS.version == :leopard
