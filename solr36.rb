4fa8b9b3c8f37667f1d993a53a6b9da383d87179
Arne Jørgensen <arne@arnested.dk>
2014-02-28 13:33:02 +0100

    solr: add startup plist
    Closes #366.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- solr36.rb
+++ solr36.rb
@@ -18,6 +18,9 @@ class Solr36 < Formula
   def install
     libexec.install Dir['*']
     (bin+'solr36').write script
+    (var+'logs'+name).mkpath
+    (etc+name).mkpath
+    (etc+name).install Dir[libexec+'example/multicore'] unless (etc+name+'multicore').exist?
   end
 
   def caveats; <<-EOS.undent
@@ -26,6 +29,33 @@ class Solr36 < Formula
 
     See the solr homepage for more setup information:
       brew home solr
+    EOS
+  end
+
+  def plist; <<-EOS.undent
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>/usr/bin/java</string>
+            <string>-Djetty.home=#{libexec}/example</string>
+            <string>-Djetty.logs=#{var}/logs/#{name}</string>
+            <string>-Dsolr.solr.home=#{etc+name+'multicore'}</string>
+            <string>-jar</string>
+            <string>#{libexec}/example/start.jar</string>
+          </array>
+          <key>ServiceDescription</key>
+          <string>Solr</string>
+          <key>WorkingDirectory</key>
+          <string>#{HOMEBREW_PREFIX}</string>
+          <key>RunAtLoad</key>
+          <true/>
+      </dict>
+      </plist>
     EOS
   end
 end
