d162eb1b1d0401c11c43f82488ded358c619afa6
Jack Nagel <jacknagel@gmail.com>
2014-03-03 16:20:59 -0600

    scala29: use resources

--- scala29.rb
+++ scala29.rb
@@ -1,18 +1,5 @@
 require 'formula'
 
-class ScalaDocs < Formula
-  homepage 'http://www.scala-lang.org/'
-  url 'http://www.scala-lang.org/files/archive/scala-docs-2.9.3.zip'
-  sha1 '633a31ca2eb87ce5b31b4f963bdfd1d4157282ad'
-end
-
-class ScalaCompletion < Formula
-  homepage 'http://www.scala-lang.org/'
-  url 'https://raw.github.com/scala/scala-dist/27bc0c25145a83691e3678c7dda602e765e13413/completion.d/2.9.1/scala'
-  version '2.9.1'
-  sha1 'e2fd99fe31a9fb687a2deaf049265c605692c997'
-end
-
 class Scala29 < Formula
   homepage 'http://www.scala-lang.org/'
   url 'http://www.scala-lang.org/files/archive/scala-2.9.3.tgz'
@@ -20,13 +7,23 @@ class Scala29 < Formula
 
   option 'with-docs', 'Also install library documentation'
 
+  resource 'docs' do
+    url 'http://www.scala-lang.org/files/archive/scala-docs-2.9.3.zip'
+    sha1 '633a31ca2eb87ce5b31b4f963bdfd1d4157282ad'
+  end
+
+  resource 'completion' do
+    url 'https://raw.github.com/scala/scala-dist/27bc0c25145a83691e3678c7dda602e765e13413/completion.d/2.9.1/scala'
+    sha1 'e2fd99fe31a9fb687a2deaf049265c605692c997'
+  end
+
   def install
     rm_f Dir["bin/*.bat"]
     doc.install Dir['doc/*']
     man1.install Dir['man/man1/*']
     libexec.install Dir['*']
     bin.install_symlink Dir["#{libexec}/bin/*"]
-    ScalaCompletion.new.brew { (prefix/'etc/bash_completion.d').install 'scala' }
-    ScalaDocs.new.brew { doc.install Dir['*'] } if build.include? 'with-docs'
+    bash_completion.install resource('completion')
+    doc.install resource('docs') if build.with? 'docs'
   end
 end
