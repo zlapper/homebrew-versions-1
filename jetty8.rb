eedb4345c88b042f298e7d96c750dac4ec0c24b7
David Ma <david@nextop.asia>
2014-03-08 00:10:39 +0800

    Jetty 8.1.14
    Closes #370.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- jetty8.rb
+++ jetty8.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Jetty8 < Formula
   homepage 'http://www.eclipse.org/jetty/'
-  url 'http://eclipse.org/downloads/download.php?file=/jetty/8.1.8.v20121106/dist/jetty-distribution-8.1.8.v20121106.tar.gz&r=1'
-  version '8.1.8'
-  sha1 '19f6c1758d5b6d73702c08574062b63195a404b5'
+  url 'http://eclipse.org/downloads/download.php?file=/jetty/8.1.14.v20131031/dist/jetty-distribution-8.1.14.v20131031.tar.gz&r=1'
+  version '8.1.14'
+  sha1 '2c3c3af2cd1e9382373387be9a32bd3c6903566f'
 
   def install
     rm_rf Dir['bin/*.{cmd,bat]}']
