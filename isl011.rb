ed147549e4a147211e725f51f74db55e60941c05
Misty De Meo <mistydemeo@gmail.com>
2014-04-16 09:12:29 -0700

    isl011: add PPC bottles

--- isl011.rb
+++ isl011.rb
@@ -7,6 +7,13 @@ class Isl011 < Formula
   mirror 'ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.11.1.tar.bz2'
   sha1 'd7936929c3937e03f09b64c3c54e49422fa8ddb3'
 
+  bottle do
+    sha1 '49d0c81d3d0e72abe18e171b58e6159122ba07bc' => :tiger_g3
+    sha1 '7500d4495c2da2059b484d96190d8e563b01a357' => :tiger_altivec
+    sha1 '5cf2a19ddde50e65786a2d5957a0d0b20f1db94c' => :leopard_g3
+    sha1 '3c97c65cdacf43c0f310309a9b5f0a242fd7b925' => :leopard_altivec
+  end
+
   keg_only 'Conflicts with isl in main repository.'
 
   depends_on 'gmp4'
