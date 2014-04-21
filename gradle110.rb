083104a5a7e0106c3ca253af9ab4567c4c14446f
Maksym Grebenets <maksym.grebenets@news.com.au>
2014-04-22 09:07:11 +1000

    gradle 1.6, 1.8, 1.10
    Closes #405.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ gradle110.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Gradle110 < Formula
+  homepage 'http://www.gradle.org/'
+  url 'http://services.gradle.org/distributions/gradle-1.10-bin.zip'
+  sha1 'f911beeff25ebbb7b4af3092dd7f0c93019aef17'
+
+  def install
+    libexec.install %w[bin lib]
+    bin.install_symlink libexec+'bin/gradle'
+  end
+end
---
+++ gradle18.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Gradle18 < Formula
+  homepage 'http://www.gradle.org/'
+  url 'http://services.gradle.org/distributions/gradle-1.8-bin.zip'
+  sha1 'f14299582a1ab6c1293a43697ecda4b2673e34b1'
+
+  def install
+    libexec.install %w[bin lib]
+    bin.install_symlink libexec+'bin/gradle'
+  end
+end
