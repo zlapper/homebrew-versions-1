715cf37e7354cf535680433fd1e97541dd536c41
Indrajit Raychaudhuri <irc@indrajit.com>
2014-04-28 14:43:31 -0500

    scala210: scala 2.10.4
    Closes #413.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ scala210.rb
@@ -0,0 +1,54 @@
+require 'formula'
+
+class Scala210 < Formula
+  homepage 'http://www.scala-lang.org/'
+  url 'http://www.scala-lang.org/files/archive/scala-2.10.4.tgz'
+  sha1 '970f779f155719838e81a267a7418a958fd4c13f'
+
+  option 'with-docs', 'Also install library documentation'
+
+  resource 'docs' do
+    url 'http://www.scala-lang.org/files/archive/scala-docs-2.10.4.zip'
+    sha1 '7ad47f9634fd2f452cadf35f7241102207c1a1cc'
+  end
+
+  resource 'completion' do
+    url 'https://raw.github.com/scala/scala-dist/27bc0c25145a83691e3678c7dda602e765e13413/completion.d/2.9.1/scala'
+    sha1 'e2fd99fe31a9fb687a2deaf049265c605692c997'
+  end
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    doc.install Dir['doc/*']
+    man1.install Dir['man/man1/*']
+    libexec.install Dir['*']
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bash_completion.install resource('completion')
+    doc.install resource('docs') if build.with? 'docs'
+
+    # Set up an IntelliJ compatible symlink farm in 'idea'
+    idea = prefix/'idea'
+    idea.install_symlink libexec/'src', libexec/'lib'
+    (idea/'doc/scala-devel-docs').install_symlink doc => 'api'
+  end
+
+  def caveats; <<-EOS.undent
+    To use with IntelliJ, set the Scala home to:
+      #{opt_prefix}/idea
+    EOS
+  end
+
+  test do
+    file = testpath/'hello.scala'
+    file.write <<-EOS.undent
+      object Computer {
+        def main(args: Array[String]) {
+          println(2 + 2)
+        }
+      }
+    EOS
+    output = `'#{bin}/scala' #{file}`
+    assert_equal "4", output.strip
+    assert $?.success?
+  end
+end
