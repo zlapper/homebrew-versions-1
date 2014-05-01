7b9070f7c79d82b908eaa39b5672fc238c2bc609
Nerian <siotopo@gmail.com>
2014-05-01 10:48:05 +0200

    Redis 1.3.10
    Closes #415.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ redis1310.rb
@@ -0,0 +1,64 @@
+require 'formula'
+
+class Redis1310 < Formula
+  homepage 'http://redis.io/'
+  url 'https://github.com/antirez/redis/tarball/v1.3.10'
+  sha1 'e4d22d8b0df0e9d686069a40bff4b91219c68676'
+
+  fails_with :llvm do
+    build 2334
+    cause 'Fails with "reference out of range from _linenoise"'
+  end
+
+  def install
+    # Architecture isn't detected correctly on 32bit Snow Leopard without help
+    ENV["OBJARCH"] = MacOS.prefer_64_bit? ? "-arch x86_64" : "-arch i386"
+
+    # Head and stable have different code layouts
+    src = (buildpath/'src/Makefile').exist? ? buildpath/'src' : buildpath
+    system "make", "-C", src, "CC=#{ENV.cc}"
+
+    %w( redis-benchmark redis-cli redis-server redis-check-dump redis-check-aof ).each { |p|
+      bin.install "#{src}/#{p}" rescue nil
+    }
+
+    %w( run db/redis log ).each { |p| (var+p).mkpath }
+
+    # Fix up default conf file to match our paths
+    inreplace "redis.conf" do |s|
+      s.gsub! "/var/run/redis.pid", "#{var}/run/redis.pid"
+      s.gsub! "dir ./", "dir #{var}/db/redis/"
+    end
+
+    etc.install 'redis.conf' unless (etc/'redis.conf').exist?
+  end
+
+  plist_options :manual => "redis-server #{HOMEBREW_PREFIX}/etc/redis.conf"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_prefix}/bin/redis-server</string>
+          <string>#{etc}/redis.conf</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/redis.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/redis.log</string>
+      </dict>
+    </plist>
+        EOS
+  end
+end
