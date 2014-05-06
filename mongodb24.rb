307faddccd19c1b81887a0129549c25f341f1c8e
Dayyan Lord <dayyan@gmail.com>
2014-05-06 15:30:24 -0700

    MongoDB 2.4.10
    Closes #420.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ mongodb24.rb
@@ -0,0 +1,112 @@
+require 'formula'
+
+class Mongodb24 < Formula
+  homepage 'http://www.mongodb.org/'
+  url 'http://downloads.mongodb.org/src/mongodb-src-r2.4.10.tar.gz'
+  sha1 'faf1f41d45934bcb30684cfed95f5d3c698663a0'
+
+  patch do
+    url 'https://github.com/mongodb/mongo/commit/be4bc7.patch'
+    sha1 '23448cae98f86e65b7248179addb2897fc4ecac5'
+  end
+
+  depends_on 'scons' => :build
+  depends_on 'openssl' => :optional
+
+  def install
+    # When 2.6 is released this conditional can be removed.
+    if MacOS.version < :mavericks
+      option "with-boost", "Compile using installed boost, not the version shipped with mongodb"
+      depends_on "boost" => :optional
+    end
+
+    args = ["--prefix=#{prefix}", "-j#{ENV.make_jobs}"]
+
+    cxx = ENV.cxx
+    if ENV.compiler == :clang && MacOS.version >= :mavericks
+      # When 2.6 is released this cxx hack can be removed
+      # ENV.append "CXXFLAGS", "-stdlib=libstdc++" does not work with scons
+      # so use this hack of appending the flag to the --cxx parameter of the sconscript.
+      # mongodb 2.4 can't build with libc++, but defaults to it on Mavericks
+      cxx += " -stdlib=libstdc++"
+    end
+
+    args << '--64' if MacOS.prefer_64_bit?
+    args << "--cc=#{ENV.cc}"
+    args << "--cxx=#{cxx}"
+
+    # --full installs development headers and client library, not just binaries
+    args << "--full"
+    args << "--use-system-boost" if build.with? "boost"
+
+    if build.with? 'openssl'
+      args << '--ssl'
+      args << "--extrapath=#{Formula["openssl"].opt_prefix}"
+    end
+
+    scons 'install', *args
+
+    (buildpath+"mongod.conf").write mongodb_conf
+    etc.install "mongod.conf"
+
+    (var+'mongodb').mkpath
+    (var+'log/mongodb').mkpath
+  end
+
+  def mongodb_conf; <<-EOS.undent
+    # Store data in #{var}/mongodb instead of the default /data/db
+    dbpath = #{var}/mongodb
+
+    # Append logs to #{var}/log/mongodb/mongo.log
+    logpath = #{var}/log/mongodb/mongo.log
+    logappend = true
+
+    # Only accept local connections
+    bind_ip = 127.0.0.1
+    EOS
+  end
+
+  plist_options :manual => "mongod --config #{HOMEBREW_PREFIX}/etc/mongod.conf"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/mongod</string>
+        <string>--config</string>
+        <string>#{etc}/mongod.conf</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <false/>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/mongodb/output.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/mongodb/output.log</string>
+      <key>HardResourceLimits</key>
+      <dict>
+        <key>NumberOfFiles</key>
+        <integer>1024</integer>
+      </dict>
+      <key>SoftResourceLimits</key>
+      <dict>
+        <key>NumberOfFiles</key>
+        <integer>1024</integer>
+      </dict>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/mongod", '--sysinfo'
+  end
+end
