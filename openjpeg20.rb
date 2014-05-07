55fbe1fa5909914dd98babcac222ab78595f68b8
Larry Shaffer <larrys@dakotacarto.com>
2014-05-07 09:41:32 -0600

    openjpeg20 2.0.0
    Closes #419.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ openjpeg20.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class Openjpeg20 < Formula
+  homepage "http://www.openjpeg.org/"
+  url "http://openjpeg.googlecode.com/files/openjpeg-2.0.0.tar.gz"
+  sha1 "0af78ab2283b43421458f80373422d8029a9f7a7"
+
+  depends_on "cmake" => :build
+  depends_on "little-cms2"
+  depends_on "libtiff"
+  depends_on :libpng
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
