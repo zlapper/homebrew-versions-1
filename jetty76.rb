dc59b8f5118fbe80adea7034930fa2df69c4c106
David Ma <david@nextop.asia>
2014-05-16 12:42:03 +0800

    jetty 7.6.15.v20140411
    Closes #426.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- jetty76.rb
+++ jetty76.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Jetty76 < Formula
   homepage 'http://www.eclipse.org/jetty/'
-  url 'http://download.eclipse.org/jetty/stable-7/dist/jetty-distribution-7.6.9.v20130131.tar.gz'
-  version '7.6.9'
-  sha1 '2aaad044f9fd049cf3795268f209297911c64e53'
+  url 'http://download.eclipse.org/jetty/stable-7/dist/jetty-distribution-7.6.15.v20140411.tar.gz'
+  version '7.6.15'
+  sha1 '732bfdb9570be6758d3bd5ca5bed9012e96348ac'
 
   def install
     rm_rf Dir['bin/*.{cmd,bat]}']
