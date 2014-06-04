b99651e19fc7cc265e3b662a18775b867d766d1f
Tony <zearin@gonk.net>
2014-06-04 14:21:56 -0400

    ruby182: Fix `--with-suffix`
    Closes #452.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- ruby182.rb
+++ ruby182.rb
@@ -9,7 +9,7 @@ class Ruby182 < Formula
   keg_only :provided_by_osx
 
   option :universal
-  option 'with-suffix', 'Suffix commands with "20"'
+  option 'with-suffix', 'Suffix commands with "182"'
   option 'with-doc', 'Install documentation'
   option 'with-tcltk', 'Install with Tcl/Tk support'
 
@@ -36,7 +36,7 @@ class Ruby182 < Formula
     ENV.prepend 'LDFLAGS', '-L.'
 
     args = %W[--prefix=#{prefix} --mandir=#{man} --enable-shared]
-    args << "--program-suffix=20" if build.with? "suffix"
+    args << "--program-suffix=182" if build.with? "suffix"
     args << "--with-arch=#{Hardware::CPU.universal_archs.join(',')}" if build.universal?
     args << "--with-out-ext=tk" if build.without? "tcltk"
     args << "--disable-install-doc" if build.without? "doc"
