0b8a360da4ad0cd403b579d45743953392dcf857
Reinaldo Junior <juniorz@gmail.com>
2014-07-23 22:23:18 -0300

    Gradle 1.12
    Closes #486.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ gradle112.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Gradle112 < Formula
+  homepage 'http://www.gradle.org/'
+  url 'https://services.gradle.org/distributions/gradle-1.12-bin.zip'
+  sha1 '4fee5af4ce5c1214bdbd5e14ef6adda9929f589f'
+
+  def install
+    libexec.install %w[bin lib]
+    bin.install_symlink libexec+'bin/gradle'
+  end
+end
