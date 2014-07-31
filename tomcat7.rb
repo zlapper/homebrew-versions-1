36873cad74b1105a8edd02ba3dddb04cfb56439c
asaph <ace@dontletgo.com>
2014-07-31 13:41:51 -0700

    tomcat 7.0.55
    Closes #492.
    Closes #493.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- tomcat7.rb
+++ tomcat7.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class Tomcat7 < Formula
   homepage "http://tomcat.apache.org/"
-  url "http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.54/bin/apache-tomcat-7.0.54.tar.gz"
-  sha1 "b0db037619c5c10cbe8d17f7a1492fd759fa5805"
+  url "http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55.tar.gz"
+  sha1 "4e23f1e3d6d24e0affebe78f6f5c5d5a49eb4e37"
 
   option "with-fulldocs", "Install full documentation locally"
 
   resource "fulldocs" do
-    url "http://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.54/bin/apache-tomcat-7.0.54-fulldocs.tar.gz"
-    version "7.0.54"
-    sha1 "2b2dc6835ebcaf12705cd3e60c40114e498651f0"
+    url "http://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55-fulldocs.tar.gz"
+    version "7.0.55"
+    sha1 "e5f6a9c882da489e9b75110bb5ca25e07070a397"
   end
 
   # Keep log folders
