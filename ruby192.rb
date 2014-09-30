b23ced5b7a274d7c83bc7d0c90c10a2df84f8c61
Jack Nagel <jacknagel@gmail.com>
2014-09-30 17:36:44 -0500

    ruby192 1.9.2-p330

--- ruby192.rb
+++ ruby192.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Ruby192 < Formula
   homepage 'http://www.ruby-lang.org/en/'
-  url 'http://cache.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p320.tar.bz2'
-  sha256 '6777f865cfa21ffdc167fcc4a7da05cb13aab1bd9e59bfcda82c4b32f75e6b51'
-  revision 1
+  url 'http://cache.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p330.tar.bz2'
+  sha256 '6d3487ea8a86ad0fa78a8535078ff3c7a91ca9f99eff0a6a08e66c6e6bf2040f'
 
   depends_on 'pkg-config' => :build
   depends_on 'readline'
