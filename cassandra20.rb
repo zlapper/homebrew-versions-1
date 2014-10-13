bd99d6d04fa9d131dbd0bce89a3db27ccf7d0930
Dana P'Simer <dana.psimer@weather.com>
2014-10-13 14:09:56 -0400

    cassandra 2.0.x
    Closes #542.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ cassandra20.rb
@@ -0,0 +1,79 @@
+require 'formula'
+
+class Cassandra20 < Formula
+  homepage 'http://cassandra.apache.org'
+  url 'http://www.apache.org/dyn/closer.cgi?path=/cassandra/2.0.9/apache-cassandra-2.0.9-bin.tar.gz'
+  sha1 '49eeb4c1dd518bd9440280a758ad4b129769a78d'
+
+  conflicts_with 'cassandra',
+    :because => "cassandra20 and cassandra install different versions of the same binaries."
+
+  def install
+    (var+"lib/cassandra").mkpath
+    (var+"log/cassandra").mkpath
+    (etc+"cassandra").mkpath
+
+    inreplace "conf/cassandra.yaml", "/var/lib/cassandra", "#{var}/lib/cassandra"
+    inreplace "conf/log4j-server.properties", "/var/log/cassandra", "#{var}/log/cassandra"
+    inreplace "conf/cassandra-env.sh", "/lib/", "/"
+
+    inreplace "bin/cassandra.in.sh" do |s|
+      s.gsub! "CASSANDRA_HOME=\"`dirname \"$0\"`/..\"", "CASSANDRA_HOME=\"#{prefix}\""
+      # Store configs in etc, outside of keg
+      s.gsub! "CASSANDRA_CONF=\"$CASSANDRA_HOME/conf\"", "CASSANDRA_CONF=\"#{etc}/cassandra\""
+      # Jars installed to prefix, no longer in a lib folder
+      s.gsub! "\"$CASSANDRA_HOME\"/lib/*.jar", "\"$CASSANDRA_HOME\"/*.jar"
+      # The jammm Java agent is not in a lib/ subdir either:
+      s.gsub! "JAVA_AGENT=\"$JAVA_AGENT -javaagent:$CASSANDRA_HOME/lib/jamm-", "JAVA_AGENT=\"$JAVA_AGENT -javaagent:$CASSANDRA_HOME/jamm-"
+    end
+
+    rm Dir["bin/*.bat"]
+
+    (etc+"cassandra").install Dir["conf/*"]
+    prefix.install Dir["*.txt", "{bin,interface,javadoc,pylib,lib/licenses}"]
+    prefix.install Dir["lib/*.jar"]
+
+    share.install [bin+'cassandra.in.sh', bin+'stop-server']
+    inreplace Dir["#{bin}/cassandra*", "#{bin}/debug-cql",
+                  "#{bin}/json2sstable", "#{bin}/nodetool",
+                  "#{bin}/sstable*"],
+      /`dirname "?\$0"?`\/cassandra.in.sh/,
+      "#{share}/cassandra.in.sh"
+  end
+
+  def caveats; <<-EOS.undent
+    If you plan to use the CQL shell (cqlsh), you will need the Python CQL library
+    installed. Since Homebrew prefers using pip for Python packages, you can
+    install that using:
+
+      pip install cql
+    EOS
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+
+        <key>Label</key>
+        <string>#{plist_name}</string>
+
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/cassandra</string>
+            <string>-f</string>
+        </array>
+
+        <key>RunAtLoad</key>
+        <true/>
+
+        <key>WorkingDirectory</key>
+        <string>#{var}/lib/cassandra</string>
+      </dict>
+    </plist>
+    EOS
+  end
+end
