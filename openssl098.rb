83849aeab21e8fbead33869f8f921c780f42f63d
Jack Nagel <jacknagel@gmail.com>
2014-10-15 12:16:25 -0500

    openssl 0.9.8zc

--- openssl098.rb
+++ openssl098.rb
@@ -1,20 +1,20 @@
-require 'formula'
+require "formula"
 
 class Openssl098 < Formula
-  homepage 'https://www.openssl.org'
-  url 'https://www.openssl.org/source/openssl-0.9.8zb.tar.gz'
-  sha256 '950e2298237de1697168debd42860bf41ead618e0c03dc9a3a56e23258e435be'
+  homepage "https://www.openssl.org"
+  url "https://www.openssl.org/source/openssl-0.9.8zc.tar.gz"
+  sha256 "461cc694f29e72f59c22e7ea61bf44671a5fc2f8b3fc2eeac89714b7be915881"
 
   keg_only :provided_by_osx
 
   def install
-    args = %W[./Configure
-               --prefix=#{prefix}
-               --openssldir=#{etc}/openssl
-               no-ssl2
-               zlib-dynamic
-               shared
-             ]
+    args = %W[
+      --prefix=#{prefix}
+      --openssldir=#{etc}/openssl
+      no-ssl2
+      zlib-dynamic
+      shared
+    ]
 
     if MacOS.prefer_64_bit?
       args << "darwin64-x86_64-cc" << "enable-ec_nistp_64_gcc_128"
@@ -22,11 +22,11 @@ class Openssl098 < Formula
       args << "darwin-i386-cc"
     end
 
-    system "perl", *args
+    system "perl", "./Configure", *args
 
     ENV.deparallelize # Parallel compilation fails
     system "make"
-    system "make test"
+    system "make", "test"
     system "make", "install", "MANDIR=#{man}", "MANSUFFIX=ssl"
   end
 end
