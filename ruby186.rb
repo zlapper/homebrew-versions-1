f986c0c8c668ec9d54c6221864b2a878bb43089d
Misty De Meo <mistydemeo@gmail.com>
2014-10-28 22:22:09 -0700

    ruby186: further openssl patches
    Fixes #550.

--- ruby186.rb
+++ ruby186.rb
@@ -31,6 +31,7 @@ class Ruby186 < Formula
 
   # First patch backports a compatibility fix from Ruby 1.8.7 for newer OpenSSL versions
   # Second patch fixes the type of a macro, also taken from 1.8.7
+  # Third patch fixes another OpenSSL macro, taken from Ruby 2.1.4
   patch :DATA
 
   def install
@@ -111,3 +112,26 @@ index 1b8f76a..73fdd03 100644
  {						\
      type *t;					\
      int i, num;					\
+
+diff --git a/ext/openssl/ossl_ssl.c b/ext/openssl/ossl_ssl.c
+index c4569c7..d6f8822 100644
+--- a/ext/openssl/ossl_ssl.c
++++ b/ext/openssl/ossl_ssl.c
+@@ -96,13 +96,16 @@ struct {
+     const char *name;
+     SSL_METHOD *(*func)(void);
+ } ossl_ssl_method_tab[] = {
+-#define OSSL_SSL_METHOD_ENTRY(name) { #name, name##_method }
++#define OSSL_SSL_METHOD_ENTRY(name) { #name, (SSL_METHOD *(*)(void))name##_method }
+     OSSL_SSL_METHOD_ENTRY(TLSv1),
+     OSSL_SSL_METHOD_ENTRY(TLSv1_server),
+     OSSL_SSL_METHOD_ENTRY(TLSv1_client),
++#if defined(HAVE_SSLV2_METHOD) && defined(HAVE_SSLV2_SERVER_METHOD) && \
++        defined(HAVE_SSLV2_CLIENT_METHOD)
+     OSSL_SSL_METHOD_ENTRY(SSLv2),
+     OSSL_SSL_METHOD_ENTRY(SSLv2_server),
+     OSSL_SSL_METHOD_ENTRY(SSLv2_client),
++#endif
+     OSSL_SSL_METHOD_ENTRY(SSLv3),
+     OSSL_SSL_METHOD_ENTRY(SSLv3_server),
+     OSSL_SSL_METHOD_ENTRY(SSLv3_client),
