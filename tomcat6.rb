d6333ae7f9f59c3ea57a2800dcb85221a12847c1
asaph <ace@dontletgo.com>
2014-11-26 08:51:21 -0800

    tomcat 6.0.43
    Closes #576.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- tomcat6.rb
+++ tomcat6.rb
@@ -2,17 +2,17 @@ require 'formula'
 
 class Tomcat6 < Formula
   homepage 'http://tomcat.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-6/v6.0.41/bin/apache-tomcat-6.0.41.tar.gz'
-  sha1 '34f35928d1067327f2aab874cdcea0660a4a3875'
+  url 'http://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-6/v6.0.43/bin/apache-tomcat-6.0.43.tar.gz'
+  sha1 '455d9aabb7fa0372623dce43b403166379c820c6'
 
   keg_only "Some scripts that are installed conflict with other software."
 
   option "with-fulldocs", "Install full documentation locally"
 
   resource 'fulldocs' do
-    url 'http://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-6/v6.0.41/bin/apache-tomcat-6.0.41-fulldocs.tar.gz'
-    version '6.0.41'
-    sha1 '35a4dd8090754cbe00a2fc4674cc9587b9e5eba1'
+    url 'http://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-6/v6.0.43/bin/apache-tomcat-6.0.43-fulldocs.tar.gz'
+    version '6.0.43'
+    sha1 '7114d0e774103ab29ad87c8a97c089f8ae1083a8'
   end
 
   def install
