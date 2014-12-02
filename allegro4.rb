1557c4f74174398d5e319734fe80a049021e1c6d
Jack Nagel <jacknagel@gmail.com>
2014-12-02 11:25:37 -0500

    allegro 4.4.2

--- allegro4.rb
+++ allegro4.rb
@@ -1,12 +1,13 @@
-require 'formula'
+require "formula"
 
 class Allegro4 < Formula
-  homepage 'http://www.allegro.cc'
-  url 'https://downloads.sourceforge.net/project/alleg/allegro/4.4.1.1/allegro-4.4.1.1.tar.gz'
-  sha1 '1970570b54c4329c7bd6d103db01624c68f2e9be'
+  homepage "http://www.allegro.cc"
+  url "https://downloads.sourceforge.net/project/alleg/allegro/4.4.2/allegro-4.4.2.tar.gz"
+  sha1 "ae0c15d2cb6b0337ef388dc98cefc620883720df"
 
-  depends_on 'cmake' => :build
-  depends_on 'libvorbis' => :optional
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libvorbis" => :optional
 
   def install
     system "cmake", ".", *std_cmake_args
