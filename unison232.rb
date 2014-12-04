ac9ebb3ec63bb27c21bf788e5377355dedfe2282
hightman <hightman@twomice.net>
2014-12-04 11:35:01 +0800

    unison232: fix segfault on 10.10
    Closes #582.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- unison232.rb
+++ unison232.rb
@@ -1,14 +1,15 @@
-require 'formula'
-
 class Unison232 < Formula
-  homepage 'http://www.cis.upenn.edu/~bcpierce/unison/'
-  url 'http://www.seas.upenn.edu/~bcpierce/unison//download/releases/unison-2.32.52/unison-2.32.52.tar.gz'
-  sha1 '68ea5709de4fcc2f9aef7b01b24637503b61b5ac'
+  homepage "http://www.cis.upenn.edu/~bcpierce/unison/"
+  url "http://www.seas.upenn.edu/~bcpierce/unison//download/releases/unison-2.32.52/unison-2.32.52.tar.gz"
+  sha1 "68ea5709de4fcc2f9aef7b01b24637503b61b5ac"
 
-  depends_on 'objective-caml'
+  depends_on "objective-caml"
 
   # http://tech.groups.yahoo.com/group/unison-users/message/9348
   # required for building 2.32.52 with ocamlc 3.12.x
+  # Part 2: Fixes segfault on >= Yosemite
+  # http://caml.inria.fr/mantis/view.php?id=6621
+  # https://github.com/Homebrew/homebrew/issues/34392
   patch :DATA
 
   def install
@@ -16,7 +17,7 @@ class Unison232 < Formula
     ENV.delete "CFLAGS" # ocamlopt reads CFLAGS but doesn't understand common options
     system "make ./mkProjectInfo"
     system "make UISTYLE=text"
-    bin.install 'unison'
+    bin.install "unison"
   end
 end
 
@@ -32,3 +33,13 @@ __END__
  
  type archive =
      ArchiveDir of Props.t * archive NameMap.t
+--- unison-2.32.52/ubase/util.ml.orig	2014-11-27 17:08:07.625677820 +0800
++++ unison-2.32.52/ubase/util.ml	2014-11-27 17:09:07.606361064 +0800
+@@ -71,7 +71,7 @@
+   if s <> !infos then begin clear_infos (); infos := s; show_infos () end
+
+ let msg f =
+-  clear_infos (); Uprintf.eprintf (fun () -> flush stderr; show_infos ()) f
++  clear_infos (); Printf.kfprintf (fun c -> flush c; show_infos ()) stderr f
+
+ let msg : ('a, out_channel, unit) format -> 'a = msg
