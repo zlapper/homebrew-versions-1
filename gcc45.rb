ec84713066be55b95698953d29e991d6673787be
Matthew Townsend <mtownsend@uievolution.com>
2014-12-11 17:32:41 -0800

    Apply 10.10 deployment target patch to gcc45
    Closes #589.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- gcc45.rb
+++ gcc45.rb
@@ -50,6 +50,12 @@ class Gcc45 < Formula
     sha1 "49e335d085567467155ea6512ffa959a18eab0ef"
   end
 
+  # Handle OS X deployment targets correctly (GCC PR target/63810 <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=63810>).
+  patch :p0 do
+    url "https://trac.macports.org/export/129382/trunk/dports/lang/gcc45/files/macosx-version-min.patch"
+    sha1 "79a3314759a33059cd6e3a078fdb3f5ee957d2e6"
+  end
+
   fails_with :llvm
 
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
