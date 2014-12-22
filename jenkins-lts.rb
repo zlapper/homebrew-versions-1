506289ec0fa9ae67fe13b5a54f920b2d05e9ce91
Mike McQuaid <mike@mikemcquaid.com>
2014-12-22 12:25:10 +0000

    jenkins-lts 1.580.2

--- jenkins-lts.rb
+++ jenkins-lts.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class JenkinsLts < Formula
   homepage "http://jenkins-ci.org/#stable"
-  url "http://mirrors.jenkins-ci.org/war-stable/1.580.1/jenkins.war"
-  sha1 "b9320b2c1ea28247f035bbf0f313948e8a735b78"
+  url "http://mirrors.jenkins-ci.org/war-stable/1.580.2/jenkins.war"
+  sha1 "7585f4920e3e1f501a4c26647b5776188d5ed290"
 
   conflicts_with "jenkins",
     :because => "both use the same data directory: $HOME/.jenkins"
