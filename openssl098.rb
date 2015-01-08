927ad81f965de4ac17a8884808d6d7b908b1d6dc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-01-08 18:24:02 +0000

    openssl098: add 0.9.8zd bottle.

--- openssl098.rb
+++ openssl098.rb
@@ -5,6 +5,13 @@ class Openssl098 < Formula
   url "https://www.openssl.org/source/openssl-0.9.8zd.tar.gz"
   sha256 "59266dcfb0be0fbe6181edead044ac3edaf83bc58991f264dcf532b01d531ee3"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/versions"
+    sha1 "65c01f5377ddcb07b4addae5dc6239840bb2fe4b" => :yosemite
+    sha1 "bfd9afdd4e9f4262341b53425e08727d44738aa2" => :mavericks
+    sha1 "a0c17c20c2ac25bd44dccd90eb44b20033142dd9" => :mountain_lion
+  end
+
   keg_only :provided_by_osx
 
   def install
