2e600fd4af2f46d66985243b89417abc936e366b
Mike McQuaid <mike@mikemcquaid.com>
2015-02-16 21:03:37 +0000

    jenkins-lts: fix sha1.

--- jenkins-lts.rb
+++ jenkins-lts.rb
@@ -3,7 +3,7 @@ require "formula"
 class JenkinsLts < Formula
   homepage "http://jenkins-ci.org/#stable"
   url "http://mirrors.jenkins-ci.org/war-stable/1.580.3/jenkins.war"
-  sha1 "ee3f94a2eab93a119baaa897a2fd0045cc401e73"
+  sha1 "18a8e6ea4a7723b10027ae51ed0196a0d994f622"
 
   conflicts_with "jenkins",
     :because => "both use the same data directory: $HOME/.jenkins"
