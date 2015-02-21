3778161340a834446db032b1d97e87ddc1a4dc6e
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-21 19:29:27 +0000

    appledoc22: style nits
    Fixes some style nits, and hopefully this time the bot's downstream
    bottle build won't fail.
    
    I misread the bot's message, which combined with successfully uploading
    to bintray threw me off. My mistake entirely, I apologise.
    
    Closes #663.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- appledoc22.rb
+++ appledoc22.rb
@@ -1,30 +1,30 @@
-require 'formula'
-
 # 2.2 (build 961) introduces support for documenting enumerated and
 # bitmask types, and will emit warnings on encountering undocumented
 # instances of those types.  An archived release is provided as a stable
 # dependency for e.g. continuous integration environments.
 class Appledoc22 < Formula
-  homepage 'http://appledoc.gentlebytes.com/'
+  homepage "http://appledoc.gentlebytes.com/"
   url "https://github.com/tomaz/appledoc/archive/2.2.1.tar.gz"
-  sha1 'a25d9ce876c4f7ee88d82b4532956d2c94b5d2e9'
+  sha1 "a25d9ce876c4f7ee88d82b4532956d2c94b5d2e9"
 
-  keg_only %{
-This formula is keg-only to avoid conflicts with the core Appledoc formula.
-The executable installed by this formula may be invoked explicitly,
-or (if it is the only version installed) linked after it is installed.
-  }
+  keg_only :provided_by_osx, <<-EOS.undent
+   The executable installed by this formula may be invoked explicitly,
+   or (if it is the only version installed) linked after it is installed.
+  EOS
 
   depends_on :xcode
   depends_on :macos => :lion
 
   # Actually works with pre-503 clang, but we don't have a way to
   # express this yet.
-  # clang 5.1 (build 503) removed support for Objective C GC, which
-  # appledoc 2.2 requires to build.
   # It's actually possible to build with GC disabled, but not advisable.
   # See: https://github.com/tomaz/appledoc/issues/439
-  fails_with :clang
+  fails_with :clang do
+    cause <<-EOS.undent
+      clang 5.1 (build 503) removed support for Objective C GC
+      which appledoc 2.2 requires to build.
+    EOS
+  end
 
   def install
     xcodebuild "-project", "appledoc.xcodeproj",
