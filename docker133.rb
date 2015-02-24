b626208f2da4feac8cbefb77109451cc36140dbf
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-24 00:28:18 +0000

    docker133: update 1.3.3 bottle.

--- docker133.rb
+++ docker133.rb
@@ -4,6 +4,14 @@ class Docker133 < Formula
   # Please update the version of boot2docker too
   url "https://github.com/docker/docker.git", :tag => "v1.3.3"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha1 "efa8a6c570d0aa167dc13451636e569378b4e13a" => :yosemite
+    sha1 "339d5b573e10c456228fe0f224af63b84c8c6f91" => :mavericks
+    sha1 "f60f9d8815545837169eebafdf328d914f9b6156" => :mountain_lion
+  end
+
   option "without-completions", "Disable bash/zsh completions"
 
   depends_on "go" => :build
