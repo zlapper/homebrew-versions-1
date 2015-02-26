6c366756a68c9679416b9c3f68f4e3c5e6ea67d1
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-26 11:08:25 +0000

    phantomjs198: add 1.9.8 bottle.

--- phantomjs198.rb
+++ phantomjs198.rb
@@ -3,6 +3,14 @@ class Phantomjs198 < Formula
   url "https://github.com/ariya/phantomjs/archive/1.9.8.tar.gz"
   sha256 "3a321561677f678ca00137c47689e3379c7fe6b83f7597d2d5de187dd243f7be"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha1 "ad447564de81f1499795a6c8e2dd7b5db474f2dc" => :yosemite
+    sha1 "ec47fc927a5ed38bb2c5802f661c635f6c15324d" => :mavericks
+    sha1 "54348237528d076e12ffcc7ffb286fb0b5e02b61" => :mountain_lion
+  end
+
   depends_on "openssl"
 
   def install
