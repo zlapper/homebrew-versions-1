5ccd304b9427e5bffc63b6a0f1e3709c01ccc292
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-27 11:59:09 +0000

    libuv0 0.10.36
    Bump.
    
    Closes #687.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- libuv0.rb
+++ libuv0.rb
@@ -1,16 +1,8 @@
 class Libuv0 < Formula
   homepage "https://github.com/libuv/libuv"
-  url "https://github.com/libuv/libuv/archive/v0.10.32.tar.gz"
-  sha1 "9f06392ebcbfd25e029d700c869ade8581ec2895"
+  url "https://github.com/libuv/libuv/archive/v0.10.36.tar.gz"
+  sha1 "0991836d1dbf9419f448bc3459559181505e29c5"
   head "https://github.com/libuv/libuv.git", :branch => "v0.10"
-
-  bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/versions"
-    cellar :any
-    sha1 "772fe584a53138a540ceefb469592c2c5279035e" => :yosemite
-    sha1 "fd8edf4c95c801eb474566ca7980c759929f4a64" => :mavericks
-    sha1 "89bab70004d9e2458c2664caaaadcd49163839e2" => :mountain_lion
-  end
 
   conflicts_with "libuv"
 
