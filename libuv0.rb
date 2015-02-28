2f6ca7747ef3d31ab16a5e07a8daabfbe0003a13
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-28 01:55:40 +0000

    libuv0: add 0.10.36 bottle.

--- libuv0.rb
+++ libuv0.rb
@@ -4,6 +4,14 @@ class Libuv0 < Formula
   sha1 "0991836d1dbf9419f448bc3459559181505e29c5"
   head "https://github.com/libuv/libuv.git", :branch => "v0.10"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha1 "49427a8c4ba598d11867e33ff05f304f61cceab8" => :yosemite
+    sha1 "eb147122bbabb9e35df0a77f795dd9899ba5320c" => :mavericks
+    sha1 "e47ad07dda7d4e2c33212ae2c458192263919365" => :mountain_lion
+  end
+
   conflicts_with "libuv"
 
   option :universal
