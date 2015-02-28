c997f801fedf51f570d9354a5fa67711025b3b3d
Dominyk Tiller <dominyktiller@gmail.com>
2015-02-28 08:51:26 +0000

    gcc43: basic style tweaks
    Basic style fixes with the underlying motivation being bottling this.
    
    Closes #690.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- gcc43.rb
+++ gcc43.rb
@@ -1,18 +1,16 @@
-require 'formula'
-
 class Gcc43 < Formula
   def arch
     if Hardware::CPU.type == :intel
       if MacOS.prefer_64_bit?
-        'x86_64'
+        "x86_64"
       else
-        'i686'
+        "i686"
       end
     elsif Hardware::CPU.type == :ppc
       if MacOS.prefer_64_bit?
-        'powerpc64'
+        "powerpc64"
       else
-        'powerpc'
+        "powerpc"
       end
     end
   end
@@ -21,21 +19,27 @@ class Gcc43 < Formula
     `uname -r`.chomp
   end
 
-  homepage 'https://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.3.6/gcc-4.3.6.tar.bz2'
-  mirror 'https://ftp.gnu.org/gnu/gcc/gcc-4.3.6/gcc-4.3.6.tar.bz2'
-  sha1 'df276018e3c664c7e6aa7ca88a180515eea61663'
+  homepage "https://gcc.gnu.org"
+  url "http://ftpmirror.gnu.org/gcc/gcc-4.3.6/gcc-4.3.6.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/gcc/gcc-4.3.6/gcc-4.3.6.tar.bz2"
+  sha1 "df276018e3c664c7e6aa7ca88a180515eea61663"
 
-  option 'enable-fortran', 'Build the gfortran compiler'
-  option 'enable-java', 'Build the gcj compiler'
-  option 'enable-all-languages', 'Enable all compilers and languages, except Ada'
-  option 'enable-nls', 'Build with native language support (localization)'
-  option 'enable-profiled-build', 'Make use of profile guided optimization when bootstrapping GCC'
+  option "with-fortran", "Build the gfortran compiler"
+  option "with-java", "Build the gcj compiler"
+  option "with-all-languages", "Enable all compilers and languages, except Ada"
+  option "with-nls", "Build with native language support (localization)"
+  option "with-profiled-build", "Make use of profile guided optimization when bootstrapping GCC"
+
+  deprecated_option "enable-fortran" => "with-fortran"
+  deprecated_option "enable-java" => "with-java"
+  deprecated_option "enable-all-languages" => "with-all-languages"
+  deprecated_option "enable-nls" => "with-nls"
+  deprecated_option "enable-profiled-build" => "with-profiled-build"
 
   depends_on MaximumMacOSRequirement => :mavericks
-  depends_on 'gmp4'
-  depends_on 'mpfr2'
-  depends_on 'ecj' if build.include? 'enable-java' or build.include? 'enable-all-languages'
+  depends_on "gmp4"
+  depends_on "mpfr2"
+  depends_on "ecj"  if build.with?("java") || build.with?("all-languages")
 
   # Fix building on darwin10
   patch :p0 do
@@ -63,14 +67,20 @@ class Gcc43 < Formula
 
   fails_with :llvm
 
+  # The bottles are built on systems with the CLT installed, and do not work
+  # out of the box on Xcode-only systems due to an incorrect sysroot.
+  def pour_bottle?
+    MacOS::CLT.installed?
+  end
+
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
   cxxstdlib_check :skip
 
   def install
     # GCC will suffer build errors if forced to use a particular linker.
-    ENV.delete 'LD'
+    ENV.delete "LD"
 
-    if build.include? 'enable-all-languages'
+    if build.with? "all-languages"
       # Everything but Ada, which requires a pre-existing GCC Ada compiler
       # (gnat) to bootstrap.
       languages = %w[c c++ fortran java objc obj-c++]
@@ -78,8 +88,8 @@ class Gcc43 < Formula
       # C, C++, ObjC compilers are always built
       languages = %w[c c++ objc obj-c++]
 
-      languages << 'fortran' if build.include? 'enable-fortran'
-      languages << 'java' if build.include? 'enable-java'
+      languages << "fortran" if build.with? "fortran"
+      languages << "java" if build.with? "java"
     end
 
     version_suffix = version.to_s.slice(/\d\.\d/)
@@ -89,7 +99,7 @@ class Gcc43 < Formula
       "--prefix=#{prefix}",
       "--mandir=#{man}",
       "--infodir=#{info}",
-      "--enable-languages=#{languages.join(',')}",
+      "--enable-languages=#{languages.join(",")}",
       # Make most executables versioned to avoid conflicts.
       "--program-suffix=-#{version_suffix}",
       "--with-gmp=#{Formula["gmp4"].opt_prefix}",
@@ -110,27 +120,26 @@ class Gcc43 < Formula
       "--with-bugurl=https://github.com/Homebrew/homebrew-versions/issues",
     ]
 
-    args << '--disable-nls' unless build.include? 'enable-nls'
+    args << "--disable-nls" if build.without? "nls"
 
-    if build.include? 'enable-java' or build.include? 'enable-all-languages'
+    if build.with?("java") || build.with?("all-languages")
       args << "--with-ecj-jar=#{Formula["ecj"].opt_prefix}/share/java/ecj.jar"
     end
 
-
-    mkdir 'build' do
+    mkdir "build" do
       unless MacOS::CLT.installed?
         # For Xcode-only systems, we need to tell the sysroot path.
-        # 'native-system-header's will be appended
+        # "native-system-headers" will be appended
         args << "--with-native-system-header-dir=/usr/include"
         args << "--with-sysroot=#{MacOS.sdk_path}"
       end
 
-      system '../configure', *args
+      system "../configure", *args
 
       # Flags for Clang compatibility
       make_flags = 'BOOT_CFLAGS="$BOOT_CFLAGS -D_FORTIFY_SOURCE=0" STAGE1_CFLAGS="$STAGE1_CFLAGS -std=gnu89 -D_FORTIFY_SOURCE=0 -fkeep-inline-functions"'
 
-      if build.include? 'enable-profiled-build'
+      if build.with? "profiled-build"
         # Takes longer to build, may bug out. Provided for those who want to
         # optimise all the way to 11.
         system "make #{make_flags} profiledbootstrap"
@@ -140,32 +149,28 @@ class Gcc43 < Formula
 
       # At this point `make check` could be invoked to run the testsuite. The
       # deja-gnu formula must be installed in order to do this.
-
-      system 'make install'
+      system "make", "install"
     end
 
     # Handle conflicts between GCC formulae.
-
     # Remove libffi stuff, which is not needed after GCC is built.
     Dir.glob(prefix/"**/libffi.*") { |file| File.delete file }
-
     # Rename libiberty.a.
     Dir.glob(prefix/"**/libiberty.*") { |file| add_suffix file, version_suffix }
-
     # Rename man7.
     Dir.glob(man7/"*.7") { |file| add_suffix file, version_suffix }
 
     # Even when suffixes are appended, the info pages conflict when
-    # install-info is run. TODO fix this.
+    # install-info is run. Fix this.
     info.rmtree
 
     # Rename java properties
-    if build.include? 'enable-java' or build.include? 'enable-all-languages'
+    if build.with?("java") || build.with?("all-languages")
       config_files = [
         "#{lib}/logging.properties",
         "#{lib}/security/classpath.security",
         "#{lib}/i386/logging.properties",
-        "#{lib}/i386/security/classpath.security"
+        "#{lib}/i386/security/classpath.security",
       ]
 
       config_files.each do |file|
@@ -174,10 +179,23 @@ class Gcc43 < Formula
     end
   end
 
-  def add_suffix file, suffix
+  def add_suffix(file, suffix)
     dir = File.dirname(file)
     ext = File.extname(file)
     base = File.basename(file, ext)
     File.rename file, "#{dir}/#{base}-#{suffix}#{ext}"
+  end
+
+  test do
+    (testpath/"hello-c.c").write <<-EOS.undent
+      #include <stdio.h>
+      int main()
+      {
+        puts("Hello, world!");
+        return 0;
+      }
+    EOS
+    system bin/"gcc-4.3", "-o", "hello-c", "hello-c.c"
+    assert_equal "Hello, world!\n", `./hello-c`
   end
 end
