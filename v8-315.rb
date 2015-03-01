4dc775e9796ffdedc49cdf74245261ac2ae9d345
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-01 20:20:56 +0000

    v8-315: add 3.15.11 bottle.

--- v8-315.rb
+++ v8-315.rb
@@ -6,6 +6,14 @@ class V8315 < Formula
   url 'https://github.com/v8/v8/archive/3.15.11.tar.gz'
   sha1 '0c47b3a5409d71d4fd6581520c8972f7451a87e4'
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha1 "d2304e24d2fa6d6d499327d98755f4a0088d83d4" => :yosemite
+    sha1 "1c3a4a0b45f0d5a706e048e80f5a497a6d2e02ec" => :mavericks
+    sha1 "be82b18f5f267be5e11da92c62efb8a4c89f33c1" => :mountain_lion
+  end
+
   keg_only 'Conflicts with V8 in main repository.'
 
   def install
