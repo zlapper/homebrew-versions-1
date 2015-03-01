1e6d8f75037f9e80f2e7724b3ab15cd8e07cd677
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-01 21:46:57 +0000

    go12: automatically install godoc/go vet
    Sync with changes pushed to Homebrew/Homebrew recently on automatically
    installing the widely requested godoc & go vet.
    
    Closes #697.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- go12.rb
+++ go12.rb
@@ -1,69 +1,92 @@
-require 'formula'
-
 class Go12 < Formula
-  homepage 'http://golang.org'
-  url 'https://storage.googleapis.com/golang/go1.2.2.src.tar.gz'
-  version '1.2.2'
-  sha1 '3ce0ac4db434fc1546fec074841ff40dc48c1167'
+  homepage "https://golang.org"
+  url "https://storage.googleapis.com/golang/go1.2.2.src.tar.gz"
+  version "1.2.2"
+  sha1 "3ce0ac4db434fc1546fec074841ff40dc48c1167"
 
-  option 'cross-compile-all', "Build the cross-compilers and runtime support for all supported platforms"
-  option 'cross-compile-common', "Build the cross-compilers and runtime support for darwin, linux and windows"
-  option 'without-cgo', "Build without cgo"
+  option "with-cc-all", "Build with cross-compilers and runtime support for all supported platforms"
+  option "with-cc-common", "Build with cross-compilers and runtime support for darwin, linux and windows"
+  option "without-cgo", "Build without cgo"
+  option "without-godoc", "godoc will not be installed for you"
+  option "without-vet", "vet will not be installed for you"
+
+  deprecated_option "cross-compile-all" => "with-cc-all"
+  deprecated_option "cross-compile-common" => "with-cc-common"
+
+  resource "gotools" do
+    url "https://go.googlesource.com/tools.git",
+    :revision => "69db398fe0e69396984e3967724820c1f631e971"
+  end
 
   def install
     # install the completion scripts
-    bash_completion.install 'misc/bash/go' => 'go-completion.bash'
-    zsh_completion.install 'misc/zsh/go' => 'go'
+    bash_completion.install "misc/bash/go" => "go-completion.bash"
+    zsh_completion.install "misc/zsh/go" => "go"
 
     # host platform (darwin) must come last in the targets list
-    if build.include? 'cross-compile-all'
+    if build.with? "cc-all"
       targets = [
-        ['linux',   ['386', 'amd64', 'arm']],
-        ['freebsd', ['386', 'amd64']],
-        ['netbsd',  ['386', 'amd64']],
-        ['openbsd', ['386', 'amd64']],
-        ['windows', ['386', 'amd64']],
-        ['darwin',  ['386', 'amd64']],
+        ["linux",   ["386", "amd64", "arm"]],
+        ["freebsd", ["386", "amd64"]],
+        ["netbsd",  ["386", "amd64"]],
+        ["openbsd", ["386", "amd64"]],
+        ["windows", ["386", "amd64"]],
+        ["darwin",  ["386", "amd64"]],
       ]
-    elsif build.include? 'cross-compile-common'
+    elsif build.with? "cc-common"
       targets = [
-        ['linux',   ['386', 'amd64', 'arm']],
-        ['windows', ['386', 'amd64']],
-        ['darwin',  ['386', 'amd64']],
+        ["linux",   ["386", "amd64", "arm"]],
+        ["windows", ["386", "amd64"]],
+        ["darwin",  ["386", "amd64"]],
       ]
     else
-      targets = [['darwin', ['']]]
+      targets = [["darwin", [""]]]
     end
 
-    cd 'src' do
+    cd "src" do
       targets.each do |os, archs|
-        cgo_enabled = os == 'darwin' && build.with?('cgo') ? "1" : "0"
+        cgo_enabled = os == "darwin" && build.with?("cgo") ? "1" : "0"
         archs.each do |arch|
-          ENV['GOROOT_FINAL'] = libexec
-          ENV['GOOS']         = os
-          ENV['GOARCH']       = arch
-          ENV['CGO_ENABLED']  = cgo_enabled
+          ENV["GOROOT_FINAL"] = libexec
+          ENV["GOOS"]         = os
+          ENV["GOARCH"]       = arch
+          ENV["CGO_ENABLED"]  = cgo_enabled
           system "./make.bash", "--no-clean"
         end
       end
     end
 
-    (buildpath/'pkg/obj').rmtree
+    (buildpath/"pkg/obj").rmtree
 
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.install_symlink Dir["#{libexec}/bin/go*"]
+
+    if build.with?("godoc") || build.with?("vet")
+      ENV.prepend_path "PATH", bin
+      ENV["GOPATH"] = buildpath
+      (buildpath/"src/golang.org/x/tools").install resource("gotools")
+
+      if build.with? "godoc"
+        cd "src/golang.org/x/tools/cmd/godoc/" do
+          system "go", "build"
+          (libexec/"bin").install "godoc"
+        end
+        bin.install_symlink libexec/"bin/godoc"
+      end
+
+      if build.with? "vet"
+        cd "src/golang.org/x/tools/cmd/vet/" do
+          system "go", "build"
+          # This is where Go puts vet natively; not in the bin.
+          (libexec/"pkg/tool/darwin_amd64/").install "vet"
+        end
+      end
+    end
   end
 
   def caveats; <<-EOS.undent
     As of go 1.2, a valid GOPATH is required to use the `go get` command:
       http://golang.org/doc/code.html#GOPATH
-
-    `go vet` and `go doc` are now part of the go.tools sub repo:
-      http://golang.org/doc/go1.2#go_tools_godoc
-
-    To get `go vet` and `go doc` run:
-      go get code.google.com/p/go.tools/cmd/godoc
-      go get code.google.com/p/go.tools/cmd/vet
 
     You may wish to add the GOROOT-based install location to your PATH:
       export PATH=$PATH:#{opt_libexec}/bin
@@ -71,7 +94,7 @@ class Go12 < Formula
   end
 
   test do
-    (testpath/'hello.go').write <<-EOS.undent
+    (testpath/"hello.go").write <<-EOS.undent
     package main
 
     import "fmt"
@@ -82,7 +105,7 @@ class Go12 < Formula
     EOS
     # Run go fmt check for no errors then run the program.
     # This is a a bare minimum of go working as it uses fmt, build, and run.
-    system "#{bin}/go", "fmt", "hello.go"
+    system bin/"go", "fmt", "hello.go"
     assert_equal "Hello World\n", `#{bin}/go run hello.go`
   end
 end
