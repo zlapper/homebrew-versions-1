e63251351c7c2f76eced395a699d8717174156f9
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-01 20:26:07 +0000

    v8-315: fix style nits

--- v8-315.rb
+++ v8-315.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class V8315 < Formula
-  homepage 'http://code.google.com/p/v8/'
+  homepage "https://code.google.com/p/v8/"
   # Use the official github mirror, it is easier to find tags there
-  url 'https://github.com/v8/v8/archive/3.15.11.tar.gz'
-  sha1 '0c47b3a5409d71d4fd6581520c8972f7451a87e4'
+  url "https://github.com/v8/v8/archive/3.15.11.tar.gz"
+  sha1 "0c47b3a5409d71d4fd6581520c8972f7451a87e4"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-versions"
@@ -14,26 +12,26 @@ class V8315 < Formula
     sha1 "be82b18f5f267be5e11da92c62efb8a4c89f33c1" => :mountain_lion
   end
 
-  keg_only 'Conflicts with V8 in main repository.'
+  keg_only "Conflicts with V8 in Homebrew/homebrew."
 
   def install
     # Lie to `xcode-select` for now to work around a GYP bug that affects
     # CLT-only systems:
     #
     #   http://code.google.com/p/gyp/issues/detail?id=292
-    ENV['DEVELOPER_DIR'] = MacOS.dev_tools_path unless MacOS::Xcode.installed?
+    ENV["DEVELOPER_DIR"] = MacOS.dev_tools_path unless MacOS::Xcode.installed?
 
-    system 'make dependencies'
-    system 'make', 'native',
+    system "make", "dependencies"
+    system "make", "native",
                    "-j#{ENV.make_jobs}",
                    "library=shared",
                    "snapshot=on",
                    "console=readline"
 
-    prefix.install 'include'
-    cd 'out/native' do
-      lib.install Dir['lib*']
-      bin.install 'd8', 'lineprocessor', 'mksnapshot', 'preparser', 'process', 'shell' => 'v8'
+    prefix.install "include"
+    cd "out/native" do
+      lib.install Dir["lib*"]
+      bin.install "d8", "lineprocessor", "mksnapshot", "preparser", "process", "shell" => "v8"
     end
   end
 end
