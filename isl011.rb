ddc5d81f8b8ffe84c01abe38f566d3e8c13d2678
Mike McQuaid <mike@mikemcquaid.com>
2015-03-12 13:34:40 +0000

    isl011: add more bottles.

--- isl011.rb
+++ isl011.rb
@@ -8,6 +8,11 @@ class Isl011 < Formula
   sha1 'd7936929c3937e03f09b64c3c54e49422fa8ddb3'
 
   bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "9f4a6d0ff2f5cd7208a1c29c84eb6c452e6bcb7b7249b597507b1f0aad13e2ef" => :yosemite
+    sha256 "c1ebc3499a91cad85af852f293fc330f5bf4b168af031d1927f2183736246ca9" => :mavericks
+    sha256 "43d1e0023ea8c7df4199668c9cb76927c22756bbc7b0b7db5b047715db8d6001" => :mountain_lion
     sha1 '49d0c81d3d0e72abe18e171b58e6159122ba07bc' => :tiger_g3
     sha1 '7500d4495c2da2059b484d96190d8e563b01a357' => :tiger_altivec
     sha1 '5cf2a19ddde50e65786a2d5957a0d0b20f1db94c' => :leopard_g3
