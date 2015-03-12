9e5bdbcff908390b0a8b003e27e3fadabf356749
Mike McQuaid <mike@mikemcquaid.com>
2015-03-12 13:34:47 +0000

    libmpc08: add more bottles.

--- libmpc08.rb
+++ libmpc08.rb
@@ -8,6 +8,11 @@ class Libmpc08 < Formula
   sha1 '5ef03ca7aee134fe7dfecb6c9d048799f0810278'
 
   bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "a52eb16b2016623c7c31bdebad9a3d90b84082bcbf96e1648dce18e1e1d8dff3" => :yosemite
+    sha256 "9e52d35c5ee1c1027701d012110dc18b546288fbbec113c09a8b3dacdb87bbc4" => :mavericks
+    sha256 "03d6b2fd2c47434d305b4be197d5a946b25706efc97d009fc4a0e1c68be5e3a2" => :mountain_lion
     sha1 '7f63cdd281274f82122d7580400a4b5561dabd93' => :tiger_g3
     sha1 'fa73d69bf25ccfe069dc553a4d90f09877ca9054' => :tiger_altivec
     sha1 'bc3ce1df9d7afcafa05f270e03ac73158c312004' => :leopard_g3
