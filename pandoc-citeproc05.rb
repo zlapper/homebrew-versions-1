e8514c8f888097ddd511059b2288b8670fff6a44
Randy Lai <randy.cs.lai@gmail.com>
2015-03-15 23:47:15 -0700

    pandoc-citeproc05 0.5 (new formula)
    Closes #716.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ pandoc-citeproc05.rb
@@ -0,0 +1,40 @@
+require "language/haskell"
+
+class PandocCiteproc05 < Formula
+  include Language::Haskell::Cabal
+
+  homepage "https://github.com/jgm/pandoc-citeproc"
+  url "https://hackage.haskell.org/package/pandoc-citeproc-0.5/pandoc-citeproc-0.5.tar.gz"
+  sha256 "83ff6d75cdf4a92d4f7fb4b7c70adcf53b30dd82831d38ad4dcb7640e9855f01"
+
+  keg_only "Conflicts with pandoc-citeproc in main repository."
+
+  depends_on "ghc" => :build
+  depends_on "cabal-install" => :build
+  depends_on "gmp"
+  depends_on "pandoc1131" => :recommended
+
+  fails_with(:clang) { build 425 } # clang segfaults on Lion
+
+  def install
+    cabal_sandbox do
+      cabal_install "--only-dependencies"
+      cabal_install "--prefix=#{prefix}"
+    end
+    cabal_clean_lib
+  end
+
+  test do
+    bib = testpath/"test.bib"
+    bib.write <<-EOS.undent
+      @Book{item1,
+      author="John Doe",
+      title="First Book",
+      year="2005",
+      address="Cambridge",
+      publisher="Cambridge University Press"
+      }
+    EOS
+    assert `#{bin}/pandoc-citeproc --bib2yaml #{bib}`.include? "- publisher-place: Cambridge"
+  end
+end
