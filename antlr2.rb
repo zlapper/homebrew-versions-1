5daf32b63dc35c9096091ab831b1249dad23a050
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-17 06:35:55 +0000

    antlr2: add 2.7.7 bottle.

--- antlr2.rb
+++ antlr2.rb
@@ -3,6 +3,13 @@ class Antlr2 < Formula
   url "http://www.antlr2.org/download/antlr-2.7.7.tar.gz"
   sha256 "853aeb021aef7586bda29e74a6b03006bcb565a755c86b66032d8ec31b67dbb9"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    sha256 "7dcc3717cb1fd192ddb3200378856b81d1ccfaeeabcb60817e6927a4c78111a9" => :yosemite
+    sha256 "f70c59f788573514d6666fc20410456b584e328108af814d17422121b17278d6" => :mavericks
+    sha256 "2d722d988a3352dae7b13d6344af7bf34a0301d39bd7d8b0da5e2f285a4af008" => :mountain_lion
+  end
+
   def install
     # C Sharp is explicitly disabled because the antlr configure script will
     # confuse the Chicken Scheme compiler, csc, for a C sharp compiler.
