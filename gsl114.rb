c83661541bc68e7b51c85b66eb8bb3f23fde1805
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-22 19:43:47 +0000

    gsl114: add 1.14 bottle.

--- gsl114.rb
+++ gsl114.rb
@@ -4,6 +4,14 @@ class Gsl114 < Formula
   mirror "https://ftp.gnu.org/gnu/gsl/gsl-1.14.tar.gz"
   sha256 "3d4a47afd9a1e7c73b97791b4180d8cc4d5f0e5db6027fe06437f1f3f957fafb"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "e3e5dcf0d83043554296bc8dc836dcd04c496b8417bd004adab5420dc8c212b5" => :yosemite
+    sha256 "5efe0db286ac52fdd72976264d1bdf1852d3d5ae6a6971a0297edc7c2f724e75" => :mavericks
+    sha256 "11f5aff7fdbc03258801ae76373ba5d7420f24e1beca93de7435464df0578b19" => :mountain_lion
+  end
+
   option :universal
 
   def install
