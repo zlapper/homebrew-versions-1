63b2a281c4f28aecdf2299fd0c7ac50c72f31e16
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-22 13:37:13 +0000

    cvsps2: added test
    Added (simple) test, bottled, fix style nits.

--- cvsps2.rb
+++ cvsps2.rb
@@ -1,12 +1,15 @@
-require 'formula'
-
 class Cvsps2 < Formula
-  homepage 'http://www.catb.org/~esr/cvsps/'
-  url 'http://www.cobite.com/cvsps/cvsps-2.1.tar.gz'
-  sha1 'a53a62b121e7b86e07a393bcb8aa4f0492a747c4'
+  homepage "http://www.catb.org/~esr/cvsps/"
+  url "http://www.cobite.com/cvsps/cvsps-2.1.tar.gz"
+  sha256 "91d3198b33463861a581686d5fcf99a5c484e7c4d819384c04fda9cafec1075a"
 
   def install
     system "make", "all"
     system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    # an assert_match on shell_output hangs indefinitely, as does normal syntax usage.
+    assert_match /special hack for parsing the/, pipe_output("#{bin}/cvsps -h 2>&1")
   end
 end
