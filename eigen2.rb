d33123639fcf6013eef21cc8f0b910701157785f
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-22 13:51:52 +0000

    eigen2: modernise and bottle
    ^

--- eigen2.rb
+++ eigen2.rb
@@ -1,19 +1,18 @@
-require 'formula'
-
 class Eigen2 < Formula
-  homepage 'http://eigen.tuxfamily.org/'
-  url 'http://bitbucket.org/eigen/eigen/get/2.0.17.tar.bz2'
-  sha1 '461546be98b964d8d5d2adb0f1c31ba0e42efc38'
+  homepage "http://eigen.tuxfamily.org/"
+  url "https://bitbucket.org/eigen/eigen/get/2.0.17.tar.bz2"
+  sha256 "7255e856ed367ce6e6e2d4153b0e4e753c8b8d36918bf440dd34ad56aff09960"
 
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
 
   def install
-    mkdir 'eigen-build' do
+    mkdir "eigen-build" do
       args = std_cmake_args
       args << "-DCMAKE_BUILD_TYPE=Release"
       args << "-Dpkg_config_libdir='#{lib}'" << ".."
+
       system "cmake", *args
-      system "make install"
+      system "make", "install"
     end
   end
 end
