a784cafcf950280f7e7d15ac585fc114f2458625
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-22 19:01:11 +0000

    giflib: modernise and bottle
    ^

--- giflib5.rb
+++ giflib5.rb
@@ -1,14 +1,12 @@
-require 'formula'
-
 class Giflib5 < Formula
-  homepage 'http://giflib.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/giflib/giflib-5.x/giflib-5.0.5.tar.bz2'
-  sha1 '926fecbcef1c5b1ca9d17257d15a197b8b35e405'
+  homepage "http://giflib.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/giflib/giflib-5.x/giflib-5.0.5.tar.bz2"
+  sha256 "606d8a366b1c625ab60d62faeca807a799a2b9e88cbdf2a02bfcdf4429bf8609"
 
   keg_only "Conflicts with giflib in main repository."
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
-    system "make install"
+    system "make", "install"
   end
 end
