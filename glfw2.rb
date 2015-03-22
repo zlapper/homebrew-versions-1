a03cd07fc6c9d61f70b6f56c6bb5e1860be01482
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-22 19:11:29 +0000

    glfw2: produce bottles
    Bottle and style nits.

--- glfw2.rb
+++ glfw2.rb
@@ -1,14 +1,12 @@
-require 'formula'
-
 class Glfw2 < Formula
-  homepage 'http://www.glfw.org/'
-  url 'https://downloads.sourceforge.net/project/glfw/glfw/2.7.9/glfw-2.7.9.tar.bz2'
-  sha1 '50ffcbfa1fb47d23aa6ad1245da329bd21bbaeca'
+  homepage "http://www.glfw.org/"
+  url "https://downloads.sourceforge.net/project/glfw/glfw/2.7.9/glfw-2.7.9.tar.bz2"
+  sha256 "d1f47e99e4962319f27f30d96571abcb04c1022c000de4d01df69ec59aae829d"
 
   option :universal
 
   def install
     ENV.universal_binary if build.universal?
-    system 'make', "PREFIX=#{prefix}", 'cocoa-dist-install'
+    system "make", "PREFIX=#{prefix}", "cocoa-dist-install"
   end
 end
