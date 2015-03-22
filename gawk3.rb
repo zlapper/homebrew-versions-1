c895c2ba272cafa806e482953b9f5abd49a814a0
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-22 18:57:58 +0000

    gawk3: add test
    Add the test from Homebrew/Homebrew, clean up the style nits, bottle.

--- gawk3.rb
+++ gawk3.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Gawk3 < Formula
-  homepage 'https://www.gnu.org/software/gawk/'
-  url 'http://ftpmirror.gnu.org/gawk/gawk-3.1.8.tar.bz2'
-  mirror 'https://ftp.gnu.org/gnu/gawk/gawk-3.1.8.tar.bz2'
-  sha1 'da1091cc39089c320f53d21fd2112bd7ce407de5'
+  homepage "https://www.gnu.org/software/gawk/"
+  url "http://ftpmirror.gnu.org/gawk/gawk-3.1.8.tar.bz2"
+  mirror "https://ftp.gnu.org/gnu/gawk/gawk-3.1.8.tar.bz2"
+  sha256 "5dbc7b2c4c328711337c2aacd09a122c7313122262e3ff034590f014067412b4"
 
   fails_with :llvm do
     build 2326
@@ -12,8 +10,13 @@ class Gawk3 < Formula
   end
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    output = pipe_output("#{bin}/gawk '{ gsub(/Macro/, \"Home\"); print }' -", "Macrobrew")
+    assert_equal 'Homebrew', output.strip
   end
 end
