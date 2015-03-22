d754b047ef8bf4ff4416f8a4f9399293989b0494
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-22 15:33:46 +0000

    eigen2: add 2.0.17 bottle.

--- eigen2.rb
+++ eigen2.rb
@@ -3,6 +3,14 @@ class Eigen2 < Formula
   url "https://bitbucket.org/eigen/eigen/get/2.0.17.tar.bz2"
   sha256 "7255e856ed367ce6e6e2d4153b0e4e753c8b8d36918bf440dd34ad56aff09960"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "59cbfaafbcba0535357bb63ac0e4877d47c2a52869cd32b7d6af74b5521d81bd" => :yosemite
+    sha256 "38c28e3bc3a78717584ecc83fabeeb840884273301c10311c7c274517ce75a6a" => :mavericks
+    sha256 "b400f68bd0c66a8616526334732eb7106070135af5fc7f6b347810f4dfbf785b" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   def install
