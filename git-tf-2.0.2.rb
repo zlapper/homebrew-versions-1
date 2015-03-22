de65cf48691dcb3a19b03d3c5e9e8ae5a07dbc05
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-22 19:06:00 +0000

    git-tf-2.0.2: improve readability
    Just a style touch-up here, since it'll never get done otherwise.

--- git-tf-2.0.2.rb
+++ git-tf-2.0.2.rb
@@ -1,21 +1,19 @@
-require 'formula'
-
 class GitTf202 < Formula
-  homepage 'http://gittf.codeplex.com/'
-  url 'http://download.microsoft.com/download/A/E/2/AE23B059-5727-445B-91CC-15B7A078A7F4/git-tf-2.0.2.20130214.zip'
-  sha1 '889c1bba6aba892e570a18a386654a50293efbd0'
+  homepage "https://gittf.codeplex.com/"
+  url "http://download.microsoft.com/download/A/E/2/AE23B059-5727-445B-91CC-15B7A078A7F4/git-tf-2.0.2.20130214.zip"
+  sha256 "2fb6d0c494b8f7007b3222f20d1fd3f5c7f406c35801f22d717c71115057eb15"
 
-  conflicts_with 'git-tf', :because => 'two different versions of the same library'
+  conflicts_with "git-tf", :because => "two different versions of the same library"
 
   def install
-    libexec.install 'git-tf'
-    libexec.install 'lib'
-    (libexec + "native").install 'native/macosx'
-    bin.write_exec_script libexec/'git-tf'
-    (share/'doc/git-tf').install Dir['Git-TF_*'] + Dir['ThirdPartyNotices*']
+    libexec.install "git-tf"
+    libexec.install "lib"
+    (libexec+"native").install "native/macosx"
+    bin.write_exec_script libexec/"git-tf"
+    (share/"doc/git-tf").install Dir["Git-TF_*"] + Dir["ThirdPartyNotices*"]
   end
 
   test do
-    system "#{bin}/git-tf"
+    system bin/"git-tf"
   end
 end
