d0ea84844d75b6be4630ede8ce8aaeb279b9ae1b
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-22 15:33:46 +0000

    cvsps2: add 2.1 bottle.

--- cvsps2.rb
+++ cvsps2.rb
@@ -3,6 +3,14 @@ class Cvsps2 < Formula
   url "http://www.cobite.com/cvsps/cvsps-2.1.tar.gz"
   sha256 "91d3198b33463861a581686d5fcf99a5c484e7c4d819384c04fda9cafec1075a"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "b8c56242eeab6032ea45267eda778852ed8101a6403ffbe570a39040849a7292" => :yosemite
+    sha256 "c28f907d790d36bdb5afdb3acccf604b8dbf10c3208ffd545e6040374fcb4988" => :mavericks
+    sha256 "37eed6e3d42ae1163b1ea40c246c9877e8f1737588b8654d4d6b28620f9b8c0c" => :mountain_lion
+  end
+
   def install
     system "make", "all"
     system "make", "install", "prefix=#{prefix}"
