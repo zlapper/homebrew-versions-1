78fb60686fe1d610f1e48a6127fc0a11207f2285
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-22 19:17:50 +0000

    gsl114: add test
    Style nits, test, bottle.
    
    Closes #733.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- gsl114.rb
+++ gsl114.rb
@@ -1,10 +1,8 @@
-require 'formula'
-
 class Gsl114 < Formula
-  homepage 'https://www.gnu.org/software/gsl/'
-  url 'http://ftpmirror.gnu.org/gsl/gsl-1.14.tar.gz'
-  mirror 'https://ftp.gnu.org/gnu/gsl/gsl-1.14.tar.gz'
-  sha1 'e1a600e4fe359692e6f0e28b7e12a96681efbe52'
+  homepage "https://www.gnu.org/software/gsl/"
+  url "http://ftpmirror.gnu.org/gsl/gsl-1.14.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/gsl/gsl-1.14.tar.gz"
+  sha256 "3d4a47afd9a1e7c73b97791b4180d8cc4d5f0e5db6027fe06437f1f3f957fafb"
 
   option :universal
 
@@ -12,8 +10,12 @@ class Gsl114 < Formula
     ENV.universal_binary if build.universal?
 
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make" # "make" and "make install" _must_ be done separately
-    system "make install"
+    system "make" # "make" and "make install" *must* be done separately
+    system "make", "install"
+  end
+
+  test do
+    system bin/"gsl-config", "--prefix", "--cflags", "--version"
   end
 end
 
