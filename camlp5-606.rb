f7ece466c804a836186c461f68527c18edc80259
BrewTestBot <brew-test-bot@googlegroups.com>
2015-03-24 12:17:44 +0000

    camlp5-606: add 6.06 bottle.

--- camlp5-606.rb
+++ camlp5-606.rb
@@ -3,6 +3,13 @@ class Camlp5606 < Formula
   url "http://pauillac.inria.fr/~ddr/camlp5/distrib/src/camlp5-6.06.tgz"
   sha256 "763f89ee6cde4ca063a50708c3fe252d55ea9f8037e3ae9801690411ea6180c5"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    sha256 "f8bf23c6b7c2be15b75b9473dd4c877c1f507e41ee569496365a7d230a2563af" => :yosemite
+    sha256 "454762aac44d7c87694b767a9d2b487385b0e1d33767060144e8b1c5ac467765" => :mavericks
+    sha256 "eb0015f2273826e0907229d5d7166b1f5497795c878c701462c8b9041ab6b57e" => :mountain_lion
+  end
+
   option "with-strict", "Compile in strict mode"
 
   depends_on "objective-caml312"
