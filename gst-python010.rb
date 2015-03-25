017ac2c6b0e6435ed6722f9a3e117cbd18ad4941
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-25 14:36:57 +0000

    gst-python010: modernise and bottle

--- gst-python010.rb
+++ gst-python010.rb
@@ -1,18 +1,16 @@
-require 'formula'
-
 class GstPython010 < Formula
-  homepage 'http://gstreamer.freedesktop.org/'
-  url 'http://gstreamer.freedesktop.org/src/gst-python/gst-python-0.10.22.tar.bz2'
-  sha1 '7012445d921ae1b325c162500750c9b0e777201f'
+  homepage "http://gstreamer.freedesktop.org/"
+  url "http://gstreamer.freedesktop.org/src/gst-python/gst-python-0.10.22.tar.bz2"
+  sha256 "8f26f519a5bccd770864317e098e5e307fc5ad1201eb96329634b6508b253178"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'homebrew/versions/gst-plugins-base010'
-  depends_on 'pygtk'
+  depends_on "pkg-config" => :build
+  depends_on "gst-plugins-base010"
+  depends_on "pygtk"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   def caveats; <<-EOS.undent
@@ -26,7 +24,7 @@ class GstPython010 < Formula
   end
 
   test do
-    (testpath/'test.py').write <<-EOS.undent
+    (testpath/"test.py").write <<-EOS.undent
       #!/usr/bin/env python
 
       import time
