37dd8ed5002604d717d830ec6f236e14e61c27cb
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-25 14:37:10 +0000

    gst-rtsp010: modernise and bottle

--- gst-rtsp010.rb
+++ gst-rtsp010.rb
@@ -1,19 +1,16 @@
-require 'formula'
-
 class GstRtsp010 < Formula
-  homepage 'http://gstreamer.freedesktop.org/'
-  url 'http://gstreamer.freedesktop.org/src/gst-rtsp/gst-rtsp-0.10.8.tar.bz2'
-  sha256 '9915887cf8515bda87462c69738646afb715b597613edc7340477ccab63a6617'
+  homepage "http://gstreamer.freedesktop.org/"
+  url "http://gstreamer.freedesktop.org/src/gst-rtsp/gst-rtsp-0.10.8.tar.bz2"
+  sha256 "9915887cf8515bda87462c69738646afb715b597613edc7340477ccab63a6617"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gettext'
-  depends_on 'homebrew/versions/gst-plugins-base010'
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gst-plugins-base010"
 
   def install
     system "./configure",  "--disable-debug", "--disable-dependency-tracking",
-                           "--prefix=#{prefix}",
-                           "--disable-gtk-doc"
+                           "--prefix=#{prefix}", "--disable-gtk-doc"
     system "make"
-    system "make install"
+    system "make", "install"
   end
 end
