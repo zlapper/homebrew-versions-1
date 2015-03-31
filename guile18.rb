eb7b98abafcd486ec756cb4130360a52c493bf20
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-31 15:28:53 +0100

    guile18: added test
    Closes #741.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- guile18.rb
+++ guile18.rb
@@ -1,18 +1,16 @@
-require 'formula'
-
 class Guile18 < Formula
-  homepage 'https://www.gnu.org/software/guile/'
-  url 'http://ftpmirror.gnu.org/guile/guile-1.8.8.tar.gz'
-  mirror 'https://ftp.gnu.org/gnu/guile/guile-1.8.8.tar.gz'
-  sha1 '548d6927aeda332b117f8fc5e4e82c39a05704f9'
+  homepage "https://www.gnu.org/software/guile/"
+  url "http://ftpmirror.gnu.org/guile/guile-1.8.8.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/guile/guile-1.8.8.tar.gz"
+  sha256 "c3471fed2e72e5b04ad133bbaaf16369e8360283679bcf19800bc1b381024050"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libtool' => :run
-  depends_on 'libffi'
-  depends_on 'libunistring'
-  depends_on 'bdw-gc'
-  depends_on 'gmp'
-  depends_on 'readline'
+  depends_on "pkg-config" => :build
+  depends_on "libtool" => :run
+  depends_on "libffi"
+  depends_on "libunistring"
+  depends_on "bdw-gc"
+  depends_on "gmp"
+  depends_on "readline"
 
   fails_with :llvm do
     build 2336
@@ -23,11 +21,23 @@ class Guile18 < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-libreadline-prefix=#{Formula["readline"].opt_prefix}"
-    system "make install"
+    system "make", "install"
 
     # A really messed up workaround required on OS X --mkhl
     Pathname.glob("#{lib}/*.dylib") do |dylib|
       lib.install_symlink dylib.basename => "#{dylib.basename(".dylib")}.so"
     end
+  end
+
+  test do
+    hello = testpath/"hello.scm"
+    hello.write <<-EOS.undent
+      (display "Hello World")
+      (newline)
+    EOS
+
+    ENV["GUILE_AUTO_COMPILE"] = "0"
+
+    system bin/"guile", hello
   end
 end
