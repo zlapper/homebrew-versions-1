cbd4a6f6ea4417e05d07571a2bc397d06ac8ccf1
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-02 01:41:36 +0100

    ppl011: added test
    Closes #745.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- ppl011.rb
+++ ppl011.rb
@@ -1,25 +1,22 @@
-require 'formula'
-
 class Ppl011 < Formula
-  homepage 'http://bugseng.com/products/ppl/'
+  homepage "http://bugseng.com/products/ppl/"
   # Track gcc infrastructure releases.
-  url 'http://bugseng.com/products/ppl/download/ftp/releases/0.11/ppl-0.11.tar.gz'
-  mirror 'ftp://gcc.gnu.org/pub/gcc/infrastructure/ppl-0.11.tar.gz'
-  sha1 'b3b12de9bcd156ed9254f76259835f40e162afc8'
+  url "http://bugseng.com/products/ppl/download/ftp/releases/0.11/ppl-0.11.tar.gz"
+  mirror "ftp://gcc.gnu.org/pub/gcc/infrastructure/ppl-0.11.tar.gz"
+  sha256 "3453064ac192e095598576c5b59ecd81a26b268c597c53df05f18921a4f21c77"
+  revision 1
 
-  keg_only 'Conflicts with ppl in main repository.'
+  conflicts_with "ppl10", :because => "They install the same binaries"
 
-  depends_on 'homebrew/dupes/m4' => :build if MacOS.version < :leopard
-  depends_on 'gmp4'
+  depends_on "homebrew/dupes/m4" => :build if MacOS.version < :leopard
+  depends_on "gmp4"
 
   # https://www.cs.unipr.it/mantis/view.php?id=596
-  # See also: https://github.com/Homebrew/homebrew/issues/27431
-  #
-  # Using a slightly different patch other than the one in upstream bug report
-  # to avoid autoreconf.
+  # https://github.com/Homebrew/homebrew/issues/27431
+  # Using different patch from upstream bug report to avoid autoreconf.
   patch do
     url "https://gist.githubusercontent.com/manphiz/9507743/raw/45081e12c2f1faf81e8536f365af05173c6dab5c/patch-ppl-flexible-array-clang_v2.patch"
-    sha1 "b60eaa2d5c5c0bef1be20ac9bee126e8af1c182e"
+    sha256 "db8ced5366ec4c3efb6fd20d3b4e440de3f8b9ec1d930a33b6a23d006dc25944"
   end
 
   def install
@@ -30,6 +27,10 @@ class Ppl011 < Formula
                           "--disable-ppl_lcdd",
                           "--disable-ppl_pips",
                           "--with-gmp-prefix=#{Formula["gmp4"].opt_prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"ppl-config", "--bindir", "--libdir", "--license"
   end
 end
