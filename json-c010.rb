9691207b04ddde221e103568d44661db9329aca3
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-02 18:49:23 +0100

    json-c010: add bottle

--- json-c010.rb
+++ json-c010.rb
@@ -1,16 +1,15 @@
-require 'formula'
-
 class JsonC010 < Formula
-  homepage 'https://github.com/json-c/json-c/wiki'
-  url 'https://github.com/downloads/json-c/json-c/json-c-0.10.tar.gz'
-  sha1 'f90f643c8455da21d57b3e8866868a944a93c596'
+  homepage "https://github.com/json-c/json-c/wiki"
+  url "https://github.com/downloads/json-c/json-c/json-c-0.10.tar.gz"
+  sha256 "274fc9d47c1911fad9caab4db117e4be5d6b68c4547eab0c508d79c4768e170c"
 
   def install
     system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
 
     # The Makefile forgets to install this header.
-    (include/'json').install 'json_object_iterator.h'
+    (include/"json").install "json_object_iterator.h"
   end
 end
