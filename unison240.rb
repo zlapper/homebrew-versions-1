6d1b617b2fdafde12553b56460fe8e3ba4f2cf38
Michael Ummels <michael@ummels.de>
2015-04-06 15:08:09 +0200

    unison240 2.40.128 (new formula)
    Needed because unison 2.48 is not protocol-compatible with unison 2.40.
    
    Closes #750.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ unison240.rb
@@ -0,0 +1,20 @@
+class Unison240 < Formula
+  homepage "http://www.cis.upenn.edu/~bcpierce/unison/"
+  url "http://www.seas.upenn.edu/~bcpierce/unison/download/releases/unison-2.40.128/unison-2.40.128.tar.gz"
+  sha256 "5a1ea828786b9602f2a42c2167c9e7643aba2c1e20066be7ce46de4779a5ca54"
+
+  depends_on "objective-caml" => :build
+
+  def install
+    ENV.j1
+    ENV.delete "CFLAGS" # ocamlopt reads CFLAGS but doesn't understand common options
+    ENV.delete "NAME" # https://github.com/Homebrew/homebrew/issues/28642
+    system "make", "./mkProjectInfo"
+    system "make", "UISTYLE=text"
+    bin.install "unison"
+  end
+
+  test do
+    system bin/"unison", "-version"
+  end
+end
