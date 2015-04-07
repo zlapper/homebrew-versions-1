468325fa8b3d408db5b91e72e5f6ce13f0477f7f
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-07 22:46:23 +0100

    isl014: add 0.14 bottle.

--- isl014.rb
+++ isl014.rb
@@ -5,6 +5,14 @@ class Isl014 < Formula
   mirror "ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.14.tar.bz2"
   sha256 "7e3c02ff52f8540f6a85534f54158968417fd676001651c8289c705bd0228f36"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "a6166d161841d901af54319a5434e955964712ae5cc19a88705d4f28669dc36b" => :yosemite
+    sha256 "cbcb2862709fa87913927a3ce261ac989f650a3506abde6c9eaa4da2d58b2b53" => :mavericks
+    sha256 "04997936c670ba1289574c526a62c69eae7e82c94c20b680df2f995df83b6d59" => :mountain_lion
+  end
+
   keg_only "Conflicts with isl in main repository."
 
   depends_on "gmp"
