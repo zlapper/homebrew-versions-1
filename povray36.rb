e8d74e96b0c4d48d16565d97f7d114d157a6f662
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-08 17:28:28 +0100

    povray36: add 3.6.1 bottle.

--- povray36.rb
+++ povray36.rb
@@ -3,6 +3,13 @@ class Povray36 < Formula
   url "http://www.povray.org/ftp/pub/povray/Old-Versions/Official-3.62/Unix/povray-3.6.1.tar.bz2"
   sha256 "4e8a7fecd44807343b6867e1f2440aa0e09613d6d69a7385ac48f4e5e7737a73"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    sha256 "b4c715205c566890afc176085d22dc5de47aa1db8b644edfb5223ac18f4b20e9" => :yosemite
+    sha256 "d2ca035fdb3a98363c038f93edc825309261019d2b329a2d13bd153f573f7a94" => :mavericks
+    sha256 "0ebfed92b039a7b61606dd0639de9fadcf0eeed9ba750dd24e5e5c168492b7a4" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "jpeg"
   depends_on "libtiff"
