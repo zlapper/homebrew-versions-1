ab172c9e1ecf961af4c5db3fdaf6f43c2c074114
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-09 20:17:25 +0100

    nettle3: add 3.1 bottle.

--- nettle3.rb
+++ nettle3.rb
@@ -4,6 +4,14 @@ class Nettle3 < Formula
   mirror "https://ftp.gnu.org/gnu/nettle/nettle-3.1.tar.gz"
   sha256 "f6859d4ec88e70805590af9862b4b8c43a2d1fc7991df0a7a711b1e7ca9fc9d3"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "e50d9eab1bbc727db7176ce14e64583e10305edf29f58478f44e81e67129f6fb" => :yosemite
+    sha256 "793f30f7cb3773776ad460a6cb65c63828d2030ebc6b09596f93c6925c22e95a" => :mavericks
+    sha256 "24923b41a6bd4a1c126dbc5ff2d202e7a49ff79d360654691b40503be06e5886" => :mountain_lion
+  end
+
   depends_on "gmp"
 
   keg_only "Conflicts with nettle in main repository and is not API compatible."
