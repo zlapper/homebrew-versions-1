b529e65f18271e6937869523e360f688b1a1fe67
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-16 05:23:50 +0100

    libpng12: add 1.2.52 bottle.

--- libpng12.rb
+++ libpng12.rb
@@ -3,6 +3,14 @@ class Libpng12 < Formula
   url "https://downloads.sourceforge.net/project/libpng/libpng12/1.2.52/libpng-1.2.52.tar.xz"
   sha256 "d4fb0fbf14057ad6d0319034188fc2aecddb493da8e3031b7b072ed28f510ec0"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "5dce4e061796e9ba41112b3f0dfd2ede83eff085e2c5fa19ceb5839a63867a41" => :yosemite
+    sha256 "88360558925258c5d2701088de404ac253952e572ed1917f3e6281f3b04d6732" => :mavericks
+    sha256 "37b4c67b1a155c392fe0813924b8e8bae8eb0bce565bf70003d712a7a28bd41e" => :mountain_lion
+  end
+
   keg_only :provided_by_osx
 
   option :universal
