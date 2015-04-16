3a39ca4b7b163435a9f8a8a7bb797b48b51c8c7d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-17 00:32:41 +0100

    tomcat7: add 7.0.61 bottle.

--- tomcat7.rb
+++ tomcat7.rb
@@ -5,6 +5,14 @@ class Tomcat7 < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.61/bin/apache-tomcat-7.0.61.tar.gz"
   sha256 "2528ad7434e44ab1198b5692d5f831ac605051129119fd81a00d4c75abe1c0e0"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "80ebcd42d337ab33d7bd0294193838e4d1a5945eafb44e07f254ba3e1229d725" => :yosemite
+    sha256 "55c01b38aac5ec11ffcfbc5a8de7eff817afa4f964c4a50210f5a5021a611c06" => :mavericks
+    sha256 "2f0d0aaa88b3a730d6c1b936c9251cba2968e15d89a5b1372e129870146969b3" => :mountain_lion
+  end
+
   option "with-fulldocs", "Install full documentation locally"
 
   resource "fulldocs" do
