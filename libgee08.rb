ade8f66541f9bc46d25506573b9ca081ffc97c4e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-16 05:23:50 +0100

    libgee08: add 0.8.0 bottle.

--- libgee08.rb
+++ libgee08.rb
@@ -3,6 +3,14 @@ class Libgee08 < Formula
   url "http://ftp.gnome.org/pub/GNOME/sources/libgee/0.8/libgee-0.8.0.tar.xz"
   sha256 "5e3707cbc1cebea86ab8865682cb28f8f80273869551c3698e396b5dc57831ea"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "71fa029e341ee653ac46ba119cbdc31a61d7477a2b5586dcb2ca1e900941e755" => :yosemite
+    sha256 "43d06d6d2d2e6cd6a0c67aa054749df8de92c609fa6fc8cf95e1a049894115ca" => :mavericks
+    sha256 "da728c343ef6dd70289da6ebd46207308d2d108e3131920db0383b03a6fbbbfe" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
   depends_on "gobject-introspection"
