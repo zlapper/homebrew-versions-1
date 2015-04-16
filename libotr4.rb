c9b9fd60ab482f32a1522a7c8802b10389b2480c
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-16 05:23:50 +0100

    libotr4: add 4.0.0 bottle.

--- libotr4.rb
+++ libotr4.rb
@@ -3,6 +3,14 @@ class Libotr4 < Formula
   url "https://otr.cypherpunks.ca/libotr-4.0.0.tar.gz"
   sha256 "3f911994409898e74527730745ef35ed75c352c695a1822a677a34b2cf0293b4"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "5e123d6699df1de4ba60a54a298969abed51582f26a9dea376f2edb3f64c8f89" => :yosemite
+    sha256 "4d49d2e193e6dcf5eeb60d096ffe8650dec6bd90145cfe120d6de38f37bca0bc" => :mavericks
+    sha256 "5ddaa483401d5d6c84a58f088f713f326869031eeefe3360c0d3a0e1112d4661" => :mountain_lion
+  end
+
   depends_on "libgcrypt"
 
   def install
