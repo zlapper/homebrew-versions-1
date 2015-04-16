06a1f8a99620c77ecd80a1b41c6968c4a00f8af3
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-16 04:58:47 +0100

    libpng12: add test
    Closes #775.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- libpng12.rb
+++ libpng12.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class Libpng12 < Formula
   homepage "http://www.libpng.org/pub/png/libpng.html"
   url "https://downloads.sourceforge.net/project/libpng/libpng12/1.2.52/libpng-1.2.52.tar.xz"
-  sha1 "8d19d762f836bb8900efc79ef8f1c3b64ee9f658"
+  sha256 "d4fb0fbf14057ad6d0319034188fc2aecddb493da8e3031b7b072ed28f510ec0"
 
   keg_only :provided_by_osx
 
@@ -11,11 +9,28 @@ class Libpng12 < Formula
 
   def install
     ENV.universal_binary if build.universal?
+
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make"
     system "make", "test"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <png.h>
+
+      int main()
+      {
+        png_structp png_ptr;
+        png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+        png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lpng", "-o", "test"
+    system "./test"
   end
 end
