0442be704abe76abdd01ab2b6355a8f09f9371a8
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-16 04:50:43 +0100

    libdvdcss12: use ssl/tls links

--- libdvdcss12.rb
+++ libdvdcss12.rb
@@ -1,13 +1,11 @@
-require "formula"
-
 class Libdvdcss12 < Formula
-  homepage "http://www.videolan.org/developers/libdvdcss.html"
-  url "http://download.videolan.org/pub/libdvdcss/1.2.13/libdvdcss-1.2.13.tar.bz2"
-  sha1 "1a4a5e55c7529da46386c1c333340eee2c325a77"
+  homepage "https://www.videolan.org/developers/libdvdcss.html"
+  url "https://download.videolan.org/pub/libdvdcss/1.2.13/libdvdcss-1.2.13.tar.bz2"
+  sha256 "84f1bba6cfef1df87f774fceaefc8e73c4cda32e8f6700b224ad0acb5511ba2c"
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
-    system "make install"
+    system "make", "install"
   end
 end
 
