c9a2c1f452832ca2a09f801111910c1b2eb303e1
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-16 04:45:02 +0100

    leptonica169: add conflict with homebrew/homebrew leptonica

--- leptonica169.rb
+++ leptonica169.rb
@@ -1,21 +1,21 @@
-require 'formula'
-
 class Leptonica169 < Formula
-  homepage 'http://www.leptonica.org/'
-  url 'http://www.leptonica.org/source/leptonica-1.69.tar.gz'
-  sha1 '91199f99d2e78b15b76ffa6fc4e86ee458a330e8'
+  homepage "http://www.leptonica.org/"
+  url "http://www.leptonica.org/source/leptonica-1.69.tar.gz"
+  sha256 "3eb7669dcda7e417f399bb3698414ea523270797dfd36c59b08ef37a3fe0a72d"
 
-  depends_on 'libpng' => :recommended
-  depends_on 'jpeg' => :recommended
-  depends_on 'libtiff' => :optional
+  depends_on "libpng" => :recommended
+  depends_on "jpeg" => :recommended
+  depends_on "libtiff" => :optional
 
-  conflicts_with 'osxutils',
+  conflicts_with "osxutils",
     :because => "both leptonica and osxutils ship a `fileinfo` executable."
+  conflicts_with "leptonica",
+    :because => "Differing versions of the same formula."
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
   end
 
   test do
