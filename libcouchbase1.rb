999dafa14502c17e7d4ccf976b9ccff2702cbc6f
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-16 04:48:40 +0100

    libcouchbase1: add conflict explanation

--- libcouchbase1.rb
+++ libcouchbase1.rb
@@ -1,21 +1,19 @@
-require 'formula'
-
 class Libcouchbase1 < Formula
-  homepage 'http://couchbase.com/develop/c/current'
-  url 'http://packages.couchbase.com/clients/c/libcouchbase-1.0.6.tar.gz'
-  sha1 '733479568b851382059b82bdd093b92081f9ac9c'
+  homepage "http://couchbase.com/develop/c/current"
+  url "http://packages.couchbase.com/clients/c/libcouchbase-1.0.6.tar.gz"
+  sha256 "ff86530a0fc21a2a6b719b389163a1f5172e379630b7dc91cbd2d16b5d586dc7"
 
-  conflicts_with 'libcouchbase'
+  conflicts_with "libcouchbase", :because => "Differing versions of the same formula."
 
-  depends_on 'libevent'
-  depends_on 'libvbucket'
+  depends_on "libevent"
+  depends_on "libvbucket"
 
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--disable-couchbasemock"
-    system "make install"
+    system "make", "install"
   end
 
   test do
