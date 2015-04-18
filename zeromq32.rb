9ac30d3f44274951ee78296234a56489df9ca55c
Alex Dunn <dunn.alex@gmail.com>
2015-04-17 22:28:44 -0700

    zeromq32 3.2.5
    Closes #780.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- zeromq32.rb
+++ zeromq32.rb
@@ -1,30 +1,28 @@
-require 'formula'
-
 class Zeromq32 < Formula
-  homepage 'http://www.zeromq.org/'
-  url 'http://download.zeromq.org/zeromq-3.2.4.tar.gz'
-  sha1 '08303259f08edd1faeac2e256f5be3899377135e'
+  homepage "http://www.zeromq.org/"
+  url "http://download.zeromq.org/zeromq-3.2.5.tar.gz"
+  sha256 "09653e56a466683edb2f87ee025c4de55b8740df69481b9d7da98748f0c92124"
 
   option :universal
-  option 'with-pgm', 'Build with PGM extension'
+  option "with-pgm", "Build with PGM extension"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libpgm' if build.with? 'pgm'
+  depends_on "pkg-config" => :build
+  depends_on "libpgm" if build.with? "pgm"
 
   def install
     ENV.universal_binary if build.universal?
 
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
-    if build.with? 'pgm'
+    if build.with? "pgm"
       # Use HB libpgm-5.2 because their internal 5.1 is b0rked.
-      ENV['OpenPGM_CFLAGS'] = %x[pkg-config --cflags openpgm-5.2].chomp
-      ENV['OpenPGM_LIBS'] = %x[pkg-config --libs openpgm-5.2].chomp
+      ENV["OpenPGM_CFLAGS"] = `pkg-config --cflags openpgm-5.2`.chomp
+      ENV["OpenPGM_LIBS"] = `pkg-config --libs openpgm-5.2`.chomp
       args << "--with-system-pgm"
     end
 
     system "./configure", *args
     system "make"
-    system "make install"
+    system "make", "install"
   end
 
   def caveats; <<-EOS.undent
@@ -33,5 +31,25 @@ class Zeromq32 < Formula
 
         ARCHFLAGS="-arch x86_64" gem install zmq -- --with-zmq-dir=#{opt_prefix}
     EOS
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <zmq.h>
+      #include <assert.h>
+
+      int main (void)
+      {
+          void *context = zmq_ctx_new ();
+          void *responder = zmq_socket (context, ZMQ_REP);
+          int rc = zmq_bind (responder, "tcp://*:5555");
+          assert (rc == 0);
+
+          return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-lzmq",
+           testpath/"test.c", "-o", "test"
+    system "./test"
   end
 end
