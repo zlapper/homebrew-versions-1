f19aa3f7e57d6fad5292d7b0099ba6818154bcf1
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-23 18:02:52 +0100

    scala210: style fixes

--- scala210.rb
+++ scala210.rb
@@ -1,9 +1,7 @@
-require 'formula'
-
 class Scala210 < Formula
-  homepage 'http://www.scala-lang.org/'
-  url 'http://www.scala-lang.org/files/archive/scala-2.10.5.tgz'
-  sha1 'e981301c06e3d06f4b24c0b983fe0bc3ca86b850'
+  homepage "http://www.scala-lang.org/"
+  url "http://www.scala-lang.org/files/archive/scala-2.10.5.tgz"
+  sha256 "918daf7de186305ff3c47bd0e6b03e74d6648c5239c050687b57b8fac2f87eb2"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-versions"
@@ -13,33 +11,33 @@ class Scala210 < Formula
     sha256 "878b3f7b0daee491594aaa80211ba49a8e3d4a7e9be8c025cf7ddbd227242bb3" => :mountain_lion
   end
 
-  keg_only 'Conflicts with scala in main repository.'
+  keg_only "Conflicts with scala in main repository."
 
-  option 'with-docs', 'Also install library documentation'
+  option "with-docs", "Also install library documentation"
 
-  resource 'docs' do
-    url 'http://www.scala-lang.org/files/archive/scala-docs-2.10.5.zip'
-    sha1 '0c104c185de295d35a44e0d0b8e743ff49e4b87c'
+  resource "docs" do
+    url "http://www.scala-lang.org/files/archive/scala-docs-2.10.5.zip"
+    sha256 "49263fc3b64f8d08bca906bb0c3f196eb6f3e699f810105a2b6d1d3d9cf030ca"
   end
 
-  resource 'completion' do
-    url 'https://raw.github.com/scala/scala-dist/27bc0c25145a83691e3678c7dda602e765e13413/completion.d/2.9.1/scala'
-    sha1 'e2fd99fe31a9fb687a2deaf049265c605692c997'
+  resource "completion" do
+    url "https://raw.github.com/scala/scala-dist/27bc0c25145a83691e3678c7dda602e765e13413/completion.d/2.9.1/scala"
+    sha256 "95aeba51165ce2c0e36e9bf006f2904a90031470ab8d10b456e7611413d7d3fd"
   end
 
   def install
     rm_f Dir["bin/*.bat"]
-    doc.install Dir['doc/*']
-    man1.install Dir['man/man1/*']
-    libexec.install Dir['*']
+    doc.install Dir["doc/*"]
+    man1.install Dir["man/man1/*"]
+    libexec.install Dir["*"]
     bin.install_symlink Dir["#{libexec}/bin/*"]
-    bash_completion.install resource('completion')
-    doc.install resource('docs') if build.with? 'docs'
+    bash_completion.install resource("completion")
+    doc.install resource("docs") if build.with? "docs"
 
-    # Set up an IntelliJ compatible symlink farm in 'idea'
-    idea = prefix/'idea'
-    idea.install_symlink libexec/'src', libexec/'lib'
-    (idea/'doc/scala-devel-docs').install_symlink doc => 'api'
+    # Set up an IntelliJ compatible symlink farm in "idea"
+    idea = prefix/"idea"
+    idea.install_symlink libexec/"src", libexec/"lib"
+    (idea/"doc/scala-devel-docs").install_symlink doc => "api"
   end
 
   def caveats; <<-EOS.undent
@@ -49,7 +47,7 @@ class Scala210 < Formula
   end
 
   test do
-    file = testpath/'hello.scala'
+    file = testpath/"hello.scala"
     file.write <<-EOS.undent
       object Computer {
         def main(args: Array[String]) {
