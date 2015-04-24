469b5605aa44fcfdc65b68d62b9e3e540e76a54a
Alex Dunn <dunn.alex@gmail.com>
2015-04-23 22:53:25 -0700

    glfw3 3.1.1
    Closes #793.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- glfw3.rb
+++ glfw3.rb
@@ -1,7 +1,7 @@
 class Glfw3 < Formula
   homepage "http://www.glfw.org/"
-  url "https://downloads.sourceforge.net/project/glfw/glfw/3.1/glfw-3.1.tar.bz2"
-  sha1 "bf7e8a7f79cbbfa68978aea2341e7fc7c6eef985"
+  url "https://downloads.sourceforge.net/project/glfw/glfw/3.1.1/glfw-3.1.1.tar.bz2"
+  sha256 "4a8516223c1df079efb398754f4533af7e943188ea9b5222e7f27c25e4822d61"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-versions"
@@ -13,9 +13,13 @@ class Glfw3 < Formula
   depends_on "cmake" => :build
 
   option :universal
-  option "static", "Build static library only (defaults to building dylib only)"
-  option "build-examples", "Build examples"
-  option "build-tests", "Build test programs"
+  option "without-shared-library", "Build static library only (defaults to building dylib only)"
+  option "with-examples", "Build examples"
+  option "with-tests", "Build test programs"
+
+  deprecated_option "build-examples" => "with-examples"
+  deprecated_option "static" => "without-shared-library"
+  deprecated_option "build-tests" => "with-tests"
 
   # make library name consistent
   patch :DATA
@@ -28,14 +32,32 @@ class Glfw3 < Formula
       -DGLFW_USE_MENUBAR=TRUE
     ]
     args << "-DGLFW_BUILD_UNIVERSAL=TRUE" if build.universal?
-    args << "-DBUILD_SHARED_LIBS=TRUE" unless build.include? "static"
-    args << "-DGLFW_BUILD_EXAMPLES=TRUE" if build.include? "build-examples"
-    args << "-DGLFW_BUILD_TESTS=TRUE" if build.include? "build-tests"
+    args << "-DBUILD_SHARED_LIBS=TRUE" if build.with? "shared-library"
+    args << "-DGLFW_BUILD_EXAMPLES=TRUE" if build.with? "examples"
+    args << "-DGLFW_BUILD_TESTS=TRUE" if build.with? "tests"
     args << "."
 
     system "cmake", *args
     system "make", "install"
-    libexec.install Dir["tests/*"] if build.include? "build-tests"
+    libexec.install Dir["tests/*"] if build.with? "tests"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #define GLFW_INCLUDE_GLU
+      #include <GLFW/glfw3.h>
+      #include <stdlib.h>
+      int main()
+      {
+        if (!glfwInit())
+          exit(EXIT_FAILURE);
+        glfwTerminate();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-lglfw3",
+           testpath/"test.c", "-o", "test"
+    system "./test"
   end
 end
 
@@ -55,6 +77,6 @@ index
 -    set(GLFW_LIB_NAME glfw3)
 -endif()
 +set(GLFW_LIB_NAME glfw3)
- 
+
  #--------------------------------------------------------------------
  # Add subdirectories
