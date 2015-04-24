d0ef01bf69429f7c55948d9f74713ce9dd526eae
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-24 03:53:47 +0100

    cloog-ppl015: style fixes
    Generating a bottle is actually the more important part here.
    A bottle should mean all of gcc46's deps are bottled, and may also
    help #515.
    
    Closes #792.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- cloog-ppl015.rb
+++ cloog-ppl015.rb
@@ -1,28 +1,23 @@
-require 'formula'
-
 class CloogPpl015 < Formula
-  homepage 'http://repo.or.cz/w/cloog-ppl.git'
-  url 'ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-ppl-0.15.11.tar.gz'
-  mirror 'http://gcc.cybermirror.org/infrastructure/cloog-ppl-0.15.11.tar.gz'
-  sha1 '42fa476a79a1d52da41608a946dcb47c70f7e3b9'
+  homepage "http://repo.or.cz/w/cloog-ppl.git"
+  url "ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-ppl-0.15.11.tar.gz"
+  mirror "http://gcc.cybermirror.org/infrastructure/cloog-ppl-0.15.11.tar.gz"
+  sha256 "7cd634d0b2b401b04096b545915ac67f883556e9a524e8e803a6bf6217a84d5f"
 
-  keg_only 'Conflicts with cloog in main repository.'
+  keg_only "Conflicts with cloog in main repository."
 
-  depends_on 'gmp4'
-  depends_on 'ppl011'
+  depends_on "gmp4"
+  depends_on "ppl011"
 
   def install
-    gmp4 = Formula["gmp4"]
-    ppl011 = Formula["ppl011"]
-
-    args = [
-      "--prefix=#{prefix}",
-      "--with-gmp=#{gmp4.opt_prefix}",
-      "--with-ppl=#{ppl011.opt_prefix}"
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-gmp=#{Formula["gmp4"].opt_prefix}"
+      --with-ppl=#{Formula["ppl011"].opt_prefix}"
     ]
 
     system "./configure", *args
     system "make", "install"
   end
-
 end
