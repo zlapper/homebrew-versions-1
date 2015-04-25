8485cf2fcd66f25f16d96a909869fbbb38281f4e
Dominyk Tiller <dominyktiller@gmail.com>
2015-04-25 21:21:31 +0100

    mkvtoolnix58: modernise ruby dependency
    Closes #797.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- mkvtoolnix58.rb
+++ mkvtoolnix58.rb
@@ -1,46 +1,24 @@
-require 'formula'
-
-class Ruby19 < Requirement
-  fatal true
-  default_formula "ruby"
-
-  satisfy :build_env => false do
-    next unless which "ruby"
-    version = /\d\.\d/.match `ruby --version 2>&1`
-    next unless version
-    Version.new(version.to_s) >= Version.new("1.9")
-  end
-
-  env do
-    ENV.prepend_path "PATH", which("ruby").dirname
-  end
-
-  def message; <<-EOS.undent
-    The mkvtoolnix buildsystem needs Ruby >=1.9
-    EOS
-  end
-end
-
 class Mkvtoolnix58 < Formula
-  homepage 'http://www.bunkus.org/videotools/mkvtoolnix/'
-  url 'http://www.bunkus.org/videotools/mkvtoolnix/sources/mkvtoolnix-5.8.0.tar.bz2'
-  sha1 '5077bcc6403acc18a867a309861e86b593227bb9'
+  homepage "https://www.bunkus.org/videotools/mkvtoolnix/"
+  url "https://www.bunkus.org/videotools/mkvtoolnix/sources/mkvtoolnix-5.8.0.tar.bz2"
+  sha256 "3c9ec7e4c035b82a35850c5ada98a29904edc44a0d1c9b900ed05d56e6274960"
 
-  depends_on 'pkg-config' => :build
-  depends_on Ruby19
-  depends_on 'libvorbis'
-  depends_on 'flac' => :optional
-  depends_on 'lzo' => :optional
+  depends_on :ruby => "1.9"
+  depends_on "pkg-config" => :build
+  depends_on "libvorbis"
+  depends_on "flac" => :optional
+  depends_on "lzo" => :optional
+
   # On Mavericks, the bottle (without c++11) can be used
   # because mkvtoolnix is linked against libc++ by default
   if MacOS.version >= "10.9"
-    depends_on 'boost'
-    depends_on 'libmatroska'
-    depends_on 'libebml'
+    depends_on "boost155"
+    depends_on "libmatroska"
+    depends_on "libebml"
   else
-    depends_on 'boost' => 'c++11'
-    depends_on 'libmatroska' => 'c++11'
-    depends_on 'libebml' => 'c++11'
+    depends_on "boost155" => "c++11"
+    depends_on "libmatroska" => "c++11"
+    depends_on "libebml" => "c++11"
   end
 
   needs :cxx11
@@ -48,19 +26,15 @@ class Mkvtoolnix58 < Formula
   def install
     ENV.cxx11
 
-    ENV['ZLIB_CFLAGS'] = '-I/usr/include'
-    ENV['ZLIB_LIBS'] = '-L/usr/lib -lz'
-
-    boost = Formula["boost"].opt_prefix
-
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--disable-gui",
                           "--disable-wxwidgets",
                           "--without-curl",
-                          "--with-boost=#{boost}"
+                          "--with-boost=#{Formula["boost155"].opt_prefix}"
+
     system "./drake", "-j#{ENV.make_jobs}"
-    system "./drake install"
+    system "./drake", "install"
   end
 
   test do
