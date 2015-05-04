85747b556d2de41c41b689065eab2bd43255bac8
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-04 22:57:42 +0100

    perl516: add test

--- perl516.rb
+++ perl516.rb
@@ -1,30 +1,43 @@
-require 'formula'
-
 class Perl516 < Formula
-  homepage 'http://www.perl.org/'
-  url 'http://www.cpan.org/src/5.0/perl-5.16.3.tar.gz'
-  sha1 '83678adf56d3dc51f47a90444a891f4fe16868da'
+  homepage "https://www.perl.org/"
+  url "http://www.cpan.org/src/5.0/perl-5.16.3.tar.gz"
+  sha256 "69cf08dca0565cec2c5c6c2f24b87f986220462556376275e5431cc2204dedb6"
 
-  keg_only 'System provides Perl. Also conflicts with other Perl versions.'
+  keg_only :provided_by_osx,
+    "OS X ships Perl and overriding that can cause unintended issues"
 
-  option 'use-dtrace', 'Build with DTrace probes'
+  option "with-dtrace", "Build with DTrace probes"
+  option "with-tests", "Build and run the test suite"
+
+  deprecated_option "use-dtrace" => "with-dtrace"
 
   def install
     args = [
-      '-des',
+      "-des",
       "-Dprefix=#{prefix}",
       "-Dman1dir=#{man1}",
       "-Dman3dir=#{man3}",
-      '-Duseshrplib',
-      '-Duselargefiles',
-      '-Dusethreads'
+      "-Duseshrplib",
+      "-Duselargefiles",
+      "-Dusethreads",
     ]
 
-    args << '-Dusedtrace' if build.include? 'use-dtrace'
+    args << "-Dusedtrace" if build.with? "dtrace"
 
-    system './Configure', *args
+    system "./Configure", *args
     system "make"
-    system "make test"
-    system "make install"
+    system "make", "test" if build.with? "tests"
+    system "make", "install"
+  end
+
+  def caveats; <<-EOS.undent
+    By default Perl installs modules in your HOME dir. If this is an issue run:
+      #{bin}/cpan o conf init
+    EOS
+  end
+
+  test do
+    (testpath/"test.pl").write "print 'Perl is not an acronym, but JAPH is a Perl acronym!';"
+    system "#{bin}/perl", "test.pl"
   end
 end
