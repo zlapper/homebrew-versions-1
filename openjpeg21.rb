01a914371c4b9256fb93a7baf0717382d69b3d16
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-04 22:43:02 +0100

    openjpeg21: bottle
    Closes #813.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- openjpeg21.rb
+++ openjpeg21.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class Openjpeg21 < Formula
   homepage "http://www.openjpeg.org/"
   url "https://downloads.sourceforge.net/project/openjpeg.mirror/2.1.0/openjpeg-2.1.0.tar.gz"
-  sha1 "c2a255f6b51ca96dc85cd6e85c89d300018cb1cb"
+  sha256 "1232bb814fd88d8ed314c94f0bfebb03de8559583a33abbe8c64ef3fc0a8ff03"
 
   depends_on "cmake" => :build
   depends_on "little-cms2"
