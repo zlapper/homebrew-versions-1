9fe827d136967bc3230a228e039da876fc7c70ca
Alex Dunn <dunn.alex@gmail.com>
2015-05-09 11:40:48 -0700

    play13 1.3.1
    Closes #819.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- play13.rb
+++ play13.rb
@@ -1,7 +1,7 @@
 class Play13 < Formula
   homepage "http://www.playframework.org/"
-  url "http://downloads.typesafe.com/play/1.3.0/play-1.3.0.zip"
-  sha1 "0637fc40bc98675f56d3f408e73be6e6c067c789"
+  url "http://downloads.typesafe.com/play/1.3.1/play-1.3.1.zip"
+  sha256 "9dae87f659cca29cd0144ef58491b714072ccb786eef4ccfa7741da1a2301ec0"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-versions"
@@ -10,12 +10,26 @@ class Play13 < Formula
     sha1 "260e3924f28c9f7df7b54a7e90d4a03a764691e7" => :mountain_lion
   end
 
+  conflicts_with "sox", :because => "Both install a `play` executable"
+  conflicts_with "play12", :because => "Both install a `play` executable"
+  conflicts_with "play22", :because => "Both install a `play` executable"
+
   def install
     rm_rf "python" # we don't need the bundled Python for windows
     rm Dir["*.bat"]
     libexec.install Dir["*"]
-    bin.mkpath
-    chmod 0755, libexec+"play"
-    ln_s libexec+"play", bin
+    chmod 0755, libexec/"play"
+    bin.install_symlink libexec/"play"
+  end
+
+  test do
+    require "open3"
+    Open3.popen3("#{bin}/play new #{testpath}/app") do |stdin, _, _|
+      stdin.write "\n"
+      stdin.close
+    end
+    %W[app conf lib public test].each do |d|
+      File.directory? testpath/"app/#{d}"
+    end
   end
 end
