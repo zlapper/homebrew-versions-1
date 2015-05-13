8372521a506bd648bc64c44636dae4be48e6973d
Dominyk Tiller <dominyktiller@gmail.com>
2015-05-13 19:25:37 +0100

    protobuf250: use correct upstream url

--- protobuf250.rb
+++ protobuf250.rb
@@ -1,12 +1,15 @@
-require "formula"
-
 class Protobuf250 < Formula
-  homepage "https://code.google.com/p/protobuf/"
-  url "https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.bz2"
-  sha1 "62c10dcdac4b69cc8c6bb19f73db40c264cb2726"
+  homepage "https://github.com/google/protobuf"
+  url "https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.bz2"
+  sha256 "13bfc5ae543cf3aa180ac2485c0bc89495e3ae711fc6fab4f8ffe90dfb4bb677"
+
+  conflicts_with "protobuf", :because => "conflicts with protobuf in main repository."
 
   option :universal
   option :cxx11
+
+  # this will double the build time approximately if enabled
+  option "with-check", "Run build-time check"
 
   depends_on :python => :optional
 
@@ -17,7 +20,6 @@ class Protobuf250 < Formula
   def install
     # Don't build in debug mode. See:
     # https://github.com/Homebrew/homebrew/issues/9279
-    # http://code.google.com/p/protobuf/source/browse/trunk/configure.ac#61
     ENV.prepend "CXXFLAGS", "-DNDEBUG"
     ENV.universal_binary if build.universal?
     ENV.cxx11 if build.cxx11?
@@ -27,20 +29,23 @@ class Protobuf250 < Formula
                           "--prefix=#{prefix}",
                           "--with-zlib"
     system "make"
+    system "make", "check" if build.with? "check" || build.bottle?
     system "make", "install"
 
     # Install editor support and examples
-    doc.install %w( editors examples )
+    doc.install "editors", "examples"
 
     if build.with? "python"
       chdir "python" do
         ENV["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "cpp"
         ENV.append_to_cflags "-I#{include}"
         ENV.append_to_cflags "-L#{lib}"
-        system "python", "setup.py", "build"
-        system "python", "setup.py", "install", "--prefix=#{prefix}",
-               "--single-version-externally-managed", "--record=installed.txt"
+        args = Language::Python.setup_install_args libexec
+        system "python", *args
       end
+      site_packages = "lib/python2.7/site-packages"
+      pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
+      (prefix/site_packages/"homebrew-protobuf.pth").write pth_contents
     end
   end
 
@@ -48,5 +53,19 @@ class Protobuf250 < Formula
     Editor support and examples have been installed to:
       #{doc}
     EOS
+  end
+
+  test do
+    (testpath/"test.proto").write <<-EOS.undent
+      package test;
+      message TestCase {
+        required string name = 4;
+      }
+      message Test {
+        repeated TestCase case = 1;
+      }
+    EOS
+    system bin/"protoc", "test.proto", "--cpp_out=."
+    system "python", "-c", "import google.protobuf" if build.with? "python"
   end
 end
