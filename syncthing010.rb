ffd56f8427b7f4290442f79d9d7d9583de5cb228
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-13 22:43:43 +0100

    syncthing010: add 0.10.31 bottle.

--- syncthing010.rb
+++ syncthing010.rb
@@ -4,6 +4,14 @@ class Syncthing010 < Formula
       :tag => "v0.10.31",
       :revision => "2470875d1436a1757e07bec09e388b75d5ac12c0"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "acea7eb8d47d40de31719437e9d12f74a0f76be0df97f33c5cedc07a5a6e4246" => :yosemite
+    sha256 "48ce978c9eec4f574517e410527eee37189a11a0db2bcbabcef3d3b10a581672" => :mavericks
+    sha256 "623b069e7a658c7760606fb9adfacb810c9b80e4c383d2cde780177644bfe92c" => :mountain_lion
+  end
+
   depends_on "go" => :build
   depends_on :hg => :build
 
