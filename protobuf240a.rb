eef499bd9b48f52f2d98ad838e584d4cd539e063
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-15 17:13:49 +0100

    protobuf240a: add 2.4.0a bottle.

--- protobuf240a.rb
+++ protobuf240a.rb
@@ -3,6 +3,14 @@ class Protobuf240a < Formula
   url "https://launchpad.net/ubuntu/+archive/primary/+files/protobuf_2.4.0a.orig.tar.gz"
   sha256 "bb20941d4958bcf2fa76fde251bb4f71463d4fe28884a015c7335894344cffcb"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    cellar :any
+    sha256 "19d29ffebfeb9beab833b7e8254ad19de11afea643bab6381dd51858e6a680db" => :yosemite
+    sha256 "8e6160ffa4e7d1eeb4dd293b07fe4dd5fd78b134ee5a0af6e77053a57a0ccea2" => :mavericks
+    sha256 "5de5e189c764f8f1922754a31f5d4151b9d65cf60b9fccd54ec727dbbf54ed0e" => :mountain_lion
+  end
+
   keg_only "Conflicts with protobuf in main repository."
 
   option :universal
