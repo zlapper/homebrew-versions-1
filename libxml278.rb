682f1fbdb06501eb1fd89059db61a7839f9c27ba
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2015-06-02 10:34:18 -0300

    libxml278 2.7.8 (new formula)
    Very useful for compiling PHP 5.2 and other old stuff as pointed in:
    http://www.devsumo.com/technotes/2014/06/php-node-lo-compiler-errors-when-building-on-os-x-10-9/
    
    Closes #848.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ libxml278.rb
@@ -0,0 +1,56 @@
+class Libxml278 < Formula
+  desc "Libxml2 is the XML C parser and toolkit"
+  homepage "http://xmlsoft.org"
+  url "http://xmlsoft.org/sources/libxml2-2.7.8.tar.gz"
+  mirror "ftp://xmlsoft.org/libxml2/libxml2-2.7.8.tar.gz"
+  sha256 "cda23bc9ebd26474ca8f3d67e7d1c4a1f1e7106364b690d822e009fdc3c417ec"
+
+  depends_on :python => :optional
+
+  keg_only :provided_by_osx
+
+  fails_with :llvm do
+    build 2326
+    cause "Undefined symbols when linking"
+  end
+
+  option :universal
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--without-python"
+    system "make"
+    ENV.deparallelize
+    system "make", "install"
+
+    if build.with? "python"
+      cd "python" do
+        # We need to insert our include dir first
+        inreplace "setup.py", "includes_dir = [", "includes_dir = ['#{include}', '#{MacOS.sdk_path}/usr/include',"
+        system "python", "setup.py", "install", "--prefix=#{prefix}"
+      end
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <libxml/tree.h>
+
+      int main()
+      {
+        xmlDocPtr doc = xmlNewDoc(BAD_CAST "1.0");
+        xmlNodePtr root_node = xmlNewNode(NULL, BAD_CAST "root");
+        xmlDocSetRootElement(doc, root_node);
+        xmlFreeDoc(doc);
+        return 0;
+      }
+    EOS
+    args = `#{bin}/xml2-config --cflags --libs`.split
+    args += %w[test.c -o test]
+    system ENV.cc, *args
+    system "./test"
+  end
+end
