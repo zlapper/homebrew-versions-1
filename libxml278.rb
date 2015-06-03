2e554555407a53fb35bcf398da00914427a1bd80
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-03 15:53:27 +0100

    libxml278: add 2.7.8 bottle.

--- libxml278.rb
+++ libxml278.rb
@@ -5,6 +5,13 @@ class Libxml278 < Formula
   mirror "ftp://xmlsoft.org/libxml2/libxml2-2.7.8.tar.gz"
   sha256 "cda23bc9ebd26474ca8f3d67e7d1c4a1f1e7106364b690d822e009fdc3c417ec"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-versions"
+    sha256 "e2afe51f87e12b0bd5f5a06803f397a0a2658ee04999a6b50569a0acd3afb4f1" => :yosemite
+    sha256 "3e8e3fe83ad886f416ccd475052f68f3bfcf30be8f4bb7a3e67d8e76eadac783" => :mavericks
+    sha256 "1ae08e74cf7b71d5c139db86a81f83098a4438546c70e030d3dc4552d49a404b" => :mountain_lion
+  end
+
   depends_on :python => :optional
 
   keg_only :provided_by_osx
