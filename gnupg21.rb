316dddf599f0c34a224374ef1ee6267cea6d1364
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-06 19:37:02 +0100

    gnupg21: remove redundant comment
    Closes https://github.com/Homebrew/homebrew/issues/40316

--- gnupg21.rb
+++ gnupg21.rb
@@ -1,4 +1,5 @@
 class Gnupg21 < Formula
+  desc "GNU Privacy Guard: a free PGP replacement"
   homepage "https://www.gnupg.org/"
   url "ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.4.tar.bz2"
   mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.4.tar.bz2"
@@ -111,7 +112,7 @@ class Gnupg21 < Formula
       https://www.gnupg.org/faq/whats-new-in-2.1.html
 
     If you are upgrading to gnupg21 from gnupg2 you should execute:
-      `killall gpg-agent && gpg-agent --daemon --use-standard-socket`
+      `killall gpg-agent && gpg-agent --daemon`
     After install. See:
       https://github.com/Homebrew/homebrew-versions/issues/681
     EOS
