1dc3065c8800df5346c36a2a6dfb5723078f9c7e
Tamir Duberstein <tamird@gmail.com>
2015-06-10 19:41:19 -0400

    mysql55: updates from core
    Closes #859.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- mysql55.rb
+++ mysql55.rb
@@ -1,4 +1,5 @@
 class Mysql55 < Formula
+  desc "Open source relational database management system"
   homepage "http://dev.mysql.com/doc/refman/5.5/en/"
   url "https://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.40.tar.gz"
   sha256 "4d3721761a31c28866e8ee0fde47558a3e9047aaef3b89135ae8d45eebb74960"
@@ -17,24 +18,30 @@ class Mysql55 < Formula
   option :universal
   option "with-tests", "Build with unit tests"
   option "with-embedded", "Build the embedded server"
-  option "with-libedit", "Compile with editline wrapper instead of readline"
   option "with-archive-storage-engine", "Compile with the ARCHIVE storage engine enabled"
   option "with-blackhole-storage-engine", "Compile with the BLACKHOLE storage engine enabled"
   option "with-local-infile", "Build with local infile loading support"
+  option "with-memcached", "Enable innodb-memcached support"
   option "with-debug", "Build with debug support"
 
   deprecated_option "enable-local-infile" => "with-local-infile"
+  deprecated_option "enable-memcached" => "with-memcached"
   deprecated_option "enable-debug" => "with-debug"
 
   keg_only "Conflicts with mysql, mariadb, percona-server, mysql-cluster, etc."
 
   fails_with :llvm do
     build 2326
-    cause "https://github.com/mxcl/homebrew/issues/issue/144"
+    cause "https://github.com/Homebrew/homebrew/issues/issue/144"
+  end
+
+  def datadir
+    var/"mysql"
   end
 
   def install
-    # Don't hard-code the libtool path
+    # Don't hard-code the libtool path. See:
+    # https://github.com/Homebrew/homebrew/issues/20185
     inreplace "cmake/libutils.cmake",
       "COMMAND /usr/bin/libtool -static -o ${TARGET_LOCATION}",
       "COMMAND libtool -static -o ${TARGET_LOCATION}"
@@ -43,18 +50,26 @@ class Mysql55 < Formula
     # compilation of gems and other software that queries `mysql-config`.
     ENV.minimal_optimization
 
-    args = [".",
-            "-DCMAKE_INSTALL_PREFIX=#{prefix}",
-            "-DMYSQL_DATADIR=#{var}/#{name}",
-            "-DINSTALL_MANDIR=#{man}",
-            "-DINSTALL_DOCDIR=#{doc}",
-            "-DINSTALL_INFODIR=#{info}",
-            # CMake prepends prefix, so use share.basename
-            "-DINSTALL_MYSQLSHAREDIR=#{share.basename}/mysql",
-            "-DWITH_SSL=yes",
-            "-DDEFAULT_CHARSET=utf8",
-            "-DDEFAULT_COLLATION=utf8_general_ci",
-            "-DSYSCONFDIR=#{etc}"]
+    # -DINSTALL_* are relative to prefix
+    args = %W[
+      .
+      -DCMAKE_INSTALL_PREFIX=#{prefix}
+      -DCMAKE_FIND_FRAMEWORK=LAST
+      -DCMAKE_VERBOSE_MAKEFILE=ON
+      -DMYSQL_DATADIR=#{datadir}
+      -DINSTALL_INCLUDEDIR=#{include}/mysql
+      -DINSTALL_MANDIR=#{man}
+      -DINSTALL_DOCDIR=#{doc}
+      -DINSTALL_INFODIR=#{info}
+      -DINSTALL_MYSQLSHAREDIR=#{share}/mysql
+      -DWITH_SSL=yes
+      -DWITH_SSL=system
+      -DDEFAULT_CHARSET=utf8
+      -DDEFAULT_COLLATION=utf8_general_ci
+      -DSYSCONFDIR=#{etc}
+      -DCOMPILATION_COMMENT=Homebrew
+      -DWITH_EDITLINE=system
+    ]
 
     # To enable unit testing at build, we need to download the unit testing suite
     if build.with? "tests"
@@ -65,9 +80,6 @@ class Mysql55 < Formula
 
     # Build the embedded server
     args << "-DWITH_EMBEDDED_SERVER=ON" if build.with? "embedded"
-
-    # Compile with readline unless libedit is explicitly chosen
-    args << "-DWITH_READLINE=yes" if build.without? "libedit"
 
     # Compile with ARCHIVE engine enabled if chosen
     args << "-DWITH_ARCHIVE_STORAGE_ENGINE=1" if build.with? "archive-storage-engine"
@@ -84,6 +96,9 @@ class Mysql55 < Formula
     # Build with local infile loading support
     args << "-DENABLED_LOCAL_INFILE=1" if build.with? "local-infile"
 
+    # Build with memcached support
+    args << "-DWITH_INNODB_MEMCACHED=1" if build.with? "memcached"
+
     # Build with debug support
     args << "-DWITH_DEBUG=1" if build.with? "debug"
 
@@ -92,32 +107,32 @@ class Mysql55 < Formula
     system "make", "install"
 
     # Don't create databases inside of the prefix!
-    # See: https://github.com/mxcl/homebrew/issues/4975
-    rm_rf prefix+"data"
+    # See: https://github.com/Homebrew/homebrew/issues/4975
+    rm_rf prefix/"data"
 
     # Link the setup script into bin
-    ln_s prefix+"scripts/mysql_install_db", bin+"mysql_install_db"
+    bin.install_symlink prefix/"scripts/mysql_install_db"
+
     # Fix up the control script and link into bin
     inreplace "#{prefix}/support-files/mysql.server" do |s|
       s.gsub!(/^(PATH=".*)(")/, "\\1:#{HOMEBREW_PREFIX}/bin\\2")
       # pidof can be replaced with pgrep from proctools on Mountain Lion
       s.gsub!(/pidof/, "pgrep") if MacOS.version >= :mountain_lion
     end
-    ln_s "#{prefix}/support-files/mysql.server", bin
 
-    # Move mysqlaccess to libexec
-    mv "#{bin}/mysqlaccess", libexec
-    mv "#{bin}/mysqlaccess.conf", libexec
+    bin.install_symlink prefix/"support-files/mysql.server"
+
+    libexec.install bin/"mysqlaccess"
+    libexec.install bin/"mysqlaccess.conf"
   end
 
   def post_install
-    # Make sure the var/mysql directory exists
-    (var/name).mkpath
-
-    unless File.exist? "#{var}/#{name}/mysql/user.frm"
+    # Make sure the datadir exists
+    datadir.mkpath
+    unless (datadir/"mysql/user.frm").exist?
       ENV["TMPDIR"] = nil
-      system "#{bin}/mysql_install_db", "--verbose", "--user=#{ENV["USER"]}",
-        "--basedir=#{prefix}", "--datadir=#{var}/#{name}", "--tmpdir=/tmp"
+      system bin/"mysql_install_db", "--verbose", "--user=#{ENV["USER"]}",
+        "--basedir=#{prefix}", "--datadir=#{datadir}", "--tmpdir=/tmp"
     end
   end
 
@@ -143,20 +158,21 @@ class Mysql55 < Formula
       <string>#{plist_name}</string>
       <key>ProgramArguments</key>
       <array>
-        <string>#{opt_prefix}/bin/mysqld_safe</string>
+        <string>#{opt_bin}/mysqld_safe</string>
         <string>--bind-address=127.0.0.1</string>
+        <string>--datadir=#{datadir}</string>
       </array>
       <key>RunAtLoad</key>
       <true/>
       <key>WorkingDirectory</key>
-      <string>#{var}</string>
+      <string>#{datadir}</string>
     </dict>
     </plist>
     EOS
   end
 
   test do
-    (prefix+"mysql-test").cd do
+    (prefix/"mysql-test").cd do
       system "./mysql-test-run.pl", "status"
     end
   end
