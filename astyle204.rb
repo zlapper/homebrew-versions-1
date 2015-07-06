a5a2f98658d5ab58cdf5a0ffb22d99cce14a2f75
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-06 15:00:13 +0100

    astyle204: add 2.04 bottle.

--- astyle204.rb
+++ astyle204.rb
@@ -4,6 +4,13 @@ class Astyle204 < Formula
   url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_macosx.tar.gz"
   sha256 "e0ba90723463172fd8a063897092284993eeebb87c63cf26ee36f555b0d89368"
 
+  bottle do
+    cellar :any
+    sha256 "e7a38bf55b8cce4d0f2c1ac3a69b83670c2809e800d45ecf825f78c3c329e233" => :yosemite
+    sha256 "11f398760d41131a866bd23b6de721e6518e0c86c54fb867eb28ed95b508addc" => :mavericks
+    sha256 "98fbe0b2ecc638bc09894bf6def96e9bcab59a501e1373582b8df5435cd7b188" => :mountain_lion
+  end
+
   conflicts_with "astyle"
 
   def install
