a7cae5da90a163538bedf795a9655015ba9ac3e0
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-09 16:58:00 +0100

    astyle204: minor nits

--- astyle204.rb
+++ astyle204.rb
@@ -1,5 +1,5 @@
 class Astyle204 < Formula
-  desc "a source code indenter, formatter, and beautifier."
+  desc "Source code indenter, formatter, and beautifier."
   homepage "http://astyle.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_macosx.tar.gz"
   sha256 "e0ba90723463172fd8a063897092284993eeebb87c63cf26ee36f555b0d89368"
@@ -11,7 +11,7 @@ class Astyle204 < Formula
     sha256 "98fbe0b2ecc638bc09894bf6def96e9bcab59a501e1373582b8df5435cd7b188" => :mountain_lion
   end
 
-  conflicts_with "astyle"
+  conflicts_with "astyle", :because => "Differing versions of same formulae"
 
   def install
     cd "src" do
