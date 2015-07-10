d629fe81640d75ab18ce79dacc7456cd89939fc5
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-10 19:12:18 +0100

    maven2: ssl/tls + sha256

--- maven2.rb
+++ maven2.rb
@@ -1,28 +1,25 @@
-require 'formula'
-
 class Maven2 < Formula
-  homepage 'http://maven.apache.org/'
-  url 'http://archive.apache.org/dist/maven/binaries/apache-maven-2.2.1-bin.tar.gz'
-  sha1 '3ac63025e5860c4d856e172ab556d14b52f9b1f1'
+  desc "Java-based project management"
+  homepage "https://maven.apache.org/"
+  url "https://archive.apache.org/dist/maven/binaries/apache-maven-2.2.1-bin.tar.gz"
+  sha256 "b9a36559486a862abfc7fb2064fd1429f20333caae95ac51215d06d72c02d376"
+
+  conflicts_with "maven", :because => "Differing versions of same formula"
+
+  depends_on :java
 
   def install
     # Remove windows files
     rm_f Dir["bin/*.bat"]
 
     # Install jars in libexec to avoid conflicts
-    prefix.install %w{ NOTICE.txt LICENSE.txt README.txt }
-    libexec.install Dir['*']
+    prefix.install %w[NOTICE.txt LICENSE.txt README.txt]
+    libexec.install Dir["*"]
 
     # Symlink binaries
     bin.mkpath
     Dir["#{libexec}/bin/*"].each do |f|
       ln_s f, bin+File.basename(f)
     end
-  end
-
-  def caveats; <<-EOS.undent
-    WARNING: This older version will conflict with Maven if installed at the
-    same time.
-    EOS
   end
 end
