a88b1f9a01c3d7f0e90d70a8e16dbbde22c6e692
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-10 19:16:02 +0100

    maven31: ssl/tls + sha256

--- maven31.rb
+++ maven31.rb
@@ -1,9 +1,12 @@
-require 'formula'
-
 class Maven31 < Formula
-  homepage 'http://maven.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz'
-  sha1 '630eea2107b0742acb315b214009ba08602dda5f'
+  desc "Java-based project management"
+  homepage "https://maven.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz"
+  sha256 "077ed466455991d5abb4748a1d022e2d2a54dc4d557c723ecbacdc857c61d51b"
+
+  conflicts_with "maven", :because => "Differing versions of same formula"
+
+  depends_on :java
 
   def install
     # Remove windows files
@@ -13,13 +16,7 @@ class Maven31 < Formula
     chmod 0644, "conf/settings.xml"
 
     prefix.install_metafiles
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.install_symlink Dir["#{libexec}/bin/*"]
-  end
-
-  def caveats; <<-EOS.undent
-    WARNING: This older version will conflict with Maven if installed at the
-    same time.
-    EOS
   end
 end
