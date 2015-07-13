ac9c346abd12673634d4640d4f801c49bc71894d
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-13 23:13:39 +0100

    jetty76: add 7.6.17.v20150415 bottle.

--- jetty76.rb
+++ jetty76.rb
@@ -5,6 +5,13 @@ class Jetty76 < Formula
   version "7.6.17.v20150415"
   sha256 "57d6bbb48771944b8c2a3124240c0c4a78284a75db2b86018f543654dfd50eef"
 
+  bottle do
+    cellar :any
+    sha256 "b00c93abc2a48dbac36169b595fc281eb0c9e66a99f9218c00e63bd132393423" => :yosemite
+    sha256 "47a9172532ffa40dc1495bc180c016d996b5dcddfa90411fb0cb3e59f9776267" => :mavericks
+    sha256 "d1036c8ad02f4b7c40abecf247e5fbbd9764a6c577f1033642eaacec8156c31f" => :mountain_lion
+  end
+
   conflicts_with "jetty", :because => "Differing version of same formula"
 
   def install
