fa2194d4a4f230864b5c9a1c9d75467fd7267243
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-13 20:34:46 +0100

    jetty76 7.6.17.v20150415
    Closes #901
    
    Closes #911.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- jetty76.rb
+++ jetty76.rb
@@ -1,20 +1,21 @@
-require 'formula'
-
 class Jetty76 < Formula
-  homepage 'http://www.eclipse.org/jetty/'
-  url 'http://download.eclipse.org/jetty/stable-7/dist/jetty-distribution-7.6.16.v20140903.tar.gz'
-  version '7.6.16'
-  sha1 '842868f5e79eaad70b00c7748a451d66850f0906'
+  desc "Java servlet engine and webserver"
+  homepage "https://www.eclipse.org/jetty/"
+  url "http://download.eclipse.org/jetty/7.6.17.v20150415/dist/jetty-distribution-7.6.17.v20150415.tar.gz"
+  version "7.6.17.v20150415"
+  sha256 "57d6bbb48771944b8c2a3124240c0c4a78284a75db2b86018f543654dfd50eef"
+
+  conflicts_with "jetty", :because => "Differing version of same formula"
 
   def install
-    rm_rf Dir['bin/*.{cmd,bat]}']
+    rm_rf Dir["bin/*.{cmd,bat]}"]
 
-    libexec.install Dir['*']
-    (libexec+'logs').mkpath
+    libexec.install Dir["*"]
+    (libexec+"logs").mkpath
 
     bin.mkpath
     Dir["#{libexec}/bin/*.sh"].each do |f|
-      scriptname = File.basename(f, '.sh')
+      scriptname = File.basename(f, ".sh")
       (bin+scriptname).write <<-EOS.undent
         #!/bin/bash
         JETTY_HOME=#{libexec}
