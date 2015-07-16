703941d1b027e79f5aac5a514e866e3ffa8d6082
Jason <jasonmichaelroth@gmail.com>
2015-07-16 07:40:06 -0600

    boot2docker 1.6.2 (new formula)
    Closes #913.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ boot2docker162.rb
@@ -0,0 +1,48 @@
+class Boot2docker162 < Formula
+  desc "boot2docker runs Docker containers on OSX"
+  homepage "https://github.com/boot2docker/boot2docker-cli"
+  url "https://github.com/boot2docker/boot2docker-cli.git", :tag => "v1.6.2", :revision => "cb2c3bcc890d8ee67bb76cc91ecf5b63927c97f9"
+  head "https://github.com/boot2docker/boot2docker-cli.git"
+
+  depends_on "docker162" => :recommended
+  depends_on "go" => :build
+
+  conflicts_with "boot2docker", :because => "Differing version of the same formula"
+
+  def install
+    (buildpath + "src/github.com/boot2docker/boot2docker-cli").install Dir[buildpath/"*"]
+
+    cd "src/github.com/boot2docker/boot2docker-cli" do
+      ENV["GOPATH"] = buildpath
+      system "go", "get", "-d"
+
+      ENV["GIT_DIR"] = cached_download/".git"
+      system "make", "goinstall"
+    end
+
+    bin.install "bin/boot2docker-cli" => "boot2docker"
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/boot2docker</string>
+        <string>up</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/boot2docker", "version"
+  end
+end
