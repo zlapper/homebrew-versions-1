aa9305b6f0156ede43f6a1029a5eb1aeebe8c78e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-16 10:59:49 +0100

    docker162: add 1.6.2 bottle.

--- docker162.rb
+++ docker162.rb
@@ -5,6 +5,13 @@ class Docker162 < Formula
       :tag => "v1.6.2",
       :revision => "7c8fca2ddb58c8d2c4fb4df31c242886df7dd257"
 
+  bottle do
+    cellar :any
+    sha256 "f6f3a78dcd9e9154b1285ab1e74b3ba1ba33f7466195b50caadf0b55c8028e22" => :yosemite
+    sha256 "91c81898fc2b781c43e734812da9310bfd9b207892ee07df518395e2df115bb7" => :mavericks
+    sha256 "dccb74261f9f0f70abf82852e3ade21914a6edaaee47f373af46cd7783c6a832" => :mountain_lion
+  end
+
   option "without-completions", "Disable bash/zsh completions"
 
   depends_on "go" => :build
