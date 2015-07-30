00850cd6c62dd93e8f0ff525793074f917b6cd13
Francis Rangel <francis.rangel@gmail.com>
2015-07-30 11:20:42 -0300

    gradle24 2.4 (new formula)
    Closes #935.
    Closes #932.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ gradle24.rb
@@ -0,0 +1,15 @@
+class Gradle24 < Formula
+  desc "Gradle build automation tool"
+  homepage "http://www.gradle.org/"
+  url "https://services.gradle.org/distributions/gradle-2.4-bin.zip"
+  sha256 "c4eaecc621a81f567ded1aede4a5ddb281cc02a03a6a87c4f5502add8fc2f16f"
+
+  def install
+    libexec.install %w[bin lib]
+    bin.install_symlink libexec+"bin/gradle"
+  end
+
+  test do
+    system "#{bin}/gradle", "-version"
+  end
+end
