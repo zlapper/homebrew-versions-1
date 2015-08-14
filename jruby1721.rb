ab6c0f8c64bdf8291f9e659f19c02af5669667d7
Andrew Holloway <andrew.holloway@untropy.net>
2015-08-14 12:13:16 -0500

    jruby1721 1.7.21 (new formula)

---
+++ jruby1721.rb
@@ -0,0 +1,34 @@
+class Jruby1721 < Formula
+  desc "Ruby implementation in pure Java"
+  homepage "http://www.jruby.org"
+  url "https://s3.amazonaws.com/jruby.org/downloads/1.7.21/jruby-bin-1.7.21.tar.gz"
+  sha256 "9fe56ea173af451ef262faaee6fb90464002584dbacc2523147f809e9d3a1c8b"
+
+  depends_on :java => "1.7+"
+
+  conflicts_with "jruby", :because => "Differing version of the same formula"
+  conflicts_with "jruby168", :because => "Differing version of the same formula"
+  conflicts_with "jruby9000", :because => "Differing version of the same formula"
+
+  def install
+    # Remove Windows files
+    rm Dir["bin/*.{bat,dll,exe}"]
+
+    cd "bin" do
+      # Prefix a 'j' on some commands to avoid clashing with other rubies
+      %w[ast rake rdoc ri testrb].each { |f| mv f, "j#{f}" }
+      # Delete some unnecessary commands
+      rm "gem" # gem is a wrapper script for jgem
+      rm "irb" # irb is an identical copy of jirb
+    end
+
+    # Only keep the OS X native libraries
+    rm_rf Dir["lib/jni/*"] - ["lib/jni/Darwin"]
+    libexec.install Dir["*"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/jruby", "-e", "puts 'hello'"
+  end
+end
