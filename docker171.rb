76d0e22d8fac3e3f929c25d093f6690335ef26e9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-17 20:22:53 +0100

    docker171: add 1.7.1 bottle.

--- docker171.rb
+++ docker171.rb
@@ -5,6 +5,13 @@ class Docker171 < Formula
       :tag => "v1.7.1",
       :revision => "786b29d4db80a6175e72b47a794ee044918ba734"
 
+  bottle do
+    cellar :any
+    sha256 "d6bbc45a5e2123492f21927e99101f2ae0be9b1909941c9a58d2954ae515c6fe" => :yosemite
+    sha256 "29fbd90ea38516a0df555e17e115bb18012cae479cdc4422a35c00b3b228c9c7" => :mavericks
+    sha256 "add1e261d95be68a612449473cf16ea05c3e0fa1f6061ec91944293b18e3f557" => :mountain_lion
+  end
+
   option "without-completions", "Disable bash/zsh completions"
 
   depends_on "go" => :build
