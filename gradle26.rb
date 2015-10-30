b2804277d0c758840e84a5ba538cbeccf31e964b
Ivars Berzins <ivars.berzins@gmail.com>
2015-10-30 11:55:08 +0200

    gradle26 2.6 (new formula)
    Closes #1002.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

---
+++ gradle26.rb
@@ -0,0 +1,17 @@
+class Gradle26 < Formula
+  desc "Gradle build automation tool"
+  homepage "https://www.gradle.org/"
+  url "https://services.gradle.org/distributions/gradle-2.6-bin.zip"
+  sha256 "18a98c560af231dfa0d3f8e0802c20103ae986f12428bb0a6f5396e8f14e9c83"
+
+  conflicts_with "gradle", :because => "Differing version of same formula"
+
+  def install
+    libexec.install %w[bin lib]
+    bin.install_symlink libexec+"bin/gradle"
+  end
+
+  test do
+    system "#{bin}/gradle", "-version"
+  end
+end
