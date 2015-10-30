db76bc261e1b161c917a63a36d9f24c14d44188a
BrewTestBot <brew-test-bot@googlegroups.com>
2015-10-30 04:58:14 +0000

    lz4r117: add r117 bottle.

--- lz4r117.rb
+++ lz4r117.rb
@@ -5,6 +5,13 @@ class Lz4r117 < Formula
   version "r117"
   sha256 "c4ca70bf6711021d5ae64e79469a619a4e5899a4c7e07e665f3eb3a517cd029d"
 
+  bottle do
+    cellar :any
+    sha256 "2e34e4bdc4424f019d3626e271a2c206dc59ded5c863711dd979d4ead20bc21c" => :el_capitan
+    sha256 "5a470c54018609d55ff28342df3c54f207444e7d3c7a9fa9bfb51210b2a86c0d" => :yosemite
+    sha256 "031be16225c00692a6aaa462237d7899ec3435b92b05e20dfdd5781d00a58f08" => :mavericks
+  end
+
   conflicts_with "lz4", :because => "Differing versions of the same formulae"
 
   def install
