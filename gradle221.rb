fb813a1ff6d57fca47233be6087472254d73b14c
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-08 04:13:56 +0000

    gradle221: bottle unneeded

--- gradle221.rb
+++ gradle221.rb
@@ -1,7 +1,9 @@
 class Gradle221 < Formula
-  homepage "http://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-2.2.1-bin.zip"
+  homepage "https://www.gradle.org/"
+  url "https://downloads.gradle.org/distributions/gradle-2.2.1-bin.zip"
   sha256 "420aa50738299327b611c10b8304b749e8d3a579407ee9e755b15921d95ff418"
+
+  bottle :unneeded
 
   def install
     libexec.install %w[bin lib]
