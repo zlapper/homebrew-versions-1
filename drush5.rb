ff249351ac99a5a2a187f4c07ea002a0f74e9bd1
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-08 03:58:17 +0000

    drush5: bottle unneeded

--- drush5.rb
+++ drush5.rb
@@ -3,6 +3,8 @@ class Drush5 < Formula
   url "http://ftp.drupal.org/files/projects/drush-7.x-5.9.tar.gz"
   sha256 "3acc2a2491fef987c17e85122f7d3cd0bc99cefd1bc70891ec3a1c4fd51dccee"
 
+  bottle :unneeded
+
   keg_only "Conflicts with drush in main repository."
 
   def install
