97269f2a3767189d2a3d1308d2d5c428ab83fe64
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-08 03:58:08 +0000

    drush4: bottle unneeded

--- drush4.rb
+++ drush4.rb
@@ -3,6 +3,8 @@ class Drush4 < Formula
   url "http://ftp.drupal.org/files/projects/drush-7.x-4.6.tar.gz"
   sha256 "c8f5a165c1624b023aaa74b4fd852da1dc426bd08f7cf1af328fe16e7de27d8d"
 
+  bottle :unneeded
+
   keg_only "Conflicts with drush in main repository."
 
   def install
