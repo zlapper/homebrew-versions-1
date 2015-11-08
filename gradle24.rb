3c591e02fdaec894fd2d001558e7e1ae2e67cf46
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-08 04:08:37 +0000

    gradle24: bottle unneeded

--- gradle24.rb
+++ gradle24.rb
@@ -1,8 +1,10 @@
 class Gradle24 < Formula
   desc "Gradle build automation tool"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-2.4-bin.zip"
+  url "https://downloads.gradle.org/distributions/gradle-2.4-bin.zip"
   sha256 "c4eaecc621a81f567ded1aede4a5ddb281cc02a03a6a87c4f5502add8fc2f16f"
+
+  bottle :unneeded
 
   conflicts_with "gradle", :because => "Differing version of same formula"
 
