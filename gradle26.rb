30f3f4223ec516f17831d92481070efc21ad3872
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-08 04:08:59 +0000

    gradle26: use newer url

--- gradle26.rb
+++ gradle26.rb
@@ -1,7 +1,7 @@
 class Gradle26 < Formula
   desc "Gradle build automation tool"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-2.6-bin.zip"
+  url "https://downloads.gradle.org/distributions/gradle-2.6-bin.zip"
   sha256 "18a98c560af231dfa0d3f8e0802c20103ae986f12428bb0a6f5396e8f14e9c83"
 
   bottle :unneeded
