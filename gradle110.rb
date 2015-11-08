0d4a68df9cc51d3c02f430d1f3a7647d5cdda14a
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-08 04:11:09 +0000

    gradle110: bottle unneeded

--- gradle110.rb
+++ gradle110.rb
@@ -1,12 +1,12 @@
-require 'formula'
-
 class Gradle110 < Formula
-  homepage 'http://www.gradle.org/'
-  url 'http://services.gradle.org/distributions/gradle-1.10-bin.zip'
-  sha1 'f911beeff25ebbb7b4af3092dd7f0c93019aef17'
+  homepage "https://www.gradle.org/"
+  url "https://downloads.gradle.org/distributions/gradle-1.10-bin.zip"
+  sha256 "6e6db4fc595f27ceda059d23693b6f6848583950606112b37dfd0e97a0a0a4fe"
+
+  bottle :unneeded
 
   def install
     libexec.install %w[bin lib]
-    bin.install_symlink libexec+'bin/gradle'
+    bin.install_symlink libexec+"bin/gradle"
   end
 end
