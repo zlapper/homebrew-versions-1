52458eebd6047048aeae5a8ef12d41e7cdd6348b
Dominyk Tiller <dominyktiller@gmail.com>
2015-11-08 04:06:43 +0000

    gradle21: bottle unneeded

--- gradle21.rb
+++ gradle21.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class Gradle21 < Formula
-  homepage "http://www.gradle.org/"
-  url "http://services.gradle.org/distributions/gradle-2.1-bin.zip"
-  sha1 "b8fa88f4053452b0b09f159b8668ce08e4dc5fa8"
+  homepage "https://www.gradle.org/"
+  url "https://downloads.gradle.org/distributions/gradle-2.1-bin.zip"
+  sha256 "3eee4f9ea2ab0221b89f8e4747a96d4554d00ae46d8d633f11cfda60988bf878"
+
+  bottle :unneeded
 
   def install
     libexec.install %w[bin lib]
