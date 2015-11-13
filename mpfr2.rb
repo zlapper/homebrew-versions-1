db498517c4fe8178bf53e3686ad00c5d6a22eeb2
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-13 11:54:25 +0100

    mpfr2: audit fixes
    Closes #1022.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- mpfr2.rb
+++ mpfr2.rb
@@ -1,28 +1,29 @@
-require 'formula'
-
 class Mpfr2 < Formula
-  homepage 'http://www.mpfr.org/'
+  desc "Multiple-precision floating-point computations C lib"
+  homepage "http://www.mpfr.org/"
   # Track gcc infrastructure releases.
-  url 'http://www.mpfr.org/mpfr-2.4.2/mpfr-2.4.2.tar.bz2'
-  mirror 'ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-2.4.2.tar.bz2'
-  sha1 '7ca93006e38ae6e53a995af836173cf10ee7c18c'
+  url "http://www.mpfr.org/mpfr-2.4.2/mpfr-2.4.2.tar.bz2"
+  mirror "ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-2.4.2.tar.bz2"
+  sha256 "c7e75a08a8d49d2082e4caee1591a05d11b9d5627514e678f02d66a124bcf2ba"
 
   bottle do
     cellar :any
     sha256 "b567a28e0d55497325ab97f73e302414bd66e48068c8dbb1092d453704ffe523" => :yosemite
     sha256 "3f195ad79022a35840e46f0726f623c932ad5ef72516a873615078f053867aef" => :mavericks
     sha256 "ab7c5d3f5aa1adb76d27b66389c9aa65ee4a52c7c23d40dee30b8ca78653ca17" => :mountain_lion
-    sha1 'd146a7ec89d73a64906ed3fa930d1062b0cb5479' => :tiger_g3
-    sha1 '87ea4788e631dd447a92e78cc183093ecf4157be' => :tiger_altivec
-    sha1 '6ffec69dca10ac3cb40a6dc7d1221200eb7d888a' => :leopard_g3
-    sha1 '39c667af86e969336c904afb55ad7b25f81a4140' => :leopard_altivec
+    sha1 "d146a7ec89d73a64906ed3fa930d1062b0cb5479" => :tiger_g3
+    sha1 "87ea4788e631dd447a92e78cc183093ecf4157be" => :tiger_altivec
+    sha1 "6ffec69dca10ac3cb40a6dc7d1221200eb7d888a" => :leopard_g3
+    sha1 "39c667af86e969336c904afb55ad7b25f81a4140" => :leopard_altivec
   end
 
-  depends_on 'gmp4'
+  option "with-32-bit"
 
-  keg_only 'Conflicts with mpfr in main repository.'
+  deprecated_option "32-bit" => "with-32-bit"
 
-  option '32-bit'
+  depends_on "gmp4"
+
+  keg_only "Conflicts with mpfr in main repository."
 
   fails_with :clang do
     build 421
@@ -35,15 +36,15 @@ class Mpfr2 < Formula
   def install
     gmp4 = Formula["gmp4"]
 
-    args = [
-      "--disable-dependency-tracking",
-      "--prefix=#{prefix}",
-      "--with-gmp=#{gmp4.opt_prefix}"
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --with-gmp=#{gmp4.opt_prefix}
     ]
 
     # Build 32-bit where appropriate, and help configure find 64-bit CPUs
     # Note: This logic should match what the GMP formula does.
-    if MacOS.prefer_64_bit? and not build.build_32_bit?
+    if MacOS.prefer_64_bit? && !build.build_32_bit?
       ENV.m64
       args << "--build=x86_64-apple-darwin"
     else
@@ -53,7 +54,7 @@ class Mpfr2 < Formula
 
     system "./configure", *args
     system "make"
-    system "make check"
-    system "make install"
+    system "make", "check"
+    system "make", "install"
   end
 end
