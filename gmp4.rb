e7f544cbd926ef55b76fce6bef9135edc23b5e78
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-20 23:17:03 +0000

    gmp4: update 4.3.2 bottle.

--- gmp4.rb
+++ gmp4.rb
@@ -10,13 +10,10 @@ class Gmp4 < Formula
 
   bottle do
     cellar :any
-    sha256 "40de88cbcd04d6869051eb8617afbb2e0754f680f6754708652a807349948815" => :yosemite
-    sha256 "7543fa0986dfa81487367b410c178ca581560df39ca53dfc7d66e2c2a4543813" => :mavericks
-    sha256 "6064daffafdeeab54380827aa6da948a47001ef847a28c2edbb0688f54e0a480" => :mountain_lion
-    sha1 "ac18cca84840aac6d004d829bc1cea398ec09487" => :tiger_g3
-    sha1 "33780b658a5f64d220fb64c07d4f6e859ddd94c5" => :tiger_altivec
-    sha1 "71bf67992e28a0b48606db40df70f22d3414c5fd" => :leopard_g3
-    sha1 "480b7c3848b069e0da3bb33331beacf9122baec3" => :leopard_altivec
+    revision 1
+    sha256 "5c5d632ea58479fba37a0844d0b9a4fd1e865e809286ada95d5484d72ab50a78" => :el_capitan
+    sha256 "932a2d0987dd26bbc3cb54b7d38d4cf6b0e5a4bd46fef8442b79b75f067abf6e" => :yosemite
+    sha256 "b7012c46ac9e5b5aa1004184e85a2e99fc047d9f92f1e8b82699ba1f1ea1f7bf" => :mavericks
   end
 
   keg_only "Conflicts with gmp in main repository."
