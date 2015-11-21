36bd572cb4bcdf9d6335f562a8dd0aca30ddb228
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-21 13:08:25 +0100

    allegro4: desc & test added
    Closes #1035.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- allegro4.rb
+++ allegro4.rb
@@ -1,4 +1,5 @@
 class Allegro4 < Formula
+  desc "C/C++ library for cross-platform game development"
   homepage "http://www.allegro.cc"
   url "https://downloads.sourceforge.net/project/alleg/allegro/4.4.2/allegro-4.4.2.tar.gz"
   sha256 "1b21e7577dbfada02d85ca4510bd22fedaa6ce76fde7f4838c7c1276eb840fdc"
@@ -13,5 +14,22 @@ class Allegro4 < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
+  end
+
+  test do
+    (testpath/"allegro_test.cpp").write <<-EOS
+      #include <assert.h>
+      #include <allegro4/allegro4.h>
+
+      int main(int n, char** c) {
+        if (!al_init()) {
+          return 1;
+        }
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-I#{include}", "-L#{lib}", "-lallegro", "-lallegro_main", "-o", "allegro_test", "allegro_test.cpp"
+    system "./allegro_test"
   end
 end
