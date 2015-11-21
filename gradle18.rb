9aeaeda01a4a728de1e84ae52ed5511a3bcce073
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-21 14:04:03 +0100

    gradle18: desc & test added

--- gradle18.rb
+++ gradle18.rb
@@ -1,12 +1,20 @@
 class Gradle18 < Formula
+  desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
   url "https://downloads.gradle.org/distributions/gradle-1.8-bin.zip"
   sha256 "a342bbfa15fd18e2482287da4959588f45a41b60910970a16e6d97959aea5703"
 
   bottle :unneeded
 
+  conflicts_with "gradle", :because => "Differing version of same formula"
+
   def install
     libexec.install %w[bin lib]
     bin.install_symlink libexec+"bin/gradle"
+  end
+
+  test do
+    ENV["GRADLE_USER_HOME"] = testpath
+    assert_match "Gradle #{version}", shell_output("#{bin}/gradle --version")
   end
 end
