e64539c13d36ad2287291697546ce758ce5250a4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-11-21 00:08:53 +0000

    libmpc08: update 0.8.1 bottle.

--- libmpc08.rb
+++ libmpc08.rb
@@ -8,13 +8,10 @@ class Libmpc08 < Formula
 
   bottle do
     cellar :any
-    sha256 "a52eb16b2016623c7c31bdebad9a3d90b84082bcbf96e1648dce18e1e1d8dff3" => :yosemite
-    sha256 "9e52d35c5ee1c1027701d012110dc18b546288fbbec113c09a8b3dacdb87bbc4" => :mavericks
-    sha256 "03d6b2fd2c47434d305b4be197d5a946b25706efc97d009fc4a0e1c68be5e3a2" => :mountain_lion
-    sha1 "7f63cdd281274f82122d7580400a4b5561dabd93" => :tiger_g3
-    sha1 "fa73d69bf25ccfe069dc553a4d90f09877ca9054" => :tiger_altivec
-    sha1 "bc3ce1df9d7afcafa05f270e03ac73158c312004" => :leopard_g3
-    sha1 "a9905224e99541da96d4c1c6fec7276bf6eb1404" => :leopard_altivec
+    revision 1
+    sha256 "e3bb3e309c6c5bd709fee9a136a642289357fcc4aa9c793df1c96785947c99d9" => :el_capitan
+    sha256 "79a024cd86280716cc82f35e88ae9695f0606d2b09e585d3d9a0f7fc800eec20" => :yosemite
+    sha256 "105469fc116eae8a19000848c91ea1bea71ad5e3f395624938030c3813eca0ed" => :mavericks
   end
 
   keg_only "Conflicts with libmpc in main repository."
