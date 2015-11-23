ee0bd41ab47489f50ba902f645f378ad3bdc92c8
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 18:16:31 +0100

    squid2: desc added

--- squid2.rb
+++ squid2.rb
@@ -1,20 +1,19 @@
-require 'formula'
-
 class Squid2 < Formula
-  homepage 'http://www.squid-cache.org/'
-  url 'http://www.squid-cache.org/Versions/v2/2.7/squid-2.7.STABLE9.tar.gz'
-  version '2.7.9'
-  sha1 '6d90fe06468b662b2eefd7ffeb47b9a78f0a871d'
+  desc "Proxy caching server for HTTP(S), FTP, and Gopher"
+  homepage "http://www.squid-cache.org/"
+  url "http://www.squid-cache.org/Versions/v2/2.7/squid-2.7.STABLE9.tar.gz"
+  version "2.7.9"
+  sha256 "d54ca048313c4b64609fcdf9f1934a70fc1702032a5f04073056d7491d3dd781"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--localstatedir=#{var}/squid"
-    system "make install"
+    system "make", "install"
 
     # Create default Squid cache and log dirs
-    (var/'squid/cache').mkpath
-    (var/'squid/logs').mkpath
+    (var/"squid/cache").mkpath
+    (var/"squid/logs").mkpath
     # Create swap directories otherwise squid will complain
     system "#{sbin}/squid", "-z"
   end
