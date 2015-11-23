5431225b6f6791c81800523f2cf819a9aaaf4952
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 12:10:44 +0100

    grails20: desc & test added

--- grails20.rb
+++ grails20.rb
@@ -1,18 +1,28 @@
-require 'formula'
-
 class Grails20 < Formula
-  homepage 'http://grails.org'
-  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.0.4.zip'
-  sha1 '8a7a0edf83f1890f87bda5ee316e35cc608e2ebd'
+  desc "Web application framework for the Groovy language"
+  homepage "http://grails.org"
+  url "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.0.4.zip"
+  sha256 "51a273b8d51c08a21111bba774745db70370fbe7df906dba5551da2376f4f41b"
+
+  bottle :unneeded
+
+  # Grails 2.0.x doesn't support Java 8
+  depends_on :java => "1.7"
 
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
     prefix.install %w[LICENSE README]
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.mkpath
     Dir["#{libexec}/bin/*"].each do |f|
       next unless File.extname(f).empty?
       ln_s f, bin+File.basename(f)
     end
+  end
+
+  test do
+    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    assert_match "Grails version: #{version}",
+      shell_output("#{bin}/grails --version", 1)
   end
 end
