84501ab77cbe3a559d5f474588564aa045fa7664
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 12:10:47 +0100

    grails22: desc & test added

--- grails22.rb
+++ grails22.rb
@@ -1,18 +1,27 @@
-require 'formula'
-
 class Grails22 < Formula
-  homepage 'http://grails.org'
-  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.2.5.zip'
-  sha1 '370c502a2add74ca3ae2f15b959c8886c965c5ac'
+  desc "Web application framework for the Groovy language"
+  homepage "http://grails.org"
+  url "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.2.5.zip"
+  sha256 "012f65c315000e9274d3eaf6a7d739a3a31668c4fd49ff7933b21ad332510291"
+
+  bottle :unneeded
+
+  depends_on :java
 
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
     prefix.install %w[LICENSE README]
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.mkpath
     Dir["#{libexec}/bin/*"].each do |f|
       next unless File.extname(f).empty?
       ln_s f, bin+File.basename(f)
     end
+  end
+
+  test do
+    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    assert_match "Grails version: #{version}",
+      shell_output("#{bin}/grails --version")
   end
 end
