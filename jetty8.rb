6f6bb4fa76df19c4cb87e8d386d2ee1767de9197
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 17:57:59 +0100

    jetty8: desc added

--- jetty8.rb
+++ jetty8.rb
@@ -1,20 +1,19 @@
-require 'formula'
-
 class Jetty8 < Formula
-  homepage 'http://www.eclipse.org/jetty/'
-  url 'http://eclipse.org/downloads/download.php?file=/jetty/8.1.15.v20140411/dist/jetty-distribution-8.1.15.v20140411.tar.gz&r=1'
-  version '8.1.15'
-  sha1 'a6a559a47253b1a45a696c6c45b2f91019e8da2f'
+  desc "Java servlet engine and webserver"
+  homepage "http://www.eclipse.org/jetty/"
+  url "http://eclipse.org/downloads/download.php?file=/jetty/8.1.15.v20140411/dist/jetty-distribution-8.1.15.v20140411.tar.gz&r=1"
+  version "8.1.15"
+  sha256 "2015725c8d1525f6d57b24ab5b16c745945a7a278dbedd1fba62016be125fb84"
 
   def install
-    rm_rf Dir['bin/*.{cmd,bat]}']
+    rm_rf Dir["bin/*.{cmd,bat]}"]
 
-    libexec.install Dir['*']
-    (libexec+'logs').mkpath
+    libexec.install Dir["*"]
+    (libexec+"logs").mkpath
 
     bin.mkpath
     Dir["#{libexec}/bin/*.sh"].each do |f|
-      scriptname = File.basename(f, '.sh')
+      scriptname = File.basename(f, ".sh")
       (bin+scriptname).write <<-EOS.undent
         #!/bin/bash
         JETTY_HOME=#{libexec}
