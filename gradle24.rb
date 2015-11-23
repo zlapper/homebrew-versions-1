26852812f834fe3187885c965173411e99419baa
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 11:00:44 +0100

    gradle24: desc & test added
    Closes #1037.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- gradle24.rb
+++ gradle24.rb
@@ -1,5 +1,5 @@
 class Gradle24 < Formula
-  desc "Gradle build automation tool"
+  desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
   url "https://downloads.gradle.org/distributions/gradle-2.4-bin.zip"
   sha256 "c4eaecc621a81f567ded1aede4a5ddb281cc02a03a6a87c4f5502add8fc2f16f"
@@ -14,6 +14,7 @@ class Gradle24 < Formula
   end
 
   test do
-    system "#{bin}/gradle", "-version"
+    ENV["GRADLE_USER_HOME"] = testpath
+    assert_match "Gradle #{version}", shell_output("#{bin}/gradle --version")
   end
 end
