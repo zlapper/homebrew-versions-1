9a08f5a3658d65458e8a20b4ff6237fb6f0d9a18
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 15:30:47 +0100

    zeromq3: desc & test added
    Closes #1041.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- zeromq3.rb
+++ zeromq3.rb
@@ -1,32 +1,31 @@
-require 'formula'
-
 class Zeromq3 < Formula
-  homepage 'http://www.zeromq.org/'
-  url 'http://download.zeromq.org/zeromq-3.2.4.tar.gz'
-  sha1 '08303259f08edd1faeac2e256f5be3899377135e'
+  desc "High-performance, asynchronous messaging library"
+  homepage "http://www.zeromq.org/"
+  url "http://download.zeromq.org/zeromq-3.2.4.tar.gz"
+  sha256 "df96a141c85b65c2cb6c0db22223126623a09eecbfccb7f74400f3f938742f59"
 
-  keg_only 'Conflicts with zeromq in main repository.'
+  keg_only "Conflicts with zeromq in main repository."
 
   option :universal
-  option 'with-pgm', 'Build with PGM extension'
+  option "with-pgm", "Build with PGM extension"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'libpgm' if build.with? 'pgm'
+  depends_on "pkg-config" => :build
+  depends_on "libpgm" if build.with? "pgm"
 
   def install
     ENV.universal_binary if build.universal?
 
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
-    if build.with? 'pgm'
+    if build.with? "pgm"
       # Use HB libpgm-5.2 because their internal 5.1 is b0rked.
-      ENV['OpenPGM_CFLAGS'] = %x[pkg-config --cflags openpgm-5.2].chomp
-      ENV['OpenPGM_LIBS'] = %x[pkg-config --libs openpgm-5.2].chomp
+      ENV["OpenPGM_CFLAGS"] = `pkg-config --cflags openpgm-5.2`.chomp
+      ENV["OpenPGM_LIBS"] = `pkg-config --libs openpgm-5.2`.chomp
       args << "--with-system-pgm"
     end
 
     system "./configure", *args
     system "make"
-    system "make install"
+    system "make", "install"
   end
 
   def caveats; <<-EOS.undent
@@ -35,5 +34,21 @@ class Zeromq3 < Formula
 
         ARCHFLAGS="-arch x86_64" gem install zmq -- --with-zmq-dir=#{opt_prefix}
     EOS
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <assert.h>
+      #include <zmq.h>
+
+      int main()
+      {
+        zmq_msg_t query;
+        assert(0 == zmq_msg_init_size(&query, 1));
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lzmq", "-o", "test"
+    system "./test"
   end
 end
