8c38d40b922e9252c2534a53c553e9a926b90077
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 11:00:32 +0100

    gradle110: desc & test added

--- gradle110.rb
+++ gradle110.rb
@@ -1,12 +1,20 @@
 class Gradle110 < Formula
+  desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
   url "https://downloads.gradle.org/distributions/gradle-1.10-bin.zip"
   sha256 "6e6db4fc595f27ceda059d23693b6f6848583950606112b37dfd0e97a0a0a4fe"
 
   bottle :unneeded
 
+  conflicts_with "gradle", :because => "Differing version of same formula"
+
   def install
     libexec.install %w[bin lib]
     bin.install_symlink libexec+"bin/gradle"
+  end
+
+  test do
+    ENV["GRADLE_USER_HOME"] = testpath
+    assert_match "Gradle #{version}", shell_output("#{bin}/gradle --version")
   end
 end
