e06c1820ff3d32f711b13893a9e2bd67ae22e2b6
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 17:58:18 +0100

    jboss-forge1: desc added

--- jboss-forge1.rb
+++ jboss-forge1.rb
@@ -1,14 +1,15 @@
-require 'formula'
-
 class JbossForge1 < Formula
-  homepage 'http://forge.jboss.org/'
-  url 'https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=1.4.4.Final&e=zip'
-  version '1.4.4.Final'
-  sha1 '7c5218f9ed9c2e4986ee450f3c958d78dc3c09fd'
+  desc "Tools to help set up and configure a project"
+  homepage "http://forge.jboss.org/"
+  url "https://repository.jboss.org/nexus/service/local/artifact/maven/redirect?r=releases&g=org.jboss.forge&a=forge-distribution&v=1.4.4.Final&e=zip"
+  version "1.4.4.Final"
+  sha256 "fb794032f769ec27a7f8cc28da72cfc4a2e349f7c4836ec15f5fa567d3af9892"
+
+  bottle :unneeded
 
   def install
     rm_f Dir["bin/*.bat"]
-    libexec.install %w{ bin modules jboss-modules.jar }
-    bin.install_symlink libexec/'bin/forge'
+    libexec.install %w[ bin modules jboss-modules.jar ]
+    bin.install_symlink libexec/"bin/forge"
   end
 end
