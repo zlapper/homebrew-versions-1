bf48eb9941c4b4bb854a48d182b67122e01d7547
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 18:05:42 +0100

    llvm-gcc28: desc added

--- llvm-gcc28.rb
+++ llvm-gcc28.rb
@@ -1,14 +1,15 @@
-require 'formula'
-
 class LlvmGcc28 < Formula
-  homepage 'http://llvm.org/releases/2.8/docs/CommandGuide/html/llvmgcc.html'
-  url 'http://llvm.org/releases/2.8/llvm-gcc-4.2-2.8-x86_64-apple-darwin10.tar.gz'
-  version '2.8'
-  sha1 'f387a5f774c36cd96ab378f919c5861351110941'
+  desc "LLVM C front-end"
+  homepage "http://llvm.org/releases/2.8/docs/CommandGuide/html/llvmgcc.html"
+  url "http://llvm.org/releases/2.8/llvm-gcc-4.2-2.8-x86_64-apple-darwin10.tar.gz"
+  version "2.8"
+  sha256 "c359d13f658a6de028615554f350053e6dd8c1cb82fe3716cffbd4c27e703bdf"
+
+  bottle :unneeded
 
   keg_only :provided_by_osx
 
   def install
-    prefix.install Dir['*']
+    prefix.install Dir["*"]
   end
 end
