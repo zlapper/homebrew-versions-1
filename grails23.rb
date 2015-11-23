ee3f1710c34cdc15ae0ec545c7ef54046b4b1c22
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 12:10:52 +0100

    grails23: desc & test added
    Closes #1040.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- grails23.rb
+++ grails23.rb
@@ -1,18 +1,27 @@
-require 'formula'
-
 class Grails23 < Formula
-  homepage 'http://grails.org'
-  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.3.11.zip'
-  sha1 'f483bcf938f6fc5a9e4913b83d4da3c3f94d8785'
+  desc "Web application framework for the Groovy language"
+  homepage "http://grails.org"
+  url "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.3.11.zip"
+  sha256 "c53e01a9d98c499f91d7c7d54312dbdfd33f99ffcdec8bc4946036e4bea2c8e1"
+
+  bottle :unneeded
+
+  depends_on :java
 
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
     prefix.install %w[LICENSE README]
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.mkpath
     Dir["#{libexec}/bin/*"].each do |f|
       next unless File.extname(f).empty?
       ln_s f, bin+File.basename(f)
     end
+  end
+
+  test do
+    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    assert_match "Grails version: #{version}",
+      shell_output("#{bin}/grails --version")
   end
 end
