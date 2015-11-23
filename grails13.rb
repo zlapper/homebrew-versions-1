9c4b086a2c2e2dc1c2096d47242f3208a3249fcd
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-23 12:10:42 +0100

    grails13: desc & test added

--- grails13.rb
+++ grails13.rb
@@ -1,18 +1,27 @@
-require 'formula'
-
 class Grails13 < Formula
-  homepage 'http://grails.org'
-  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-1.3.9.zip'
-  sha1 'aa87e7caabd230b40fd888ac226b8b6d6b4703ba'
+  desc "Web application framework for the Groovy language"
+  homepage "http://grails.org"
+  url "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-1.3.9.zip"
+  sha256 "1aea08ae97713b816d87ad699e65203f8c50843e84b4cad442119ab4f081d11a"
+
+  bottle :unneeded
+
+  depends_on :java
 
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
     prefix.install %w[LICENSE README]
-    libexec.install Dir['*']
+    libexec.install Dir["*"]
     bin.mkpath
     Dir["#{libexec}/bin/*"].each do |f|
       next unless File.extname(f).empty?
       ln_s f, bin+File.basename(f)
     end
+  end
+
+  test do
+    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    assert_match "Grails #{version}",
+      shell_output("#{bin}/grails --version", 1)
   end
 end
