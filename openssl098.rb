63e6f7b27eecd088c555e158a2f8d98ebc865fe0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-04 07:08:30 +0000

    openssl098: update 0.9.8zh bottle.

--- openssl098.rb
+++ openssl098.rb
@@ -5,9 +5,9 @@ class Openssl098 < Formula
   sha256 "f1d9f3ed1b85a82ecf80d0e2d389e1fda3fca9a4dba0bf07adbf231e1a5e2fd6"
 
   bottle do
-    sha256 "7543cbff1b366dcbde0f554033b7531d162acd9682a0773e7b8bfbf495ef9751" => :yosemite
-    sha256 "f6b0ccc6cbcbfb60ea1e960a45351497d664c4172361b1c8ccb9db657549e60a" => :mavericks
-    sha256 "c832d4b68e71c13d82d064bb6a7813871d59f862dd2b7c48f5c0fa2a1ea8f2dc" => :mountain_lion
+    sha256 "625b28afafa9e0cfb69c1d1ebf19f9f4f93ae2b92c54727ead0833b15a677465" => :el_capitan
+    sha256 "51777b4e0896b69c3c336fbc18234ccf0f0b0e4b423dae7bcba45a3ca742950a" => :yosemite
+    sha256 "23142a869f86d9334476b54136eac3dbaaac3db0120554afe88e67c5d91f31e7" => :mavericks
   end
 
   keg_only :provided_by_osx
