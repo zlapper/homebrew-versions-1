b5442b0ea231deb1df10a6dc8e46f667f76fd1a7
Fuji, Goro (gfx) <gfuji@cpan.org>
2015-12-11 14:50:10 +0900

    elasticsearch17: set log paths
    Closes #1073.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- elasticsearch17.rb
+++ elasticsearch17.rb
@@ -98,9 +98,9 @@ class Elasticsearch17 < Formula
           <key>WorkingDirectory</key>
           <string>#{var}</string>
           <key>StandardErrorPath</key>
-          <string>/dev/null</string>
+          <string>#{var}/log/elasticsearch17.log</string>
           <key>StandardOutPath</key>
-          <string>/dev/null</string>
+          <string>#{var}/log/elasticsearch17.log</string>
         </dict>
       </plist>
     EOS
