d9d457148c0a954cf3ff48407d77a2cf5eb71237
BrewTestBot <brew-test-bot@googlegroups.com>
2015-12-18 15:32:56 +0000

    libidn2: add 0.10 bottle.

--- libidn2.rb
+++ libidn2.rb
@@ -4,6 +4,13 @@ class Libidn2 < Formula
   url "http://alpha.gnu.org/gnu/libidn/libidn2-0.10.tar.gz"
   sha256 "3d301890bdbb137424f5ea495f82730a4b85b6a2549e47de3a34afebeac3e0e3"
 
+  bottle do
+    cellar :any
+    sha256 "d25ba971f164b3345fa0d9fcce5b8f0b59a8ae4348a3570f296410305f253ef6" => :el_capitan
+    sha256 "730dd356547fe41ea848335319608ad75ff98ae59f1550676cb86011723b57af" => :yosemite
+    sha256 "49b9882fe6971e1623b8f64debfe6234e798b81424b4aac076b393d0e8cca07e" => :mavericks
+  end
+
   def install
     ENV.universal_binary if build.universal?
     system "./configure", "--disable-debug",
