900b1ab4f9765cc12fd5dea8deb84f313841cafd
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 16:31:48 +0100

    hive011: desc added

--- hive011.rb
+++ hive011.rb
@@ -1,4 +1,5 @@
 class Hive011 < Formula
+  desc "Hadoop-based data summarization, query, and analysis"
   homepage "https://hive.apache.org"
   url "https://archive.apache.org/dist/hive/hive-0.11.0/hive-0.11.0-bin.tar.gz"
   sha256 "c22ee328438e80a8ee4b66979dba69650511a73f8b6edf2d87d93c74283578e5"
