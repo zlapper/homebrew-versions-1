b57877a5d865d70ac5c3964036ad70f3d5a28d82
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 16:31:49 +0100

    ruby182: desc added

--- ruby182.rb
+++ ruby182.rb
@@ -1,4 +1,5 @@
 class Ruby182 < Formula
+  desc "Powerful, clean, object-oriented scripting language"
   homepage "https://www.ruby-lang.org/en/"
   url "http://cache.ruby-lang.org/pub/ruby/1.8/ruby-1.8.2.tar.gz"
   mirror "http://mirrorservice.org/sites/ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.2.tar.bz2"
