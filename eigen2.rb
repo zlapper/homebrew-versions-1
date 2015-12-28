19c4576d0f355267a17352f172482bf3707bcf8e
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 16:31:48 +0100

    eigen2: desc added

--- eigen2.rb
+++ eigen2.rb
@@ -1,4 +1,5 @@
 class Eigen2 < Formula
+  desc "C++ template library for linear algebra"
   homepage "http://eigen.tuxfamily.org/"
   url "https://bitbucket.org/eigen/eigen/get/2.0.17.tar.bz2"
   sha256 "7255e856ed367ce6e6e2d4153b0e4e753c8b8d36918bf440dd34ad56aff09960"
