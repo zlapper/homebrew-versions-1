0c3349579194f0c207bd04448e3223d5237e001e
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 16:31:49 +0100

    mysql51: desc added

--- mysql51.rb
+++ mysql51.rb
@@ -1,4 +1,5 @@
 class Mysql51 < Formula
+  desc "Open source relational database management system"
   homepage "https://dev.mysql.com/doc/refman/5.1/en/"
   url "http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/mysql-5.1.73.tar.gz"
   sha256 "05ebe21305408b24407d14b77607a3e5ffa3c300e03f1359d3066f301989dcb5"
