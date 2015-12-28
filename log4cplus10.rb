02523e44bfa8f48e39804e3d453903a81dd5038f
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 16:31:49 +0100

    log4cplus10: desc added

--- log4cplus10.rb
+++ log4cplus10.rb
@@ -1,4 +1,5 @@
 class Log4cplus10 < Formula
+  desc "Logging Framework for C++"
   homepage "http://log4cplus.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/log4cplus/log4cplus-stable/1.0.4/log4cplus-1.0.4.3.tar.bz2"
   sha256 "9af936fc4a25c3a59b7ae2c34ce95e08e8a705797ffe27e13272c01732649491"
