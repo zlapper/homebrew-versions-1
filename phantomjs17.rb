d173b04b350ab3d71abdce00df3c9bbf4dfa57aa
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 16:31:49 +0100

    phantomjs17: desc added

--- phantomjs17.rb
+++ phantomjs17.rb
@@ -1,4 +1,5 @@
 class Phantomjs17 < Formula
+  desc "Headless WebKit scriptable with a JavaScript API"
   homepage "http://www.phantomjs.org/"
   url "https://phantomjs.googlecode.com/files/phantomjs-1.7.0-macosx.zip"
   sha256 "5e3cd030dd0c420cc6e88aedff997a10c77322a9507f6be162e05a5894825705"
