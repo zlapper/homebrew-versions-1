68d1d8c94ebe47a7a93324083c7b08273b562837
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-28 14:01:59 +0100

    zeromq32: desc added

--- zeromq32.rb
+++ zeromq32.rb
@@ -1,4 +1,5 @@
 class Zeromq32 < Formula
+  desc "High-performance, asynchronous messaging library"
   homepage "http://www.zeromq.org/"
   url "http://download.zeromq.org/zeromq-3.2.5.tar.gz"
   sha256 "09653e56a466683edb2f87ee025c4de55b8740df69481b9d7da98748f0c92124"
